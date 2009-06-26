b009dc4a42d6083d52893fb8d8cdb3a80d936b24
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2009-06-26 12:08:04 +0000

    added running of tests to the setup script Date: 2009-06-02 16:37:05
    git-svn-id: file:///home/amuse/svn/amuse/trunk@31 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 from distutils.command.build import build
 from support.build_latex import build_latex
+from support.run_tests import run_tests
 
 build.sub_commands.append(('build_latex',None))
 
 setup(
 	name = 'amuse',
 	version = '1.0',
-	cmdclass = {'build_latex':build_latex}
+	cmdclass = {'build_latex':build_latex, 'tests':run_tests}
 )
 #latex_documents = ['doc/install/amuse-scope.tex','doc/instal/amuse-overview.tex'],
 	
---
+++ support/run_tests.py
@@ -0,0 +1,39 @@
+__revision__ = "$Id:$"
+
+import sys, os, re, subprocess
+
+from distutils.core import Command
+from distutils import log
+
+from nose.core import TestProgram
+
+# check if Python is called on the first line with this expression
+first_line_re = re.compile('^#!.*python[0-9.]*([ \t].*)?$')
+
+class run_tests(Command):
+
+    description = "run add unit tests"
+
+    user_options = [
+        ('test-dir', 't', "root directory of the tests cases")
+        ]
+
+    boolean_options = []
+
+
+    def initialize_options (self):
+        self.test_dir = None
+
+    def finalize_options (self):
+    	if not self.test_dir:
+		self.test_dir = 'test'
+
+    def run (self):
+        TestProgram(argv=[self.test_dir], exit=False) 
+            
+            
+            
+            
+            
+    
+            
