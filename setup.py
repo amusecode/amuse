92f3b4734e322f721ebe360d881098b8f8bcdd42
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2009-06-26 12:07:49 +0000

    added support to help build the latex files Date: 2009-06-02 14:57:13
    git-svn-id: file:///home/amuse/svn/amuse/trunk@28 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from distutils.command.build import build
+from dist.build_latex import build_latex
+
+build.sub_commands.append(('build_latex',None))
+
+setup(
+	name = 'amuse',
+	version = '1.0',
+	cmdclass = {'build_latex':build_latex}
+)
+#latex_documents = ['doc/install/amuse-scope.tex','doc/instal/amuse-overview.tex'],
+	
