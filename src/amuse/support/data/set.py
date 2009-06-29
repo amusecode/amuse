a2ef57deb030d1474b567708cc27b060aacddf62
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2009-06-29 07:57:53 +0000

    design
    git-svn-id: file:///home/amuse/svn/amuse/trunk@64 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/support/data/set.py
@@ -0,0 +1,3 @@
+"""
+A set is a collection of values with the same units and precision
+"""
---
+++ src/amuse/support/data/values.py
@@ -0,0 +1,11 @@
+"""
+Value system
+
+Every value has a type, unit and precision. All data is made up out of values.
+
+From si::
+
+ The value of a physical quantity is the quantitative expression of a particular physical quantity as the product of a number and a unit, the number being its numerical value. Thus, the numerical value of a particular physical quantity depends on the unit in which it is expressed.
+"""
+
+
