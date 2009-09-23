99e4848dba92c2ee3097bea47b483160c863d2c8
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2009-09-23 08:55:58 +0000

    added energy support
    git-svn-id: file:///home/amuse/svn/amuse/trunk@233 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/legacy/bhtree/muse_dynamics_mpi.py
+++ src/amuse/legacy/bhtree/muse_dynamics_mpi.py
@@ -184,5 +184,10 @@ class BHTree(LegacyInterface):
             if x.name == 'mass':
                 self.set_mass(id, self.convert_nbody.to_nbody(x.value()).number)
         
+    def get_energies(self):
+        energy_unit = nbody_system.mass * nbody_system.length ** 2  * nbody_system.time ** -2
+        kinetic_energy = self.get_kinetic_energy() | energy_unit
+        potential_energy = self.get_potential_energy() | energy_unit
+        return (self.convert_nbody.to_si(kinetic_energy), self.convert_nbody.to_si(potential_energy))
     
   
