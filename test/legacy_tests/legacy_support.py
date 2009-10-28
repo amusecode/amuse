f9615779a6284997e74449e0545c1d6b9b6b6acb
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2009-10-28 15:34:56 +0000

    clean-up mpi workers
    git-svn-id: file:///home/amuse/svn/amuse/trunk@329 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/legacy_tests/legacy_support.py
+++ test/legacy_tests/legacy_support.py
@@ -1,11 +1,12 @@
 import unittest
 
-from amuse.legacy.support.core import is_mpd_running
+from amuse.legacy.support.core import is_mpd_running, stop_interfaces
 
 class TestWithMPI(unittest.TestCase):
 
     def setUp(self):
         self.assertTrue(is_mpd_running(), "MPICH2 mpd deamon process not running, cannot run this test as it requires MPI")
             
-            
+    def tearDown(self):
+        stop_interfaces()
             
