fc8f621064303895b4ca91a22470fd5574015aae
marosvolgyi <marosvolgyi@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2009-10-30 10:14:10 +0000

    literature refs with namedtuple
    git-svn-id: file:///home/amuse/svn/amuse/trunk@340 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/legacy/support/lit.py
+++ src/amuse/legacy/support/lit.py
@@ -1,5 +1,6 @@
 from docutils import core
 import docutils.nodes as nodes
+from collections import namedtuple
 
 class LiteratureRefs(object):
 
@@ -9,35 +10,55 @@ class LiteratureRefs(object):
         self.fill_literature_list()
  
     @classmethod
-    def print_refs(cls, modus):
+    def print_refs(cls):
         print "You are currently using the following codes, which contain literature references"
         print cls.all_literature_references_string()
  
     @classmethod
+    def export2html(cls):
+        pass
+
+    @classmethod
+    def export2bibtex(cls):
+        pass
+   
+    @classmethod
     def names_of_classes_with_references(cls):
         return [x[0] for x in cls.literature_list]
-
+ 
     @classmethod
     def all_literature_references_string(cls):
         lines = []
         for s in cls.literature_list:
-            lines.append('\n\t"%s"' % s[0])
-            for key_and_note in s[1]:
-                lines.append('\t\t%s' % (key_and_note[1]))
+            lines.append('\n\t"%s"' % s.name_of_class_with_refs)
+            for literature_reference_of_class_item in s.literature_references_of_class:
+                lines.append('\t\t%s' % (literature_reference_of_class_item.footnote))
         return "\n".join(lines)
         
     @classmethod
     def fill_literature_list(cls):
         """filter the refs form the docstring, if no refs there is no append"""
-        docstringin = cls.__doc__
+        classes_with_references_item = namedtuple("cwrl", 
+                                                  "name_of_class_with_refs literature_references_of_class"
+                                                 )
+        literature_references_of_class_item = namedtuple("lrocl", 
+                                                         "id footnote"
+                                                         )
+
+        docstring_in = cls.__doc__
         objectname = cls.__name__
-        Mydoctree  = core.publish_doctree(source = docstringin)
-        footnote_list = []
+        Mydoctree  = core.publish_doctree(source = docstring_in)
+
+        literature_references_of_class = []
+        
         for ikey, ival in Mydoctree.ids.iteritems():
             if isinstance(ival,nodes.footnote):
-                footnote_list.append([ikey, ival.rawsource])
+                literature_references_of_class.append(literature_references_of_class_item(ikey, ival.rawsource))
         
-        filled = bool(footnote_list)
+        filled = bool(literature_references_of_class)
         
         if filled:
-            cls.literature_list.append([objectname, footnote_list])
+            cls.literature_list.append(classes_with_references_item(objectname, 
+                                                                    literature_references_of_class
+                                                                    )
+                                       )
