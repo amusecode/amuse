b7d311a29e436a8c9428316d1f5a9fd6fb97707f
marosvolgyi <marosvolgyi@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2009-11-06 14:28:21 +0000

    work in pr. on consistency with gd
    git-svn-id: file:///home/amuse/svn/amuse/trunk@355 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/legacy/hermite0/interface.py
+++ src/amuse/legacy/hermite0/interface.py
@@ -3,7 +3,12 @@ from amuse.legacy.interface.gd import GravitationalDynamics
 from amuse.legacy.support.lit import LiteratureRefs
 
 class Hermite(LegacyInterface, LiteratureRefs, GravitationalDynamics):
-		
+    """ 
+        .. [#] Hut, P., Makino, J. & McMillan, S., *Astrophysical Journal Letters* , **443**, L93-L96 (1995)
+    """		
+    include_headers = ['hermite_code.h', 'parameters.h', 'local.h']
+
     def __init__(self):
         LegacyInterface.__init__(self, name_of_the_worker="worker_code")
-        
+        LiteratureRefs.__init__(self)
+
