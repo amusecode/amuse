c71d166a09f75724d2713ab628ee8fe7289220e5
pelupes <pelupes@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-01-07 11:00:59 +0000

    SI prefixes, eg: deca(m), centi(m), etc
    git-svn-id: file:///home/amuse/svn/amuse/trunk@466 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/support/units/si.py
+++ src/amuse/support/units/si.py
@@ -16,3 +16,45 @@ no_unit = core.none_unit('no unit','')
 none = core.none_unit('no unit','')
 
 named = core.named_unit
+
+# SI prefixes
+def deca(unit):
+    return named('deca'+unit.name,'da'+unit.symbol,10.*unit)    
+def hecto(unit):
+    return named('hecto'+unit.name,'h'+unit.symbol,100.*unit)    
+def kilo(unit):
+    return named('kilo'+unit.name,'k'+unit.symbol,1000.*unit)    
+def mega(unit):
+    return named('mega'+unit.name,'M'+unit.symbol,1.e6*unit)    
+def giga(unit):
+    return named('giga'+unit.name,'G'+unit.symbol,1.e9*unit)    
+def terra(unit):
+    return named('terra'+unit.name,'T'+unit.symbol,1.e12*unit)    
+def peta(unit):
+    return named('peta'+unit.name,'P'+unit.symbol,1.e15*unit)    
+def exa(unit):
+    return named('exa'+unit.name,'E'+unit.symbol,1.e18*unit)    
+def zetta(unit):
+    return named('zetta'+unit.name,'Z'+unit.symbol,1.e21*unit)    
+def yotta(unit):
+    return named('yotta'+unit.name,'Y'+unit.symbol,1.e24*unit)    
+def deci(unit):
+    return named('deci'+unit.name,'d'+unit.symbol,0.1*unit)    
+def centi(unit):
+    return named('centi'+unit.name,'c'+unit.symbol,0.01*unit)    
+def milli(unit):
+    return named('milli'+unit.name,'m'+unit.symbol,0.001*unit)    
+def micro(unit):
+    return named('micro'+unit.name,'mu'+unit.symbol,1.e-6*unit)    
+def nano(unit):
+    return named('nano'+unit.name,'n'+unit.symbol,1.e-9*unit)    
+def pico(unit):
+    return named('pico'+unit.name,'p'+unit.symbol,1.e-12*unit)    
+def femto(unit):
+    return named('femto'+unit.name,'f'+unit.symbol,1.e-15*unit)    
+def atto(unit):
+    return named('atto'+unit.name,'a'+unit.symbol,1.e-18*unit)    
+def zepto(unit):
+    return named('zepto'+unit.name,'z'+unit.symbol,1.e-21*unit)    
+def yocto(unit):
+    return named('yocto'+unit.name,'y'+unit.symbol,1.e-24*unit)    
