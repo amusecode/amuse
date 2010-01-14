f285957ca4aef7dcb8f9d7777cad24cc1137f6a5
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-01-14 14:22:07 +0000

    Added EVtwin error handling
    git-svn-id: file:///home/amuse/svn/amuse/trunk@515 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/legacy/evtwin/interface.py
+++ src/amuse/legacy/evtwin/interface.py
@@ -212,28 +212,84 @@ class EVtwinBinding(InterfaceWithParametersBinding, InterfaceWithObjectsBinding)
         
     def evolve_model(self, end_time = None):
         if end_time is None:
-            self._evolve_particles(self.particles)
-            return
+            result = self._evolve_particles(self.particles)
+            return result
             
         end_times = end_time.as_vector_with_length(len(self.particles))
         
         particles_set = self.particles.to_set()
+        print 'len(particles_set): ', len(particles_set)
         while len(particles_set) > 0:
-            self._evolve_particles(particles_set)
+            result = self._evolve_particles(particles_set)
+            if result != 0:
+                # Abort evolving because of error (result!=0)
+                return result
             particles_set = particles_set.select(lambda x : x < end_time, ["age"])
-            
+        # All particles have been succesfully evolved (result==0)
+        return result
                 
     def _evolve_particles(self, particles):
         for particle in particles:
             index = self.particles._private.attribute_storage.mapping_from_particle_key_to_index_in_the_code[particle.key]
             
             errorcode = self.evolve(index)
-            #print index, errorcode
             if errorcode != 0:
-                print index, errorcode
-                print particle
-                raise Exception("Error during evolve of particle")
-            #print particle
+                print 'Retreived error while evolving particle: ', index, particle
+                if errorcode == -2:
+                    error_string = ' -2 -- BEGINN -- requested mesh too large'
+                elif errorcode == -1:
+                    error_string = ' -1 -- STAR12 -- no timesteps required'
+                elif errorcode == 2:
+                    error_string = '  2 -- BACKUP -- tstep reduced below limit; quit'
+                elif errorcode == 3:
+                    error_string = '  3 -- NEXTDT -- *2 evolving beyond last *1 model' 
+                elif errorcode == 4:
+                    error_string = '  4 -- PRINTB -- *1 rstar exceeds rlobe by limit'
+                elif errorcode == 5:
+                    error_string = '  5 -- PRINTB -- age greater than limit'
+                elif errorcode == 6:
+                    error_string = '  6 -- PRINTB -- C-burning exceeds limit'
+                elif errorcode == 7:
+                    error_string = '  7 -- PRINTB -- *2 radius exceeds rlobe by limit'
+                elif errorcode == 8:
+                    error_string = '  8 -- PRINTB -- close to He flash'
+                elif errorcode == 9:
+                    error_string = '  9 -- PRINTB -- massive (>1.2 msun) deg. C/O core'
+                elif errorcode == 10:
+                    error_string = ' 10 -- PRINTB -- |M1dot| exceeds limit' 
+                elif errorcode == 11:
+                    error_string = ' 11 -- NEXTDT -- impermissible FDT for *2' 
+                elif errorcode == 14:
+                    error_string = ' 14 -- PRINTB -- funny compos. distribution'
+                elif errorcode == 15:
+                    error_string = ' 15 -- STAR12 -- terminated by hand'
+                elif errorcode == 16:
+                    error_string = ' 16 -- MAIN   -- ZAHB didnt converge'
+                elif errorcode == 17:
+                    error_string = ' 17 -- BEGINN -- Nucleosynthesis did not converge'
+                elif errorcode == 51:
+                    error_string = ' 51 -- PRINTB -- end of MS (core H below limit)'
+                elif errorcode == 52:
+                    error_string = ' 52 -- PRINTB -- Radius exceeds limit'
+                elif errorcode == 53:
+                    error_string = ' 53 -- PRINTB -- Convergence to target model reached minimum'
+                elif errorcode == 12:
+                    error_string = '  12 -- BACKUP -- tstep reduced below limit; quit -- core H non-zero'
+                elif errorcode == 22:
+                    error_string = '  22 -- BACKUP -- tstep reduced below limit; quit -- core He non-zero'
+                elif errorcode == 32:
+                    error_string = '  32 -- BACKUP -- tstep reduced below limit; quit -- core C non-zero'
+                else:
+                    error_string = 'Unknown errorcode: ' + str(errorcode)
+                    
+                # Just print the error message or raise an exception: (have to sort out what would be more convenient later)
+                #print error_string
+                raise Exception(error_string)
+                
+                # Abort evolving because of error (errorcode!=0)
+                return errorcode
+        # All particles have been succesfully evolved (errorcode==0)
+        return errorcode
         
     def current_model_time(self):
         return self._current_model_time
