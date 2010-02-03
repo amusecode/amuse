255f6cfa61098092e9672d15806db57d2bbb139b
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-02-03 15:25:47 +0000

    code interface units converted
    git-svn-id: file:///home/amuse/svn/amuse/trunk@595 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/support/interface.py
+++ src/amuse/support/interface.py
@@ -1,6 +1,8 @@
 
 from amuse.support.data import parameters
-
+from amuse.support.data import core
+from amuse.support.data import values
+import inspect
 
 class OldObjectsBindingMixin(object):
                 
@@ -26,3 +28,52 @@ class CodeInterface(OldObjectsBindingMixin):
     
     def __init__(self):
         self.parameters = parameters.Parameters(self.parameter_definitions, self)
+        
+
+class CodeInterfaceWithConvertedUnits(object):
+    class UnitsConvertionMethod(object):
+        def __init__(self, real_method, converter):
+            self.real_method = real_method
+            self.converter = converter
+            
+        def __call__(self, *list_arguments, **keyword_arguments):
+            converted_list_arguments = [self.from_source_to_target(x) for x in list_arguments]
+            converted_keyword_arguments = {}
+            for key, value in keyword_arguments:
+                converted_keyword_arguments[key] = self.from_source_to_target(value)
+            
+            result = self.real_method(*converted_list_arguments, **converted_keyword_arguments)
+            return self.from_target_to_source(result)
+            
+        def from_source_to_target(self, x):
+            if isinstance(x, values.Quantity):
+                return self.converter.from_source_to_target(x)
+            else:
+                x
+                
+        def from_target_to_source(self, x):
+            if isinstance(x, values.Quantity):
+                return self.converter.from_target_to_source(x)
+            else:
+                x
+    
+    def __init__(self, interface, converter):
+        self.converter = converter
+        self.interface = interface
+        
+    def __getattr__(self, name):
+        attribute = getattr(self.interface, name)
+        if inspect.ismethod(attribute):
+            return self.UnitsConvertionMethod(attribute, self.converter)
+        elif isinstance(attribute, core.AbstractParticleSet):
+            return ParticlesWithUnitsConverted(attribute, self.converter)
+        elif isinstance(attribute, values.Quantity):
+            return self.converter.from_target_to_source(attribute)
+        else:
+            return attribute
+            
+    def __dir__(self):
+        return dir(self.interface)
+        
+        
+
---
+++ test/core_tests/test_interface.py
@@ -0,0 +1,49 @@
+from amuse.support import interface
+from amuse.support.units import units
+from amuse.support.units import nbody_system
+from amuse.support.data.binding import *
+
+import unittest
+
+class CodeInterfaceWithConvertedUnitsTests(unittest.TestCase):
+    class TestClass(interface.CodeInterface):
+        
+        total_mass = CodeProperty("get_mass", nbody_system.mass)
+        
+        def get_mass(self):
+            return 10.0, 0
+            
+        def add_to_length(self, length):
+            return length + (10.0 | nbody_system.length)
+    
+    def test1(self):
+        convert_nbody = nbody_system.nbody_to_si(10.0 | units.kg , 5.0 | units.m )
+        
+        original = self.TestClass()
+        instance = interface.CodeInterfaceWithConvertedUnits(
+                original,  
+                convert_nbody.as_converter_from_si_to_nbody()
+        )
+        
+        self.assertEquals(original.total_mass, 10.0 | nbody_system.mass)
+        
+        self.assertAlmostEquals(instance.total_mass.value_in(units.kg), 100.0, 10)
+        
+    def test2(self):
+        convert_nbody = nbody_system.nbody_to_si(10.0 | units.kg , 5.0 | units.m )
+        
+        original = self.TestClass()
+        instance = interface.CodeInterfaceWithConvertedUnits(
+                original,  
+                convert_nbody.as_converter_from_si_to_nbody()
+        )
+        
+        self.assertEquals(original.total_mass, 10.0 | nbody_system.mass)
+        
+        self.assertAlmostEquals(instance.total_mass.value_in(units.kg), 100.0, 10)
+        
+        self.assertEquals(original.add_to_length(5|nbody_system.length), 15.0 | nbody_system.length)
+        self.assertAlmostEquals(instance.add_to_length(5|units.m).value_in(units.m), 55.0, 10.0)
+        
+        
+
