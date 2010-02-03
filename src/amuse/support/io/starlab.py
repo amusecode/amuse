11fe833d2a337134381ac1cbb802dfb3d951ffb5
marosvolgyi <marosvolgyi@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-02-03 13:03:33 +0000

    dyn 2 amuse paritcle converter
    git-svn-id: file:///home/amuse/svn/amuse/trunk@591 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/support/io/starlab.py
@@ -0,0 +1,265 @@
+from amuse.support.data import core
+from amuse.support.units import units
+
+import sys
+import xml.dom.minidom 
+import pdb #use with pdb.set_trace()
+import re
+
+class Dyn2Xml(object):
+    
+    def __init__(self):
+
+        self._xml = ""
+    
+    def convertToXml(self,inputfile):
+        """
+            convertToXml converts legacy starlab fileformat .dyn to .xml
+       
+        """
+
+        try:
+            f = open(inputfile,'r')
+        except IOError: # as (errno, strerror):
+            print "I/O error({0}): {1}".format(errno, strerror)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+        
+        fooline = f.readline()
+            
+        #lines either start with (,) followed by name of stories or Particle
+        #Log, Dynamics, Hydro, Star
+        #or with parameters
+        #m = 0.5 etc...
+        #Here we do not care about structure, whether the particle is a sister
+        #or daughter etc...
+        #..Thinking about reimplementing this using regularexpressions...
+
+        #The xml string, init and add the root        
+        #We need the root, an xml file with only sisters is not well fromed as it is
+        #interpreted as multiple roots then. xml.dom yields an error
+        xmls = '<?xml version="1.0"?>\n<System>\n'
+        
+        #counting opening and closing of parenthesis to check match at the end.
+        #we do not want bad xml files.
+        
+        openpar  = 0
+        closepar = 0
+        
+        while fooline:
+
+            #State machine...
+            
+            fooline = fooline.rstrip()
+                   
+            if fooline.startswith("("):
+                #Either Particle or stories
+                #don't care about structure but do count parenthesis though
+                openpar += 1
+                #strip the (
+                fooline = fooline.lstrip("(")
+                newline = "<"+fooline+">"
+                
+            elif fooline.startswith(")"):
+                closepar +=1
+                #strip the )
+                fooline = fooline.lstrip(")")
+                newline = "</"+fooline+">"
+            else:
+                newline = self.convert_storyline(fooline)
+          
+            
+            xmls += newline + "\n"                
+            #Get next line from file...                
+            fooline = f.readline()
+
+        xmls += "</System>\n"    
+
+        f.close()
+
+        if closepar!=openpar:
+            print "\nConversion failure, parenthesis mismatch. Return: no output string written\n"
+            return 1#Error
+        else:
+            self._xmls = xmls
+            return 0#no Error
+    
+    def convert_storyline(self, fooline):
+        
+        if "===>" in fooline or fooline.startswith("       "):
+            newline = fooline 
+        else:
+            splithere = fooline.find("=")
+            newline = "  <pm "+fooline[:splithere].lstrip()+'= "'+fooline[splithere+1:].lstrip()+'" />'
+        
+        return newline
+    
+class Xml2Particles(object):
+
+    def __init__(self):
+
+        self.xmls = ""
+
+        self.reclim = sys.getrecursionlimit()
+        
+        self.system = core.Stars(0)
+
+        self.translator = {'N':'number','m':'mass','r':'position','v':'velocity'}
+
+    def add_particle_with_parameters(self, subnode, Parent):
+    	new_particle = core.Stars(1)                                  
+    	self.system.add_particles(new_particle)                       
+    	added_particle = self.system[-1]                              
+    	self._findNodes(subnode.childNodes,                           
+    	                added_particle, Parent = added_particle)      
+    	if Parent == None:                                            
+    	    pass
+    	else:                                                         
+    	    Parent.add_child(added_particle)                          
+
+    def _findNodes(self, nodeList, SCL, Parent = None):
+
+        for subnode in nodeList:
+            if subnode.nodeType == subnode.ELEMENT_NODE:
+                if subnode.tagName == "System":
+                    self._findNodes(subnode.childNodes, SCL)
+                if subnode.tagName == "Particle":
+                    self.add_particle_with_parameters(subnode, Parent)
+                elif subnode.tagName == u"pm":                                
+                    key = subnode.attributes.keys()[0]
+                    value = subnode.getAttribute(key)
+                    self.copy_starlab_parameter_to_star(key, value, SCL)
+                elif subnode.tagName in ["Log", "Dynamics", "Star", "Hydro"]:  
+                    self._findNodes(subnode.childNodes,SCL)  
+
+    def copy_starlab_parameter_to_star(self, key, value, SCL):
+
+	 if key in self.translator.keys():                                     
+	     amuse_key = self.translator[key]                                  
+	     if amuse_key == 'mass':                                           
+	         SCL.mass = float(value)|units.kg                              
+	     if amuse_key == "position":                                       
+	         SCL.position = self.convert2vec(value)|units.m                
+	     if amuse_key == "velocity":                                       
+	         SCL.velocity = self.convert2vec(value)|(units.m/units.s)      
+        
+    def convert2vec(self, attribute):
+
+        maybe_vec = attribute.split()
+
+        if len(maybe_vec)>1:
+            barout = [float(f) for f in maybe_vec]
+        else:
+            barout = float(maybe_vec[0])
+        return barout
+
+    def walk_me(self, SCL):
+
+        self.xmls += self.elementstr(SCL,"open") +"\n"
+            
+        for attrlistitem in SCL.attriblist:
+
+            attribute = attrlistitem[0]
+            modified = attrlistitem[2]
+            
+            if attribute == "Sister" :
+                if SCL.nsisters > 0:
+                    for i in range(SCL.nsisters):
+                        self.walk_me(SCL.Sister[i])
+                        
+            elif attribute in ["Log","Dynamics","Star","Hydro"]:
+                self.walk_me(SCL.__getattribute__(attribute))
+                
+            elif not attribute in ["nsisters"]:
+                foostr = str(SCL.__getattribute__(attribute))
+                if not foostr == "" and modified:
+                    #dont bother if empty
+                    foostr = self.convert2propstr(foostr)
+                    if attribute == "text":
+                        #text is not a <pm />
+                        self.xmls += foostr + "\n"
+                    else:
+                        self.xmls += '  <pm '+attribute+' = "'+ foostr +'" />\n'
+
+        self.xmls += self.elementstr(SCL,"close")+"\n"
+        
+        
+    def elementstr(self,SCL,oc):
+        #write an element node,
+        #If particle is root then it is called System
+        
+        if oc == "open":
+            prestr = "<"
+        elif oc == "close":
+            prestr = "</"
+
+        if SCL.__class__.__name__ == "Particle":
+            if SCL.isroot:
+                barstr =  prestr+"System>"
+            else:
+                barstr = prestr+SCL.__class__.__name__+">"        
+        else:
+            barstr = prestr+SCL.__class__.__name__.lstrip("_")+">"
+
+        return barstr
+    
+    def convert2propstr(self,foostr):
+        #if foostr is a vector we dont want the brackets and the ,'s
+        return foostr.replace("[","").replace("]","").replace(","," ")
+
+    def makexml(self):
+        #clear and init xmls
+        self.xmls = '<?xml version="1.0"?>\n'
+        self.walk_me(self.System)
+    
+    def dumpxml(self):
+        #Convert System to xml string and dump it on the screen
+        self.makexml()
+        print self.xmls
+
+    def savexml(self, file):
+        #Convert System to xml string and put it in file with name file
+        f = open(file,'w')
+        self.makexml()
+        f.writelines(self.xmls)
+        f.close()
+       
+    def loadxml(self,file):
+        #Parse file into dom doc
+        try:
+            f = open(file,'r')
+        except IOError as (errno, strerror):
+            print "I/O error({0}): {1}".format(errno, strerror)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+        
+        doc = xml.dom.minidom.parse(f)
+        f.close()
+
+        #copy the doc to a system object
+        self._findNodes(nodeList=doc.childNodes, SCL = self.system)
+
+    def parse_xml(self, xmlstring):
+        doc = xml.dom.minidom.parseString(xmlstring)
+        self._findNodes(nodeList=doc.childNodes, SCL = self.system)
+
+class ParticlesFromDyn(object):
+    
+    def __init__(self, dyn_filename=None):
+        
+        Foo = Dyn2Xml()
+        err0 = Foo.convertToXml(dyn_filename)
+        xml_string = Foo._xmls
+        del Foo
+        
+        if not err0:
+            Foo2 = Xml2Particles()
+            err1 = Foo2.parse_xml(xml_string)
+        
+            self.Particles = Foo2.system
+            del Foo2
+        
+    def number_of_particles(self):
+        return len(self.Particles)
