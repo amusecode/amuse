ae2a71f30018aa54c83477cdee726cf8ec837450
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-02-19 12:25:12 +0000

    Temporarily removing MESA...
    git-svn-id: file:///home/amuse/svn/amuse/trunk@689 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/legacy/mesa/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-The Astrophysical Multipurpose Software Environment
-
-AMUSE contains a lot of functionality to execute astrophysical experiments.
-Look into the interface packages for the interfacing to physical model simulation software.
-You can also use AMUSE to convert common data formats.
-
-"""
