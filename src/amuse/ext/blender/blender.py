268d57283579c3c0b7b603c8a72e9ca26c5751b8
marosvolgyi <marosvolgyi@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-03-05 09:21:45 +0000

    blender integration module
    git-svn-id: file:///home/amuse/svn/amuse/trunk@757 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/ext/blender/blender.py
@@ -0,0 +1,23 @@
+import Blender
+import bpy
+import pylab as pl
+from Blender import Mesh
+
+class Primitives(object):
+    def __init__(self):
+        pass
+
+    @classmethod
+    def sphere(cli, segments, rings, radius):
+        me = Mesh.Primitives.UVsphere(segments, rings, radius)
+        scn = bpy.data.scenes.active     # link object to current scene
+        ob = scn.objects.new(me, 'sphere')
+        return ob
+
+    @classmethod
+    def cube(cli, radius):
+        me = Mesh.Primitives.Cube(radius)
+        scn = bpy.data.scenes.active     # link object to current scene
+        ob = scn.objects.new(me, 'cube')
+        return ob
+
