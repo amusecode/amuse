9a2497027041abc1a3ec00f493805ec8fc0dc710
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-03-09 14:31:07 +0000

    updated salpeter imf code
    git-svn-id: file:///home/amuse/svn/amuse/trunk@786 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/ext/salpeter.py
+++ src/amuse/ext/salpeter.py
@@ -7,8 +7,8 @@ from amuse.support.units import units
 
 class SalpeterIMF(object):
     def __init__(self, mass_min = 0.1 | units.MSun, mass_max = 125 | units.MSun, alpha = -2.35):
-        self.mass_min = mass_min.as_quantity_in(units.MSun)
-        self.mass_max = mass_max.as_quantity_in(units.MSun)
+        self.mass_min = mass_min
+        self.mass_max = mass_max
         self.alpha = alpha
         self.random = random.Random()
         self.random.seed()
@@ -16,26 +16,27 @@ class SalpeterIMF(object):
     def mass_mean(self):
         alpha1 = self.alpha + 1
         alpha2 = self.alpha + 2
-        l1 = pow(self.mass_min.value_in(units.MSun), alpha1)
-        l2 = pow(self.mass_min.value_in(units.MSun), alpha2)
-        u1 = pow(self.mass_max.value_in(units.MSun), alpha1)
-        u2 = pow(self.mass_max.value_in(units.MSun), alpha2)
-        return ((u2 - l2) * alpha1) / ((u1 - l1) * alpha2) | units.MSun
+        l1 = pow(self.mass_min, alpha1)
+        l2 = pow(self.mass_min, alpha2)
+        u1 = pow(self.mass_max, alpha1)
+        u2 = pow(self.mass_max, alpha2)
+        return ((u2 - l2) * alpha1) / ((u1 - l1) * alpha2)
         
     def mass(self, random_number):
+        one = 1.0 | units.none
         alpha1 = self.alpha + 1
-        factor = (pow(self.mass_max.value_in(units.MSun) / self.mass_min.value_in(units.MSun) , alpha1) - 1.0)
-        return self.mass_min.value_in(units.MSun) * (pow(1 + (factor * random_number), 1.0 / alpha1)) | units.MSun
+        factor = (pow(self.mass_max / self.mass_min , alpha1) - one )
+        return self.mass_min * (pow(one + (factor * random_number), 1.0 / alpha1))
         
     def next_mass(self):
         return self.mass(self.random.random())
         
     def next_set(self, number_of_stars):
-        set_of_masses = numpy.zeros(number_of_stars)
-        total_mass = 0.0 | units.MSun
+        set_of_masses = self.mass_min.unit.new_quantity(numpy.zeros(number_of_stars))
+        total_mass = self.mass_min.unit.new_quantity(0.0)
         for i in range(number_of_stars):
            mass = self.next_mass()
-           set_of_masses[i] = mass.value_in(units.MSun)
+           set_of_masses[i] = mass
            total_mass += mass
-        return (total_mass, units.MSun.new_quantity(set_of_masses))
+        return (total_mass, set_of_masses)
         
