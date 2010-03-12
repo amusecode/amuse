07dee2e2a583dc8b96d65401fa8d8437a0a51285
marosvolgyi <marosvolgyi@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-03-12 11:57:48 +0000

    hermite clean up
    git-svn-id: file:///home/amuse/svn/amuse/trunk@793 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/legacy_tests/test_hermite.py
+++ test/legacy_tests/test_hermite.py
@@ -1,9 +1,8 @@
-
 from legacy_support import TestWithMPI
 import os
 import sys
 
-from amuse.legacy.hermite0 import muse_dynamics_mpi as mpi_interface
+from amuse.legacy.hermite0.interface import HermiteInterface, Hermite
 
 from amuse.support.data import core
 from amuse.support.units import nbody_system
@@ -20,96 +19,94 @@ except ImportError:
 
 
 class TestMPIInterface(TestWithMPI):
+
+    def test0(self):
+        instance = HermiteInterface()
+        self.assertTrue("Hut" in instance.all_literature_references_string())
     
     def test1(self):
-        hermite = mpi_interface.Hermite()
-        hermite.setup_module()
-        hermite.add_particle(1, 11.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-        retrieved_state = hermite.get_state(1)
-        self.assertEquals(11.0,  retrieved_state['mass'])
-        self.assertEquals(hermite.get_number(), 1)
-        hermite.cleanup_module()
-        del hermite
+        instance = HermiteInterface()
+        instance.setup_module()
+
+        res1 = instance.new_particle(mass = 11.0, radius = 2.0, x = 0.0, y = 0.0, z = 0.0, vx = 0.0, vy = 0.0, vz = 0.0)
+        res2 = instance.new_particle(mass = 21.0, radius = 5.0, x = 10.0, y = 0.0, z = 0.0, vx = 10.0, vy = 0.0, vz = 0.0)
         
+        self.assertEquals(0, res1['index_of_the_particle'])
+        self.assertEquals(1, res2['index_of_the_particle'])
+
+        retrieved_state1 = instance.get_state(0)
+        retrieved_state2 = instance.get_state(1)
+
+        self.assertEquals(11.0,  retrieved_state1['mass'])
+        self.assertEquals(21.0,  retrieved_state2['mass'])
+        self.assertEquals(0.0,  retrieved_state1['x'])
+        self.assertEquals(10.0,  retrieved_state2['x'])
+
+        instance.cleanup_module()
+        del instance
+
     def test2(self):
-        hermite = mpi_interface.Hermite()
+        instance = HermiteInterface()
+        instance.setup_module()
+
+        for i in [0, 1, 2]:
+            temp_particle = instance.new_particle(mass = i, radius = 1.0, x = 0.0, y = 0.0, z = 0.0, vx = 0.0, vy = 0.0, vz = 0.0)
+            self.assertEquals(i, temp_particle['index_of_the_particle'])
+            
+        instance.delete_particle(1)
+      
+        self.assertEquals(2, instance.get_number_of_particles()['number_of_particles'])
+        
+        self.assertEquals(0, instance.get_index_of_first_particle()['index_of_the_particle'])
+        
+        self.assertEquals(2, instance.get_index_of_next_particle(0)['index_of_the_next_particle'])
+        self.assertEquals(0, instance.get_index_of_next_particle(0)['__result'])
+        self.assertEquals(-1, instance.get_index_of_next_particle(1)['__result'])
+        self.assertEquals(1, instance.get_index_of_next_particle(2)['__result'])
+        
+    def test3(self):
+        hermite = HermiteInterface()
         hermite.eps2 = 0.101
         self.assertEquals(0.101, hermite.eps2)
         hermite.eps2 = 0.110
         self.assertEquals(0.110, hermite.eps2)
         del hermite
-        
-    def test3(self):
-        hermite = mpi_interface.Hermite()
+
+    def test4(self):
+        hermite = HermiteInterface()
         hermite.flag_collision = 1
         self.assertEquals(1, hermite.flag_collision)
         hermite.flag_collision = 0
         self.assertEquals(0, hermite.flag_collision)
         del hermite
-        
-    def test4(self):
-        hermite = mpi_interface.Hermite()
-        hermite.setup_module()
-        
-        hermite.add_particle( [1,2,3,4]
-            , [11.0,12.0,13.0,14.0]
-            , [2.0,3.0,4.0,5.0]
-            , [2.1,3.1,4.1,5.1]
-            , [2.2,3.2,4.2,5.2]
-            , [2.3,3.3,4.3,5.3]
-            , [2.4,3.4,4.4,5.4]
-            , [2.5,3.5,4.5,5.5]
-            , [2.6,3.6,4.6,5.6])
-        retrieved_state = hermite.get_state(1)
-        self.assertEquals(11.0,  retrieved_state['mass'])
-        
-        retrieved_state = hermite.get_state([2,3,4])
-        self.assertEquals(12.0,  retrieved_state['mass'][0])
-        self.assertEquals(hermite.get_number(), 4)
-        hermite.cleanup_module()
-        
-    
+
     def test5(self):
-        hermite = mpi_interface.Hermite()
+        hermite = HermiteInterface()
         hermite.setup_module()
-        n = 10000
-        ids = [i for i in range(1,n)]
-        values = [1.0 * i for i in range(1,n)]
-        hermite.add_particle(ids
-            , values
-            , values
-            , values
-            , values
-            , values
-            , values
-            , values
-            , values)
-        retrieved_state = hermite.get_state(3999)
-        self.assertEquals(3999.0,  retrieved_state['mass'])
-        hermite.cleanup_module()
         
+        hermite.new_particle([10,20],[1,1],[0,0],[0,0], [0,0], [0,0], [0,0], [0,0])
+        retrieved_state = hermite.get_state(0)
+        
+        self.assertEquals(10.0,  retrieved_state['mass'])
+        self.assertEquals(1, retrieved_state['radius'])
+
+        retrieved_state = hermite.get_state([0,1])
+        self.assertEquals(20.0,  retrieved_state['mass'][1])
+        self.assertEquals(hermite.get_number_of_particles()['number_of_particles'], 2)
+        hermite.cleanup_module() 
+
     def test6(self):
-        hermite = mpi_interface.Hermite()
+        hermite = HermiteInterface()
         hermite.setup_module()
-        n = 4000
-        ids = [i for i in range(1,n)]
-        values = [1.0 * i for i in range(1,n)]
-        for i in range(n-1):
-            hermite.add_particle(ids[i]
-                , values[i]
-                , values[i]
-                , values[i]
-                , values[i]
-                , values[i]
-                , values[i]
-                , values[i]
-                , values[i])
-                
-        retrieved_state = hermite.get_state(1)
-        self.assertEquals(1.0,  retrieved_state['mass'])
-        hermite.cleanup_module()
-        del hermite
         
+        hermite.new_particle([10,10],[1,1],[-1,1],[0,0], [0,0], [0,0], [0,0], [0,0])
+        retrieved_state = hermite.get_state(0)
+        
+        retr = hermite.get_potential_at_point(0.01, 0,0,0)
+        self.assertEqual(retr['phi'], -20.0)
+        hermite.cleanup_module()
+       
+
 class TestAmuseInterface(TestWithMPI):
     def new_system_of_sun_and_earth(self):
         stars = core.Stars(2)
@@ -127,56 +124,57 @@ class TestAmuseInterface(TestWithMPI):
         
         return stars
         
-    def xtest1(self):
+    
+    def test1(self):
         convert_nbody = nbody_system.nbody_to_si(1.0 | units.MSun, 149.5e6 | units.km)
 
-        hermite = mpi_interface.Hermite(convert_nbody)
+        hermite = Hermite(convert_nbody)
         hermite.parameters.epsilon_squared = 0.0 | units.AU**2
         hermite.setup_module()
         hermite.dt_dia = 5000
         
         stars = self.new_system_of_sun_and_earth()
         earth = stars[1]
-
-        hermite.add_particles(stars)
-
+                
+        hermite.setup_particles(stars)
+        
         hermite.evolve_model(365.0 | units.day)
         hermite.update_particles(stars)
+        print stars[0].x
         
-        postion_at_start = earth.position.value_in(units.AU)[0]
-        postion_after_full_rotation = earth.position.value_in(units.AU)[0]
-        
-        self.assertAlmostEqual(postion_at_start, postion_after_full_rotation, 6)
+        position_at_start = earth.position.value_in(units.AU)[0]
+        position_after_full_rotation = earth.position.value_in(units.AU)[0]
+        self.assertAlmostEqual(position_at_start, position_after_full_rotation, 6)
         
         hermite.evolve_model(365.0 + (365.0 / 2) | units.day)
         
         hermite.update_particles(stars)
-        postion_after_half_a_rotation = earth.position.value_in(units.AU)[0]
-        self.assertAlmostEqual(-postion_at_start, postion_after_half_a_rotation, 2)
-        
-        
+        position_after_half_a_rotation = earth.position.value_in(units.AU)[0]
+        self.assertAlmostEqual(-position_at_start, position_after_half_a_rotation, 2)
+                
         hermite.evolve_model(365.0 + (365.0 / 2) + (365.0 / 4)  | units.day)
         
         hermite.update_particles(stars)
-        postion_after_half_a_rotation = earth.position.value_in(units.AU)[1]
-        self.assertAlmostEqual(-postion_at_start, postion_after_half_a_rotation, 3)
+        position_after_half_a_rotation = earth.position.value_in(units.AU)[1]
+        self.assertAlmostEqual(-position_at_start, position_after_half_a_rotation, 3)
+        
         hermite.cleanup_module()
+        
         del hermite
+        
 
-    
-    def xtest2(self):
+    def test2(self):
         convert_nbody = nbody_system.nbody_to_si(1.0 | units.MSun, 149.5e6 | units.km)
 
-        instance = mpi_interface.Hermite(convert_nbody)
+        instance = Hermite(convert_nbody)
         instance.parameters.epsilon_squared = 0.0 | units.AU**2
         instance.setup_module()
         instance.dt_dia = 5000
         
         stars = self.new_system_of_sun_and_earth()
         earth = stars[1]
-        instance.add_particles(stars)
+        instance.setup_particles(stars)
     
-        stars.savepoint()
         for x in range(1,2000,10):
             instance.evolve_model(x | units.day)
             instance.update_particles(stars)
@@ -186,8 +184,9 @@ class TestAmuseInterface(TestWithMPI):
             figure = pyplot.figure()
             plot = figure.add_subplot(1,1,1)
             
-            x_points = stars.get_timeline_of_attribute(earth, "x")
-            y_points = stars.get_timeline_of_attribute(earth, "y")
+            x_points = earth.get_timeline_of_attribute("x")
+            y_points = earth.get_timeline_of_attribute("y")
+            
             x_points_in_AU = map(lambda (t,x) : x.value_in(units.AU), x_points)
             y_points_in_AU = map(lambda (t,x) : x.value_in(units.AU), y_points)
             
@@ -195,32 +194,69 @@ class TestAmuseInterface(TestWithMPI):
             
             plot.set_xlim(-1.5, 1.5)
             plot.set_ylim(-1.5, 1.5)
+               
             
             test_results_path = path_to_test_results.get_path_to_test_results()
-            output_file = os.path.join(test_results_path, "hermite-earth-sun.svg")
-            figure.savefig(output_file)    
+            output_file = os.path.join(test_results_path, "hermite-earth-sun2.svg")
+            figure.savefig(output_file)
+        
+        
         
         instance.cleanup_module()
         del instance
-        
-    def xtest3(self):
-        convert_nbody = nbody_system.nbody_to_si(units.MSun(1.0), units.km(149.5e6))
 
-        instance = mpi_interface.Hermite(convert_nbody)
+    def test3(self):
+        convert_nbody = nbody_system.nbody_to_si(1.0 | units.MSun, 149.5e6 | units.km)
+
+        instance = Hermite(convert_nbody)
+        instance.parameters.epsilon_squared = 0.0 | units.AU**2
         instance.setup_module()
         instance.dt_dia = 5000
         
-        stars = self.new_system_of_sun_and_earth()
-        instance.add_particles(stars)
-        factor = instance.get_energies()[0] / (instance.get_energies()[1] / (-2 | units.none))
-        total_energy0 =  sum(instance.get_energies(), 0|units.J)
-        self.assertAlmostEqual(factor.value_in(units.none), 1.000, 3)
-        instance.evolve_model(100 | units.day)
-        total_energy1 =  sum(instance.get_energies(), 0|units.J)
-        factor = instance.get_energies()[0] / (instance.get_energies()[1] / (-2 | units.none))
-        self.assertAlmostEqual(factor.value_in(units.none), 1.000, 3)
-        instance.evolve_model(100 | units.day)
-        total_energy2=  sum(instance.get_energies(), 0|units.J)
-        self.assertAlmostEqual((total_energy2 /  total_energy0).value_in(units.none), 1.0, 7)
-        self.assertAlmostEqual(total_energy2.value_in(units.J), total_energy1.value_in(units.J), 10)
+        stars = core.Stars(2)
+        star1 = stars[0]
+        star2 = stars[1]
+
+        star1.mass = units.MSun(1.0)
+        star1.position = units.AU(numpy.array((-1.0,0.0,0.0)))
+        star1.velocity = units.AUd(numpy.array((0.0,0.0,0.0)))
+        star1.radius = units.RSun(1.0)
+
+        star2.mass = units.MSun(1.0)
+        star2.position = units.AU(numpy.array((1.0,0.0,0.0)))
+        star2.velocity = units.AUd(numpy.array((0.0,0.0,0.0)))
+        star2.radius = units.RSun(100.0)
         
+        instance.setup_particles(stars)
+    
+        for x in range(1,2000,10):
+            instance.evolve_model(x | units.day)
+            instance.update_particles(stars)
+            stars.savepoint()
+        
+    
+    def test4(self):
+        convert_nbody = nbody_system.nbody_to_si(5.0 | units.kg, 10.0 | units.m)
+
+        instance = Hermite(convert_nbody)
+        instance.setup_module()
+        
+        particles = core.Particles(2)
+        self.assertEquals(len(instance.particles), 0)
+        
+        particles.mass = [15.0, 30.0] | units.kg
+        particles.radius =  [10.0, 20.0] | units.m
+        particles.position = [[10.0, 20.0, 30.0], [20.0, 40.0, 60.0]] | units.m
+        particles.velocity = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]] | units.m / units.s
+
+        
+        instance.particles.add_particles(particles)
+        self.assertEquals(len(instance.particles), 2)
+        
+        instance.particles.mass =  [17.0, 33.0] | units.kg
+        
+        
+        self.assertEquals(instance.get_mass(0), 17.0| units.kg) 
+        self.assertEquals(instance.get_mass(1), 33.0| units.kg)  
+
+         
