49d16afaa211737d17c1b10929af527400d9e1a0
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-03-15 12:44:04 +0000

    bugfix for previous commit, missed one reference
    git-svn-id: file:///home/amuse/svn/amuse/trunk@802 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/examples/test_HRdiagram_cluster.py
+++ test/examples/test_HRdiagram_cluster.py
@@ -10,7 +10,7 @@ except ImportError:
 
 from amuse.support.units import units
 from amuse.support.data import core
-from amuse.legacy.sse.muse_stellar_mpi import SSE
+from amuse.legacy.sse.interface import SSE
 from amuse.legacy.evtwin.interface import EVtwin
 from amuse.legacy.mesa.interface import MESA
 from amuse.legacy.support.core import is_mpd_running
