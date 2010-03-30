1cfe68fa4515931959b2d407509aff29687079db
marosvolgyi <marosvolgyi@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-03-30 12:26:06 +0000

    test for parameters docstring and __str__
    git-svn-id: file:///home/amuse/svn/amuse/trunk@889 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ test/core_tests/test_parameter_doc.py
@@ -0,0 +1,70 @@
+import os
+import sys
+
+from amuse.test import amusetest
+from amuse.legacy.bhtree.interface import BHTreeInterface, BHTree
+from amuse.legacy.hermite0.interface import HermiteInterface, Hermite
+from amuse.legacy.phiGRAPE.interface import PhiGRAPEInterface, PhiGRAPE
+from amuse.support.data import core
+from amuse.support.units import nbody_system
+from amuse.support.units import units
+
+class TestParameterDoc(amusetest.TestCase):
+
+    def test_bhtree(self):
+
+        convert_nbody = nbody_system.nbody_to_si(1.0 | units.MSun, 149.5e6 | units.km)
+
+        bhtree = BHTree(convert_nbody)
+
+        bhtree.parameters.epsilon_squared = 10 | units.km**2
+        bhtree.parameters.timestep = 1.0 | units.s
+        bhtree.parameters.openings_angle = 0.1
+
+        docstring =  bhtree.parameters.__doc__
+
+        self.assertTrue("smoothing parameter for gravity calculations (default value:0.3 nbody length * nbody length)" in docstring)
+        print docstring
+
+        parameter_str_method_output = str(bhtree.parameters)
+
+        self.assertTrue("epsilon_squared: 10000000.0 m**2" in parameter_str_method_output)
+        self.assertTrue("timestep: 1.0 s" in parameter_str_method_output)
+        self.assertTrue("openings_angle: 0.1" in parameter_str_method_output)
+
+    def test_hermite(self):
+
+        convert_nbody = nbody_system.nbody_to_si(1.0 | units.MSun, 149.5e6 | units.km)
+
+        hermite = Hermite(convert_nbody)
+
+        hermite.parameters.epsilon_squared = 10 | units.km**2
+
+        docstring =  hermite.parameters.__doc__
+
+        self.assertTrue("smoothing parameter for gravity calculations (default value:0.3 nbody length * nbody length)" in docstring)
+
+        parameter_str_method_output = str(hermite.parameters)
+
+        self.assertTrue("epsilon_squared: 10000000.0 m**2" in parameter_str_method_output)
+
+
+    def test_phigrape(self):
+
+        convert_nbody = nbody_system.nbody_to_si(1.0 | units.MSun, 149.5e6 | units.km)
+
+        phigrape = PhiGRAPE(convert_nbody)
+
+        phigrape.parameters.epsilon_squared = 10 | units.km**2
+
+        docstring =  phigrape.parameters.__doc__
+
+        self.assertTrue("smoothing parameter for gravity calculations (default value:0.0 nbody length * nbody length)" in docstring)
+        self.assertTrue("timestep parameter (default value:0.02)" in docstring)
+        self.assertTrue("parameter to determine the initial timestep (default value:0.01)" in docstring)
+
+        parameter_str_method_output = str(phigrape.parameters)
+
+        self.assertTrue("epsilon_squared: 10000000.0 m**2" in parameter_str_method_output)
+        self.assertTrue("timestep_parameter: 4.47422288341e-16" in parameter_str_method_output)
+        self.assertTrue("initial_timestep_parameter: 0.0" in parameter_str_method_output)
