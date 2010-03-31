33861ce8147e8cb90df344861a9ca39ecd51e5cc
marosvolgyi <marosvolgyi@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-03-31 09:15:34 +0000

    typo
    git-svn-id: file:///home/amuse/svn/amuse/trunk@902 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/core_tests/test_phigrape.py
+++ test/core_tests/test_phigrape.py
@@ -13,25 +13,16 @@ import numpy
 
 class Test(amusetest.TestCase):
 
-    def xtest1(self):
-        directory = os.path.dirname(__file__)
-        convert_nbody = nbody_system.nbody_to_si(1|units.kg, 1|units.m)
-        instance = phigrape.Inp2Particles()
-        instance.convert_to_particles(os.path.join(directory, 'plummer_100.ini'), convert_nbody)
-        #16k = 2**14
-        self.assertAlmostEqual(instance.Particles.mass,
-                               values.new_quantity(1.0/2**14*numpy.ones(101), units.kg),
-                               8)
 
-    def test2(self):
+    def test(self):
         directory = os.path.dirname(__file__)
         instance = phigrape.Inp2Particles()
         instance.convert_to_particles(os.path.join(directory, 'plummer_100.ini'))
         rev_instance = phigrape.Particles2Inp()
-        rev_instance.convert_to_inp(I.Particles, 'plummer_back_100.ini')
+        rev_instance.convert_to_inp(instance.Particles, 'plummer_back_100.ini')
 
         control_instance = phigrape.Inp2Particles()
-        control_instance_.convert_to_particles(os.path.join(directory, 'plummer_back_100.ini'))
+        control_instance.convert_to_particles(os.path.join(directory, 'plummer_back_100.ini'))
 
         self.assertAlmostEqual(control_instance.Particles.mass, instance.Particles.mass, 16)
         self.assertAlmostEqual(control_instance.Particles[1].position, instance.Particles[1].position, 16)
