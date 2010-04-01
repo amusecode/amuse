9ac148057b1f67b0a2f42c7365222b941735fd76
marosvolgyi <marosvolgyi@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-04-01 12:05:50 +0000

    test for octgrav, ticket #28
    git-svn-id: file:///home/amuse/svn/amuse/trunk@911 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ test/legacy_tests/test_octgrav.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+from amuse.legacy.octgrav.interface import OctgravInterface, Octgrav
+
+from amuse.support.data import core
+from amuse.support.units import nbody_system
+from amuse.support.units import units
+from amuse.legacy.support import channel
+
+from legacy_support import TestWithMPI
+import path_to_test_results
+
+import numpy
+
+try:
+    from matplotlib import pyplot
+    HAS_MATPLOTLIB = True
+except ImportError:
+    HAS_MATPLOTLIB = False
+
+
+class TestMPIInterface(TestWithMPI):
+
+    def test1(self):
+        instance = OctgravInterface()
+        instance.new_particle(11.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        retrieved_state = instance.get_state(1)
+        self.assertEquals(11.0,  retrieved_state['mass'])
+        self.assertEquals(2.0, retrieved_state['radius'])
+        self.assertEquals(instance.get_number_of_particles()['number_of_particles'], 1)
+        instance.cleanup_module()
+        del instance
+
+    def test2(self):
+        instance = OctgravInterface()
+        instance.initialize_code()
+        instance.new_particle(
+            [1,10,100],
+            [2,11,101],
+            [3,12,102],
+            [4,13,103],
+            [5,14,104],
+            [6,15,105],
+            [7,16,106],
+            [8,17,107])
+
+        particle1_state = instance.get_state(1)
+        self.assertEquals(1,   particle1_state['mass'])
+        particle2_state = instance.get_state(2)
+        self.assertEquals(10,  particle2_state['mass'])
+        instance.delete_particle(1)
+        size_result = instance.get_number_of_particles()
+        self.assertEquals(2, size_result['number_of_particles'])
+        new_particle1_state = instance.get_state(1)
+        self.assertEquals(10,  new_particle1_state['mass'])
+        instance.cleanup_module()
+        del instance
