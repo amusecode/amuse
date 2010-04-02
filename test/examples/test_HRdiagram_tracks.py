5ff47fe8ea3c20a37ed2e2be2f089a9c9eb36710
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-04-02 18:25:23 +0000

    Refactoring example HR tracks + option parser added. Ticket #89.
    git-svn-id: file:///home/amuse/svn/amuse/trunk@920 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/examples/test_HRdiagram_tracks.py
+++ test/examples/test_HRdiagram_tracks.py
@@ -1,23 +1,32 @@
 import sys
 import numpy 
 import os
+import warnings
 
-try:
-    from matplotlib import pyplot
-    HAS_MATPLOTLIB = True
-except ImportError:
-    HAS_MATPLOTLIB = False
+from optparse import OptionParser
 
 from amuse.support.units import units
 from amuse.support.data import core
+
 from amuse.legacy.sse.interface import SSE
 from amuse.legacy.evtwin.interface import EVtwin
 from amuse.legacy.mesa.interface import MESA
+
 from amuse.legacy.support.core import is_mpd_running
 import path_to_test_results
 
-def simulate_evolution_tracks(masses = [0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0] | units.MSun, \
-    name_of_the_figure = "HR_evolution_tracks.png", stellar_evolution_code=1):
+usage = """\
+usage: %prog [options]
+    
+This script will generate HR diagram tracks for 
+stars with specified a masses.
+"""
+
+def simulate_evolution_tracks(
+    stellar_evolution,
+    masses = [0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0] | units.MSun,
+    name_of_the_figure = "HR_evolution_tracks.png"
+    ):
     """
     For every mass in the `masses' array, a stellar evolution track across the Hertzsprung-Russell
     diagram will be calculated and plotted. Each star will be created, evolved and removed one by 
@@ -31,25 +40,14 @@ def simulate_evolution_tracks(masses = [0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0] | 
     all_tracks_luminosity = []
     all_tracks_temperature = []
     all_tracks_stellar_type = []
-    
-    if stellar_evolution_code == 1:
-        stellar_evolution = SSE()
-    elif stellar_evolution_code == 2:
-        stellar_evolution = EVtwin()
-    elif stellar_evolution_code == 3:
-        stellar_evolution = MESA()
-    else:
-        print "Unknown stellar_evolution_code: ", stellar_evolution_code
-        return
     stellar_evolution.initialize_module_with_current_parameters() 
     
     print "The evolution across the Hertzsprung-Russell diagram of ", str(number_of_stars), \
         " stars with\nvarying masses will be simulated..."
     
     for j in range(number_of_stars):
-        star=core.Particle()
+        star = core.Particle()
         star.mass = masses[j]
-        star.radius = 0.0 | units.RSun
         print "Created new star with mass: ", star.mass
 
         star = stellar_evolution.particles.add_particle(star)
@@ -78,8 +76,8 @@ def simulate_evolution_tracks(masses = [0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0] | 
             stellar_type_at_time.append(star.stellar_type)
             # Fudged: final stellar type annotation at previous (Teff, L);
             # BHs and neutron stars would otherwise fall off the chart.
-            luminosity_at_time.append(luminosity_at_time[len(luminosity_at_time)-1])
-            temperature_at_time.append(temperature_at_time[len(temperature_at_time)-1])
+            luminosity_at_time.append(luminosity_at_time[-1])
+            temperature_at_time.append(temperature_at_time[-1])
         print " ... evolved model to t = " + str(star.age.as_quantity_in(units.Myr))
         print "Star has now become a: ", star.stellar_type, "(stellar_type: "+str(star.stellar_type.value_in(units.stellar_type))+")"
         print
@@ -90,30 +88,19 @@ def simulate_evolution_tracks(masses = [0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0] | 
 #       Remove the star before creating the next one. See comments at the top.
         stellar_evolution.particles.remove_particle(star)
 
-    stellar_evolution.print_refs()
     stellar_evolution.stop()
     
-    plot_HR_diagram(number_of_stars, masses, all_tracks_luminosity, all_tracks_temperature, 
-        all_tracks_stellar_type, name_of_the_figure, stellar_evolution_code)
+    plot_HR_diagram(masses, all_tracks_luminosity, all_tracks_temperature, 
+        all_tracks_stellar_type, name_of_the_figure)
     
-    
+    print "All done!"        
 
-def plot_HR_diagram(number_of_stars, masses, all_tracks_luminosity, all_tracks_temperature, 
-        all_tracks_stellar_type, name_of_the_figure, stellar_evolution_code):
-    if HAS_MATPLOTLIB:
+def plot_HR_diagram(masses, luminosity_tracks, temperature_tracks, stellar_type_tracks, plotfile):
+    try:
+        from matplotlib import pyplot
         print "Plotting the data..."
         pyplot.figure(figsize = (7, 8))
-        pyplot.suptitle('Hertzsprung-Russell diagram', fontsize=16)
-        if stellar_evolution_code == 1:
-            pyplot.title('Evolutionary tracks were simulated using the SSE package\n'
-                '(Hurley J.R., Pols O.R., Tout C.A., 2000, MNRAS, 315, 543)', 
-                fontsize=12)
-        elif stellar_evolution_code == 2:
-            pyplot.title('Evolutionary tracks were simulated using the EVtwin package', 
-                fontsize=12)
-        elif stellar_evolution_code == 3:
-            pyplot.title('Evolutionary tracks were simulated using the MESA package', 
-                fontsize=12)
+        pyplot.title('Hertzsprung-Russell diagram', fontsize=12)
         pyplot.xlabel('Effective Temperature (K)')
         pyplot.ylabel('Luminosity (solar luminosity)')
 
@@ -123,10 +110,11 @@ def plot_HR_diagram(number_of_stars, masses, all_tracks_luminosity, all_tracks_t
         plot_format_strings_symbols=["r^","y^","c^","b^","m^","rs","ys","cs","bs","ms"]
         len_fmt_str_sym=len(plot_format_strings_symbols)
         
+        number_of_stars = len(masses)
         for j in range(number_of_stars):
 #           Plot track of the current star j
-            x_values = all_tracks_temperature[j].value_in(units.K)
-            y_values = all_tracks_luminosity[j].value_in(units.LSun)
+            x_values = temperature_tracks[j].value_in(units.K)
+            y_values = luminosity_tracks[j].value_in(units.LSun)
             pyplot.loglog(x_values, y_values, plot_format_strings_lines[j%len_fmt_str_lin])
             
 #           Plot symbols whenever this star has switched to the next stellar evolution phase
@@ -134,11 +122,11 @@ def plot_HR_diagram(number_of_stars, masses, all_tracks_luminosity, all_tracks_t
             y_values = []
             text_values = []
             previous_type = 15 | units.stellar_type
-            for i, type in enumerate(all_tracks_stellar_type[j]):
+            for i, type in enumerate(stellar_type_tracks[j]):
                 if not type == previous_type:
-                    x_values.append(all_tracks_temperature[j][i].value_in(units.K))
-                    y_values.append(all_tracks_luminosity[j][i].value_in(units.LSun))
-                    text_values.append(all_tracks_stellar_type[j][i].value_in(units.stellar_type))
+                    x_values.append(temperature_tracks[j][i].value_in(units.K))
+                    y_values.append(luminosity_tracks[j][i].value_in(units.LSun))
+                    text_values.append(stellar_type_tracks[j][i].value_in(units.stellar_type))
                     previous_type = type
 
             pyplot.loglog(x_values, y_values, plot_format_strings_symbols[j%len_fmt_str_sym])
@@ -156,41 +144,86 @@ def plot_HR_diagram(number_of_stars, masses, all_tracks_luminosity, all_tracks_t
         pyplot.axis([300000., 2500., 1.e-2, 1.e6])
 #       Or use these axes to also view neutron stars and black holes:
 #        pyplot.axis([1.e7, 2500., 1.e-11, 1.e6])
-        pyplot.savefig(name_of_the_figure)
-        
-        print
+        pyplot.savefig(plotfile)
         print "Meaning of the stellar evolution phase markers (black numbers):"
         for i in range(16):
-            print str(i)+": ", (i | units.stellar_type)
+            print str(i)+": ", (i | units.stellar_type)        
+    except ImportError:
+        print "Unable to produce plot: couldn't find matplotlib."
+
+class InstantiateCode(object):
+    
+    def sse(self, number_of_stars):
+        return SSE()
+    
+    def evtwin(self, number_of_stars):
+        result = EVtwin()
+        if number_of_stars > result.parameters.maximum_number_of_stars:
+            result.parameters.maximum_number_of_stars = number_of_stars
+            warnings.warn("You're simulating a large number of stars with EVtwin. This may not be such a good idea...")
+        return result
         
-    print
-    print "All done!"        
+    def mesa(self, number_of_stars):
+        result = MESA()
+        if number_of_stars > (10 | units.none):
+            warnings.warn("You're simulating a large number of stars with MESA. This may not be such a good idea...")
+        if number_of_stars > (1000| units.none):
+            raise Exception("You want to simulate with more than 1000 stars using MESA, this is not supported")
+        return result
+    
+    def new_code(self, name_of_the_code, number_of_stars):
+        if hasattr(self, name_of_the_code):
+            return getattr(self, name_of_the_code)(number_of_stars)
+        else:
+            raise Exception("Cannot instantiate code with name '{0}'".format(name_of_the_code))
+
+def new_code(name_of_the_code, number_of_stars):
+    return InstantiateCode().new_code(name_of_the_code, number_of_stars)
 
 def test_simulate_one_star():
     assert is_mpd_running()
+    code = new_code("sse", 1 | units.none)
     test_results_path = path_to_test_results.get_path_to_test_results()
     output_file = os.path.join(test_results_path, "HR_evolution_tracks.png")
-    simulate_evolution_tracks([20.0] | units.MSun, name_of_the_figure=output_file, 
-        stellar_evolution_code = 1)
-    
+    simulate_evolution_tracks(
+        code,
+        [20.0] | units.MSun,
+        name_of_the_figure=output_file, 
+    )
+
+def new_commandline_option_parser():
+    result = OptionParser(usage)
+    result.add_option(
+        "-c",
+        "--code",
+        choices=["sse", "evtwin","mesa"],
+        default="sse",
+        dest="code",
+        metavar="CODE",
+        help="CODE to use for stellar evolution"
+    )
+    result.add_option(
+        "-p",
+        "--plot_file",
+        type="string",
+        default="cluster_HR_diagram.png",
+        dest="plot_filename",
+        help="Name of the file to plot to"
+    )
+    return result
+
 if __name__ == '__main__':
     if not is_mpd_running():
         print "There is no mpd server running. Please do 'mpd &' first."
         sys.exit()
-    if len(sys.argv) == 1:
-        simulate_evolution_tracks()
-    elif len(sys.argv) == 2:
-        simulate_evolution_tracks(name_of_the_figure = sys.argv[1])
-    else:
-        if sys.argv[2] == "evtwin":
-            simulate_evolution_tracks(name_of_the_figure = sys.argv[1], 
-                stellar_evolution_code = 2)
-        elif sys.argv[2] == "mesa":
-            simulate_evolution_tracks(name_of_the_figure = sys.argv[1], 
-                stellar_evolution_code = 3)
-        else:
-            print "Unknown option:", sys.argv[2]
-            print "For EVtwin use option ' evtwin'."
-            print "For MESA use option ' mesa'. Now using SSE..."
-            simulate_evolution_tracks(name_of_the_figure = sys.argv[1], 
-                stellar_evolution_code = 1)
+    parser = new_commandline_option_parser()
+    (options, arguments) = parser.parse_args()
+    if arguments:
+        parser.error("unknown arguments '{0}'".format(arguments))
+    mass_list = [0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 30.0] | units.MSun
+    code = new_code(options.code, len(mass_list) | units.none)
+    simulate_evolution_tracks(
+        code,
+        masses = mass_list,
+        name_of_the_figure=options.plot_filename
+    )
