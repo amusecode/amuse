06ed1c16657c0cf2af71b7dad0e0f696b70339ec
pelupes <pelupes@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-07-20 10:44:45 +0000

    return exact number of particles in case of random base_grid
    git-svn-id: file:///home/amuse/svn/amuse/trunk@1224 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/ext/evrard_test.py
+++ src/amuse/ext/evrard_test.py
@@ -47,11 +47,12 @@ class body_centered_grid_unit_cube(object):
 class uniform_unit_sphere(object):
   def __init__(self,targetN, base_grid=None):
     cube_sphere_ratio=4/3.*numpy.pi*0.5**3
-    self.targetN=targetN/cube_sphere_ratio
-    if base_grid is None: 
-      self.base_grid=uniform_random_unit_cube(self.targetN)
+    self.targetN=targetN
+    self.estimatedN=targetN/cube_sphere_ratio
+    if base_grid is None:
+      self.base_grid=uniform_random_unit_cube
     else:
-      self.base_grid=base_grid(self.targetN)
+      self.base_grid=base_grid
  
   def cutout_sphere(self,x,y,z):
     r=x**2+y**2+z**2
@@ -62,7 +63,15 @@ class uniform_unit_sphere(object):
     return x,y,z
 
   def make_xyz(self):
-    return self.cutout_sphere(*self.base_grid.make_xyz())
+    if(self.base_grid==uniform_random_unit_cube):
+      estimatedN=self.estimatedN
+      x=[]
+      while len(x) < self.targetN:
+        estimadedN=estimatedN*1.1+1
+        x,y,z=self.cutout_sphere(*(self.base_grid(estimatedN)).make_xyz())
+      return x[0:self.targetN],y[0:self.targetN],z[0:self.targetN]  
+    else:
+      return self.cutout_sphere(*(self.base_grid(self.estimatedN)).make_xyz())
         
 class MakeEvrardTest(object):
     def __init__(self, targetN, base_grid=None, size=1.,
@@ -156,3 +165,8 @@ that the potential energy is -0.5 in nbody-units.
 def new_evrard_gas_sphere(target_number_of_particles, *list_arguments, **keyword_arguments):
     uc = MakeEvrardModel(target_number_of_particles, *list_arguments, **keyword_arguments)
     return uc.result
+
+
+if __name__=="__main__":
+  x,y,z=uniform_unit_sphere(10000).make_xyz()
+  print len(x)
