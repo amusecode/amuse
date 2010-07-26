b7a3257f002afdc47ca9448a6473b299134009c6
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2010-07-26 08:20:53 +0000

    fixed two tests
    git-svn-id: file:///home/amuse/svn/amuse/trunk@1227 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/ext_tests/test_evrardmodel.py
+++ test/ext_tests/test_evrardmodel.py
@@ -9,13 +9,13 @@ class TestEvrardModel(amusetest.TestCase):
         print "First test: making an Evrard gas sphere model."
         target_number_of_particles = 1000
         gas_parts = new_evrard_gas_sphere(target_number_of_particles, seed=1234)
-        self.assertEquals(len(gas_parts), 978)
+        self.assertEquals(len(gas_parts), 1000)
     
     def test2(self):
         print "Testing properties of an Evrard model."
         target_number_of_particles = 1000
         gas_parts = new_evrard_gas_sphere(target_number_of_particles, do_scale=True, seed=1234)
-        self.assertEquals(len(gas_parts), 978)
+        self.assertEquals(len(gas_parts), 1000)
         self.assertAlmostEqual(gas_parts.kinetic_energy(),             0.00 | nbody.energy)
         self.assertAlmostEqual(gas_parts.potential_energy(G=nbody.G), -0.50 | nbody.energy)
         self.assertAlmostEqual(gas_parts.center_of_mass(),          [0,0,0] | nbody.length)
