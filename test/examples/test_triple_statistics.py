ecf7082586917b8fb3568dde3bf9e2350c7a8f98
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-01-25 16:11:23 +0000

    still bug in triple, codes don't startup
    git-svn-id: file:///home/amuse/svn/amuse/trunk@1758 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/examples/test_triple_statistics.py
+++ test/examples/test_triple_statistics.py
@@ -92,7 +92,6 @@ class SimulateTripleSystemUntilDecay(object):
             self.from_code_to_model_channel.copy()
             has_escaper = self.has_triple_an_escaper(self.particles)
             self.store_point_positions()
-            print self.time
             
         if self.time >= self.tmax:
             self.make_plot()
@@ -145,7 +144,9 @@ def calculate_escape_time(index, number_of_systems, queue):
     print "start of subprocess", index
     try:
         for x in range(number_of_systems):
-            code = SimulateTripleSystemUntilDecay(Hermite(), new_initial_system())
+            gravity = Hermite()
+            print "code instantiated for", index
+            code = SimulateTripleSystemUntilDecay(gravity, new_initial_system())
             tend = code.evolve_model_until_escape()
             code.stop()
             queue.put(tend)
