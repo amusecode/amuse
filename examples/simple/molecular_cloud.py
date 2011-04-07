5074d962b5ba11014074f3aee10a248afc6de2bb
pelupes <pelupes@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-04-07 09:48:06 +0000

    cleaned up of molecular cloud example
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2042 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- examples/simple/molecular_cloud.py
+++ examples/simple/molecular_cloud.py
@@ -1,7 +1,9 @@
 """
   example of molecular cloud evolution with explictly 
   split SPH and grav evolution
-  
+
+  Initial condition is a smooth spherical cloud with random velocities
+  as in Bonnell et al. (2003)  
   
 """  
 
@@ -20,11 +22,7 @@ from amuse.ext.evrard_test import body_centered_grid_unit_cube
 from amuse.ext.derived_grav_systems import copycat
 from amuse.ext.bridge import bridge
 
-nc=2
-nr=2
-nplot=nc*nr
-  
-def make_map(sph,fig,t,i,N=100,L=1):
+def make_map(sph,N=100,L=1):
 
     x,y=numpy.indices( ( N+1,N+1 ))
 
@@ -43,20 +41,16 @@ def make_map(sph,fig,t,i,N=100,L=1):
     vz=units.kms(vz)
 
     rho,rhovx,rhovy,rhovz,rhoe=sph.get_hydro_state_at_point(x,y,z,vx,vy,vz)
-
     rho=rho.reshape((N+1,N+1))
 
-    s=fig.add_subplot(nc,nr,i)
-    s.imshow(numpy.log10(1.e-5+rho.value_in(units.amu/units.cm**3)),
-        extent=[-L/2,L/2,-L/2,L/2],vmin=2,vmax=6)
-  
-  
+    return rho
+    
 def run_mc(N=5000,Mcloud=10000. | units.MSun,Rcloud=1. | units.parsec):
 
     conv = nbody_system.nbody_to_si(Mcloud,Rcloud)
 
-    dt=0.005 | units.Myr
-    tend=0.12 | units.Myr
+    dt=0.01 | units.Myr
+    tend=0.36 | units.Myr
 
     parts=molecular_cloud(targetN=N,convert_nbody=conv,
             base_grid=body_centered_grid_unit_cube).result
@@ -82,16 +76,25 @@ def run_mc(N=5000,Mcloud=10000. | units.MSun,Rcloud=1. | units.parsec):
 
     fig=pyplot.figure(figsize=(12,12))
 
-    i=1
-    make_map(sph,fig,0.*tend,i,N=200,L=3)
+    ncolumn=2
+    nrow=2
+    nplot=ncolumn*nrow
+
+    i=0
+    L=3
     while i<nplot:
+      ttarget=i*tend/(nplot-1)
+      sys.evolve_model(ttarget,timestep=dt) 
+      rho=make_map(sph,N=200,L=L)
+      subplot=fig.add_subplot(ncolumn,nrow,i+1)
+      subplot.imshow(numpy.log10(1.e-5+rho.value_in(units.amu/units.cm**3)),
+        extent=[-L/2,L/2,-L/2,L/2],vmin=1,vmax=5)
+      subplot.set_title(ttarget.in_(units.Myr))
       i=i+1
-      sys.evolve_model((i-1)*tend/(nplot-1),timestep=dt) 
-      make_map(sph,fig,(i-1)*tend/(nplot-1),i,N=200,L=3)
 
     pyplot.show()
 #    pyplot.savefig('test.png')
   
 if __name__ in ["__main__","__plot__"]:
-    run_mc(10000,Mcloud=10000. | units.MSun,Rcloud=1. | units.parsec)
+    run_mc(10000,Mcloud=1000. | units.MSun,Rcloud=1. | units.parsec)
   
