a893eb88a039dcb50fed45dae0b310f4d1468304
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-04-07 14:33:51 +0000

    Ticket #195: Conversion of a hydrodynamics grid to SPH particles + tests
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2047 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/ext/grid_to_sph.py
@@ -0,0 +1,112 @@
+import numpy
+import pickle
+import os.path
+from amuse.support.data.core import Particles
+from amuse.support.units import units, constants
+from amuse.support.exceptions import AmuseWarning, AmuseException
+from amuse.ext.spherical_model import new_spherical_particle_distribution, get_enclosed_mass_from_tabulated, EnclosedMassInterpolator
+from amuse.community.gadget2.interface import Gadget2
+from amuse.support.units.generic_unit_converter import ConvertBetweenGenericAndSiUnits
+
+class Grid2SPH(object):
+    """
+    Converts a (cartesian) hydrodynamics Grid into an SPH model consisting of the 
+    specified number of particles. The Grid must have position, rho, momentum and
+    energy defined on each cell.
+    
+    grid, number_of_sph_particles, base_distribution_type = "uniform", seed = None):
+    :argument grid:  Star particle to be converted to an SPH model
+    :argument number_of_sph_particles:  Number of gas particles in the resulting model
+    :argument base_distribution_type:  Type of the base particle distribution ("random" or "uniform")
+        "random":  particle positions are randomly sampled from the density weighted cells
+        "uniform":  particles are uniformly sampled from the density weighted cells (note
+            that they are assigned a random (!) position within that cell subsequently)
+    :argument seed:  If provided, seed for the random number generator
+    """
+    
+    def __init__(self, grid, number_of_sph_particles, base_distribution_type = "uniform", seed = None):
+        if (grid.number_of_dimensions() != 3):
+            raise AmuseException("Grid must be 3D")
+        
+        self.grid = grid
+        self.shape = grid.shape
+        self.number_of_sph_particles = number_of_sph_particles
+        self.base_distribution_type = base_distribution_type # "random" or "uniform"
+        if seed:
+            numpy.random.seed(seed)
+    
+    def setup_lookup_tables(self):
+        # Retrieve details of the grid and convert them to fast lookup tables
+        shape_for_vector_multiply = list(self.grid.shape)
+        shape_for_vector_multiply.append(1)
+        density = self.grid.rho
+        summed_density = density.sum()
+        self.cumulative_weight = numpy.cumsum((density / summed_density).value_in(units.none))
+        self.position_lookup_table = self.grid.position.reshape((-1,3))
+        self.velocity_lookup_table = (self.grid.momentum / density.reshape(shape_for_vector_multiply)).reshape((-1,3))
+        self.specific_internal_energy_lookup_table = (self.grid.energy / density).flatten()
+        cellsize = self.grid.cellsize()
+        self.cellsize_unit = cellsize.unit
+        self.cellsize_number = cellsize.value_in(cellsize.unit)
+        self.mass = summed_density * (cellsize[0] * cellsize[1] * cellsize[2])
+    
+    def setup_variates(self):
+        # Generate (quasi-)random realisation
+        variates = self.generate_variates(self.number_of_sph_particles)
+        self.indices = numpy.searchsorted(self.cumulative_weight, variates)
+    
+    def generate_variates(self, number_of_variates):
+        if self.base_distribution_type == "uniform":
+            return numpy.linspace(0.0, 1.0, num=number_of_variates, endpoint=False)
+        elif self.base_distribution_type == "random":
+            return numpy.random.uniform(0.0, 1.0, number_of_variates)
+        else:
+            raise AmuseException("Unknown base_distribution_type: {0}. Possible "
+                "options are: 'random' or 'uniform'.".format(self.base_distribution_type))
+    
+    def new_particle_positions(self):
+        base_positions = self.position_lookup_table[self.indices]
+        return base_positions + self.cellsize_unit.new_quantity(
+            self.cellsize_number * numpy.random.uniform(-0.5, 0.5, (self.number_of_sph_particles, 3)))
+    
+    def new_particle_velocities(self):
+        return self.velocity_lookup_table[self.indices]
+    
+    def new_particle_specific_internal_energies(self):
+        return self.specific_internal_energy_lookup_table[self.indices]
+    
+    @property
+    def result(self):
+        self.setup_lookup_tables()
+        self.setup_variates()
+        
+        sph_particles = Particles(self.number_of_sph_particles)
+        sph_particles.position = self.new_particle_positions()
+        sph_particles.velocity = self.new_particle_velocities()
+        sph_particles.u        = self.new_particle_specific_internal_energies()
+        
+        sph_particles.mass = (self.mass.number * 1.0 / self.number_of_sph_particles) | self.mass.unit
+        # Crude estimate of the smoothing length; the SPH code will calculate the true value itself.
+        sph_particles.h_smooth = (self.grid.get_volume() * 50.0/self.number_of_sph_particles)**(1/3.0)
+        
+        return sph_particles
+
+    
+
+def convert_grid_to_SPH(grid, number_of_sph_particles, **keyword_arguments):
+    """
+    Converts a (cartesian) hydrodynamics Grid into an SPH model consisting of the 
+    specified number of particles. The Grid must have position, rho, momentum and
+    energy defined on each cell.
+    
+    grid, number_of_sph_particles, base_distribution_type = "uniform", seed = None):
+    :argument grid:  Star particle to be converted to an SPH model
+    :argument number_of_sph_particles:  Number of gas particles in the resulting model
+    :argument base_distribution_type:  Type of the base particle distribution ("random" or "uniform")
+        "random":  particle positions are randomly sampled from the density weighted cells
+        "uniform":  particles are uniformly sampled from the density weighted cells (note
+            that they are assigned a random (!) position within that cell subsequently)
+    :argument seed:  If provided, seed for the random number generator
+    """
+    converter = Grid2SPH(grid, number_of_sph_particles, **keyword_arguments)
+    return converter.result
---
+++ test/ext_tests/test_grid_to_sph.py
@@ -0,0 +1,87 @@
+import os.path
+import numpy
+from amuse.test.amusetest import get_path_to_results, TestWithMPI
+try:
+    from matplotlib import pyplot
+    HAS_MATPLOTLIB = True
+    from amuse.plot import plot, semilogy, xlabel, ylabel, loglog
+except ImportError:
+    HAS_MATPLOTLIB = False
+
+from amuse.support.data.core import Particles, Particle, ParticlesSuperset, Grid
+from amuse.support.units import units, generic_unit_system, nbody_system, constants
+from amuse.support.units.generic_unit_converter import ConvertBetweenGenericAndSiUnits
+from amuse.support.exceptions import AmuseException
+from amuse.community.gadget2.interface import Gadget2
+from amuse.community.fi.interface import Fi
+from amuse.ext.grid_to_sph import Grid2SPH, convert_grid_to_SPH
+
+
+class TestGrid2SPH(TestWithMPI):
+    
+    def setup_simple_grid(self):
+        test_grid = Grid.create((4,3,2), [1.0, 1.0, 1.0] | units.m)
+        test_grid.rho = numpy.linspace(1.0, 2.0, num=24).reshape(test_grid.shape) | units.kg/units.m**3
+        test_grid.rhovx = test_grid.rho * (3.0 | units.m/units.s)
+        test_grid.rhovy = test_grid.rho * (4.0 | units.m/units.s)
+        test_grid.rhovz = test_grid.rho * (0.0 | units.m/units.s)
+        test_grid.energy = test_grid.rho * (1.0 | (units.m/units.s)**2)
+        return test_grid
+    
+    def test0(self):
+        print "Testing the simple example grid"
+        test_grid = self.setup_simple_grid()
+        self.assertEqual(test_grid.position[ 0][ 0][ 0],  [1.0/8.0, 1.0/6.0, 1.0/4.0] | units.m)
+        self.assertEqual(test_grid.position[-1][-1][-1],  [7.0/8.0, 5.0/6.0, 3.0/4.0] | units.m)
+        self.assertEqual(test_grid.momentum[ 0][ 0][ 0],  [3.0, 4.0, 0.0] | (units.kg/units.m**3) * (units.m/units.s))
+        self.assertEqual(test_grid.momentum[-1][-1][-1],  [6.0, 8.0, 0.0] | (units.kg/units.m**3) * (units.m/units.s))
+        self.assertEqual(test_grid.energy[ 0][ 0][ 0],  1.0 | (units.J/units.m**3))
+        self.assertEqual(test_grid.energy[-1][-1][-1],  2.0 | (units.J/units.m**3))
+        
+    def test1(self):
+        print "Testing the converter"
+        number_of_particles = 10000
+        test_grid = self.setup_simple_grid()
+        converter = Grid2SPH(test_grid, number_of_particles)
+        self.assertTrue(converter.grid is test_grid)
+        self.assertEqual(converter.shape, (4,3,2))
+        self.assertEqual(converter.number_of_sph_particles, number_of_particles)
+        self.assertEqual(converter.base_distribution_type, "uniform")
+        
+        converter.setup_lookup_tables()
+        converter.setup_variates()
+        self.assertEqual(converter.cumulative_weight[0],  1.0/(1.5*4*3*2))
+        self.assertEqual(converter.cumulative_weight[-1], 1.0)
+        self.assertEqual(converter.position_lookup_table[0],  [1.0/8.0, 1.0/6.0, 1.0/4.0] | units.m)
+        self.assertEqual(converter.position_lookup_table[-1], [7.0/8.0, 5.0/6.0, 3.0/4.0] | units.m)
+        self.assertEqual(converter.position_lookup_table[9],  [3.0/8.0, 3.0/6.0, 3.0/4.0] | units.m)
+        self.assertAlmostEqual(converter.velocity_lookup_table,  [3.0, 4.0, 0.0] | units.m/units.s)
+        self.assertEqual(converter.specific_internal_energy_lookup_table,  1.0 | units.J/units.kg)
+        self.assertEqual(converter.cellsize_unit,  units.m)
+        self.assertTrue(converter.cellsize_unit is units.m)
+        self.assertAlmostEqual(converter.cellsize_number, [0.25, 1/3.0, 0.5])
+        self.assertAlmostEqual(converter.mass, 1.5 | units.kg)
+        # The number of particles in a cell should scale with the amount of mass in the cell:
+        self.assertAlmostRelativeEqual(
+            converter.mass * numpy.histogram(converter.indices, bins=4*3*2)[0] * 1.0/number_of_particles, 
+            test_grid.rho.flatten()*test_grid.cellsize().prod(), 
+            places = 2
+        )
+    
+    def test2(self):
+        print "Testing the user interface"
+        number_of_particles = 10000
+        test_grid = self.setup_simple_grid()
+        sph_particles = convert_grid_to_SPH(test_grid, number_of_particles)
+        self.assertEqual(len(sph_particles), number_of_particles)
+        self.assertAlmostEqual(sph_particles.mass.sum(),  1.5 | units.kg)
+        self.assertAlmostEqual(sph_particles.velocity,  [3.0, 4.0, 0.0] | units.m/units.s)
+        self.assertAlmostEqual(sph_particles.u,  1.0 | (units.m/units.s)**2)
+        # The number of particles in a cell should scale with the amount of mass in the cell:
+        self.assertAlmostRelativeEqual(
+            (1.5 | units.kg)/number_of_particles * numpy.histogramdd(sph_particles.position.value_in(units.m), bins=(4,3,2))[0], 
+            test_grid.rho*test_grid.cellsize().prod(), 
+            places = 2
+        )
+        self.assertAlmostEqual(sph_particles.h_smooth,  (50.0/number_of_particles)**(1.0/3) | units.m)
+    
