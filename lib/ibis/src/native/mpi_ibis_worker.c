93f113d4895e6bf9b0389431430ef7ca62bcf843
niels <niels@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-04-08 14:46:18 +0000

    new mpi ibis worker (doesn't work though)
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2051 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- lib/ibis/src/native/mpi_ibis_worker.c
+++ lib/ibis/src/native/mpi_ibis_worker.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dlfcn.h>
+#include <unistd.h>
 
 #include "ibis_amuse_CommunityCode.h"
 
@@ -157,6 +158,8 @@ JNIEXPORT void JNICALL Java_ibis_amuse_CommunityCode_init(JNIEnv *env,
 			MPI_ARGV_NULL, 1, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercom,
 			MPI_ERRCODES_IGNORE);
 
+	fprintf(stderr, "mpi_ibis_worker.c: size of int = %d\n", sizeof(int));
+
 }
 
 /*
@@ -215,18 +218,29 @@ JNIEXPORT void JNICALL Java_ibis_amuse_CommunityCode_init(JNIEnv *env,
  * Signature: ()V
  */JNIEXPORT void JNICALL Java_ibis_amuse_CommunityCode_call(JNIEnv *env,
 		jobject this_object) {
+	int result;
+	int return_code;
+	MPI_Status status;
 	char error[100];
 	jclass exception_class;
 	int call_count = header_in[HEADER_CALL_COUNT_INDEX];
 	int mpi_header[8];
 
 	fprintf(stderr,
-			"Amuse/Ibis/MPI native code: doing call, function ID = %d\n",
-			header_in[HEADER_FUNCTION_ID_INDEX]);
+			"Amuse/Ibis/MPI native code: doing call, function ID = %d, count = %d\n",
+			header_in[HEADER_FUNCTION_ID_INDEX], call_count);
 
 	header_out[HEADER_CALL_ID_INDEX] = header_in[HEADER_CALL_ID_INDEX];
 	header_out[HEADER_FUNCTION_ID_INDEX] = header_in[HEADER_FUNCTION_ID_INDEX];
 	header_out[HEADER_CALL_COUNT_INDEX] = header_in[HEADER_CALL_COUNT_INDEX];
+
+	//we do not support the "redirect output" function, simply pretend it worked...
+	if (header_in[HEADER_FUNCTION_ID_INDEX] == 1141573512) {
+		header_out[HEADER_INT_COUNT_INDEX] = 1;
+		ensure_primitive_output_capacity(env);
+		ints_out[0] = 0;
+		return;
+	}
 
 	mpi_header[0] = header_in[HEADER_FUNCTION_ID_INDEX];
 	mpi_header[1] = header_in[HEADER_CALL_COUNT_INDEX];
@@ -237,10 +251,168 @@ JNIEXPORT void JNICALL Java_ibis_amuse_CommunityCode_init(JNIEnv *env,
 	mpi_header[6] = header_in[HEADER_BOOLEAN_COUNT_INDEX] / call_count;
 	mpi_header[7] = header_in[HEADER_LONG_COUNT_INDEX] / call_count;
 
+	fprintf(stderr, "sending header\n");
 	MPI_Bcast(mpi_header, 8, MPI_INT, MPI_ROOT, intercom);
 
 
-	//	Send(data, 10, MPI_INT, 0, 0, intercom);
+	if (header_in[HEADER_DOUBLE_COUNT_INDEX] > 0) {
+		fprintf(stderr, "sending doubles\n");
+		MPI_Bcast(doubles_in, header_in[HEADER_DOUBLE_COUNT_INDEX], MPI_DOUBLE,
+				MPI_ROOT, intercom);
+	}
+
+	if (header_in[HEADER_INT_COUNT_INDEX] > 0) {
+		fprintf(stderr, "sending ints\n");
+		MPI_Bcast(ints_in, header_in[HEADER_INT_COUNT_INDEX], MPI_INT,
+				MPI_ROOT, intercom);
+	}
+
+	if (header_in[HEADER_FLOAT_COUNT_INDEX] > 0) {
+		fprintf(stderr, "sending floats\n");
+		MPI_Bcast(floats_in, header_in[HEADER_FLOAT_COUNT_INDEX], MPI_FLOAT,
+				MPI_ROOT, intercom);
+	}
+
+	if (header_in[HEADER_STRING_COUNT_INDEX] > 0) {
+		//TODO: something with strings!
+		MPI_Bcast(ints_in, 0, MPI_INT, MPI_ROOT, intercom);
+		MPI_Bcast(booleans_in, 0, MPI_CHARACTER, MPI_ROOT, intercom);
+
+		jclass exception_class = (*env)->FindClass(env, "java/lang/Exception");
+		sprintf(error, "eep! a string!");
+		if (exception_class != NULL) {
+			(*env)->ThrowNew(env, exception_class, error);
+		}
+		return;
+	}
+
+	if (header_in[HEADER_BOOLEAN_COUNT_INDEX] > 0) {
+		fprintf(stderr, "sending booleans");
+
+		MPI_Bcast(booleans_in, header_in[HEADER_BOOLEAN_COUNT_INDEX],
+				MPI_LOGICAL, MPI_ROOT, intercom);
+	}
+
+	if (header_in[HEADER_LONG_COUNT_INDEX] > 0) {
+		fprintf(stderr, "sending longs");
+
+		MPI_Bcast(longs_in, header_in[HEADER_LONG_COUNT_INDEX], MPI_INTEGER8,
+				MPI_ROOT, intercom);
+	}
+
+	//now wait for result...
+
+	fprintf(stderr, "Amuse/Ibis/MPI native code: waiting for result of function %d\n",
+			header_in[HEADER_FUNCTION_ID_INDEX]);
+
+	mpi_header[4] = 57575;
+
+	result = MPI_Recv(mpi_header, 8, MPI_INTEGER4, 0, 999, intercom, &status);
+
+	if (result != MPI_SUCCESS) {
+		fprintf(stderr, "error in mpi call = %d\n", result);
+
+	} else {
+		fprintf(stderr, "received result %d %d %d %d %d %d %d %d\n", mpi_header[0], mpi_header[1], mpi_header[2], mpi_header[3], mpi_header[4], mpi_header[5], mpi_header[6], mpi_header[7]);
+	}
+
+	return_code = mpi_header[0]; //-1 and -2 for error
+
+	header_out[HEADER_DOUBLE_COUNT_INDEX] = mpi_header[2] * call_count;
+	header_out[HEADER_INT_COUNT_INDEX] = mpi_header[3] * call_count;
+	header_out[HEADER_FLOAT_COUNT_INDEX] = mpi_header[4] * call_count;
+	header_out[HEADER_STRING_COUNT_INDEX] = mpi_header[5] * call_count;
+	header_out[HEADER_BOOLEAN_COUNT_INDEX] = mpi_header[6] * call_count;
+	header_out[HEADER_LONG_COUNT_INDEX] = mpi_header[7] * call_count;
+
+	print_object(env, "got header, ensuring capacity of ", message_out);
+
+	//ensure_primitive_output_capacity(env);
+
+	//print_object(env, "got header, receiving data for ", message_out);
+
+//	if (header_out[HEADER_DOUBLE_COUNT_INDEX] > 0) {
+//		fprintf(stderr, "Amuse/Ibis/MPI native code: receiving %d doubles\n",
+//				header_out[HEADER_DOUBLE_COUNT_INDEX]);
+//
+//		result = MPI_Recv(&doubles_out, header_out[HEADER_DOUBLE_COUNT_INDEX],
+//				MPI_DOUBLE, 0, 999, intercom, NULL);
+//
+//		if (result != MPI_SUCCESS) {
+//			fprintf(stderr, "error in mpi call = %d\n", result);
+//
+//		}
+//
+//	}
+//
+//	fprintf(stderr, "size of MPI_INT = %d ", sizeof(MPI_INT) );
+//
+//	if (header_out[HEADER_INT_COUNT_INDEX] > 0) {
+//		fprintf(stderr, "Amuse/Ibis/MPI native code: receiving %d ints\n",
+//				header_out[HEADER_INT_COUNT_INDEX]);
+
+		int array[1];
+
+		result = MPI_Recv(array, 1, MPI_INTEGER4, 0, 999, intercom, &status);
+
+		if (result != MPI_SUCCESS) {
+					fprintf(stderr, "error in mpi call = %d\n", result);
+				}
+
+		fprintf(stderr, "Amuse/Ibis/MPI native code: received!! %d ints\n",
+				header_out[HEADER_INT_COUNT_INDEX]);
+
+//	}
+
+	if (header_out[HEADER_FLOAT_COUNT_INDEX] > 0) {
+		fprintf(stderr, "Amuse/Ibis/MPI native code: receiving %d floats\n",
+				header_out[HEADER_FLOAT_COUNT_INDEX]);
+
+		MPI_Recv(floats_out, header_out[HEADER_FLOAT_COUNT_INDEX], MPI_FLOAT,
+				0, 999, intercom, NULL);
+	}
+
+	if (header_out[HEADER_STRING_COUNT_INDEX] > 0) {
+		fprintf(stderr, "Amuse/Ibis/MPI native code: eep! strings!\n");
+
+		//TODO: something with strings!
+		return_code = -3;
+	}
+
+	if (header_out[HEADER_BOOLEAN_COUNT_INDEX] > 0) {
+		fprintf(stderr, "Amuse/Ibis/MPI native code: receiving %d boolenas\n",
+				header_out[HEADER_BOOLEAN_COUNT_INDEX]);
+
+		MPI_Recv(booleans_out, header_out[HEADER_BOOLEAN_COUNT_INDEX],
+				MPI_LOGICAL, 0, 999, intercom, NULL);
+	}
+
+	if (header_out[HEADER_LONG_COUNT_INDEX] > 0) {
+		fprintf(stderr, "Amuse/Ibis/MPI native code: receiving %d longs\n",
+				header_out[HEADER_LONG_COUNT_INDEX]);
+
+		MPI_Recv(longs_out, header_out[HEADER_LONG_COUNT_INDEX], MPI_INTEGER8,
+				0, 999, intercom, NULL);
+	}
+
+	fprintf(stderr, "checking for error in result\n");
+
+	if (return_code != header_in[HEADER_FUNCTION_ID_INDEX]) {
+		jclass exception_class = (*env)->FindClass(env, "java/lang/Exception");
+		sprintf(error, "error %d in calling code function %id in call %d",
+				return_code,
+				header_in[HEADER_FUNCTION_ID_INDEX],
+				header_in[HEADER_CALL_ID_INDEX]);
+
+		if (exception_class != NULL) {
+			(*env)->ThrowNew(env, exception_class, error);
+		}
+		return;
+	}
+
+	fprintf(stderr, "done!\n");
+
+	///  comm.Recv(array,  source=0, tag=999)
 
 	//	MPI_Comm_disconnect(&intercom);
 
