b6405cfb9168add2b61725ac1c222ee875214092
pelupes <pelupes@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-04-13 11:46:26 +0000

    protoplanetary disk initial condition + simple example
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2067 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ examples/simple/protoplanetarydisk.py
@@ -0,0 +1,86 @@
+"""
+  simple example of setting up a protoplanetary disk around a sun-like star
+
+"""
+
+
+import numpy
+
+from matplotlib import pyplot
+
+from amuse.community.fi.interface import Fi
+
+from amuse.support.units import units
+from amuse.support.units import nbody_system
+from amuse.support.data.core import Particles
+
+from amuse.ext.protodisk import ProtoPlanetaryDisk
+
+def make_map(sph,N=100,L=1):
+
+    x,y=numpy.indices( ( N+1,N+1 ))
+
+    x=L*(x.flatten()-N/2.)/N
+    y=L*(y.flatten()-N/2.)/N
+    z=x*0.
+    vx=0.*x
+    vy=0.*x
+    vz=0.*x
+
+    x=units.AU(x)
+    y=units.AU(y)
+    z=units.AU(z)
+    vx=units.kms(vx)
+    vy=units.kms(vy)
+    vz=units.kms(vz)
+
+    rho,rhovx,rhovy,rhovz,rhoe=sph.get_hydro_state_at_point(x,y,z,vx,vy,vz)
+    rho=rho.reshape((N+1,N+1))
+
+    return rho
+                    
+if __name__ in ["__main__","__plot__"]:
+
+    N=10000
+    tend=150 | units.yr
+    
+    convert=nbody_system.nbody_to_si(1. | units.MSun, 1. | units.AU)
+    proto=ProtoPlanetaryDisk(N,convert_nbody=convert,densitypower=1.5,Rmin=4,Rmax=20,q_out=1.)
+    gas=proto.result
+    gas.h_smooth=0.5 | units.AU
+     
+    sun=Particles(1)
+    sun.mass=1. | units.MSun
+    sun.radius=2.5 | units.AU
+    sun.x=0.|units.AU
+    sun.y=0.|units.AU
+    sun.z=0.|units.AU
+    sun.vx=0.|units.kms
+    sun.vy=0.|units.kms
+    sun.vz=0.|units.kms
+ 
+    sph=Fi(convert)
+ 
+    sph.parameters.use_hydro_flag=True
+    sph.parameters.radiation_flag=False
+    sph.parameters.self_gravity_flag=True
+    sph.parameters.gamma=1.
+    sph.parameters.isothermal_flag=True
+    sph.parameters.integrate_entropy_flag=False
+    sph.parameters.timestep=0.125 | units.yr  
+
+    sph.gas_particles.add_particles(gas)
+    sph.particles.add_particles(sun)
+        
+    sph.evolve_model(tend)    
+            
+    L=50
+    rho=make_map(sph,N=200,L=L)
+    pyplot.figure(figsize=(8,8))
+    pyplot.imshow(numpy.log10(1.e-5+rho.value_in(units.amu/units.cm**3)),
+        extent=[-L/2,L/2,-L/2,L/2],vmin=10,vmax=15)    
+    pyplot.title(tend)
+    pyplot.xlabel('AU')
+#    pyplot.savefig('test.png')
+    pyplot.show()
+         
---
+++ src/amuse/ext/protodisk.py
@@ -0,0 +1,145 @@
+import numpy
+
+from matplotlib import pyplot
+
+from amuse.support.units import units
+from amuse.support.units import nbody_system
+from amuse.support.data.core import Particles, ParticlesWithUnitsConverted
+
+from amuse.ext.evrard_test import uniform_random_unit_cube,regular_grid_unit_cube,body_centered_grid_unit_cube
+
+def approximate_inverse_error_function(x):
+  a=8*(numpy.pi-3)/3*numpy.pi*(4-numpy.pi)
+  return numpy.sign(x)*numpy.sqrt(
+    numpy.sqrt((2/numpy.pi/a+numpy.log(1-x**2)/2)**2-numpy.log(1-x**2)/a)-(2/numpy.pi/a+numpy.log(1-x**2)/2)
+   )
+
+class uniform_unit_cylinder(object):
+    def __init__(self,targetN, base_grid=None):
+        cube_cylinder_ratio=numpy.pi*0.5**2
+        self.targetN=targetN
+        self.estimatedN=targetN/cube_cylinder_ratio
+        if base_grid is None:
+            self.base_grid=uniform_random_unit_cube
+        else:
+            self.base_grid=base_grid
+   
+    def cutout_cylinder(self,x,y,z):
+        r=x**2+y**2
+        selection=r < numpy.ones_like(r)        
+        x=x.compress(selection)
+        y=y.compress(selection)
+        z=z.compress(selection)
+        return x,y,z
+
+    def make_xyz(self):
+        if(self.base_grid==uniform_random_unit_cube):
+            estimatedN=self.estimatedN
+            x=[]
+            while len(x) < self.targetN:
+                estimadedN=estimatedN*1.1+1
+                x,y,z=self.cutout_cylinder(*(self.base_grid(estimatedN)).make_xyz())
+            return x[0:self.targetN],y[0:self.targetN],z[0:self.targetN]  
+        else:
+            return self.cutout_cylinder(*(self.base_grid(self.estimatedN)).make_xyz())
+
+
+class ProtoPlanetaryDisk(object):
+
+    def __init__(self, targetN, convert_nbody = None, discfraction=0.1,
+                   densitypower=1., thermalpower=0.5, Rmin=1,Rmax=100,
+                   gamma=5./3.,q_out=2.,base_grid=None):
+        self.targetN=targetN
+        self.convert_nbody=convert_nbody
+        self.densitypower=densitypower
+        self.thermalpower=thermalpower
+        self.Rmin=Rmin
+        self.Rmax=Rmax
+        self.gamma=gamma
+        self.q_out=q_out
+        self.discfraction=discfraction
+
+        self.a=self.thermalpower
+        self.a2=self.thermalpower/2
+        self.g=densitypower
+        self.g2=2-densitypower
+        self.k_out=((1+discfraction)/Rmax**3)**0.5
+        self.sigma_out=self.g2*discfraction/(2*numpy.pi*Rmax**self.g*(Rmax**self.g2-Rmin**self.g2))
+        self.cs_out=self.q_out*numpy.pi*self.sigma_out/self.k_out
+        
+        self.base_cylinder=uniform_unit_cylinder(targetN,base_grid)   
+
+
+    def sigma(self,r):
+        return self.sigma_out*(self.Rmax/r)**self.g
+
+    def csound(self,r):
+        return self.cs_out*(self.Rmax/r)**self.a2
+
+    def cmass(self,r):
+        return self.discfraction*(r**self.g2-self.Rmin**self.g2)/(self.Rmax**self.g2-self.Rmin**self.g2)
+
+    def mass_encl(self,r):
+        return 1+self.cmass(r)
+
+    def kappa(self,r):
+        return (self.mass_encl(r)/r**3)**0.5
+  
+    def toomreQ(self,r):
+        return self.csound(r)*self.kappa(r)/numpy.pi/self.sigma(r)
+
+    def getradius(self,f):
+        return ((self.Rmax**self.g2-self.Rmin**self.g2)*f+self.Rmin**self.g2)**(1./self.g2)
+
+    def zscale(self,r):
+        return self.csound(r)/self.kappa(r) 
+
+    def u(self,r):
+        return self.csound(r)**2/(self.gamma-1)
+
+    def vcirc(self,r):
+        return (self.mass_encl(r)/r)**0.5
+    
+    def new_model(self):
+        x,y,z=self.base_cylinder.make_xyz()
+        self.actualN=len(x)
+        f=x**2+y**2
+        r=f**0.5
+        rtarget=self.getradius(f)
+        
+        mass=self.discfraction*numpy.ones_like(x)/self.actualN
+        internal_energy=self.u(rtarget)
+        zscale=self.zscale(rtarget)
+        r=r.clip(1.e-8,2.)
+        x=x/r
+        y=y/r
+
+        vx=-y*self.vcirc(rtarget)
+        vy=x*self.vcirc(rtarget)
+        vz=numpy.zeros_like(x)
+         
+        x=rtarget*x 
+        y=rtarget*y                  
+        z=approximate_inverse_error_function(z)*zscale
+
+        return (mass, x, y, z, vx, vy, vz, internal_energy)
+  
+    @property
+    def result(self):
+        masses, x,y,z, vx,vy,vz, internal_energies = self.new_model()
+        result = Particles(self.actualN)
+        result.mass = nbody_system.mass.new_quantity(masses)
+        result.x = nbody_system.length.new_quantity(x)
+        result.y = nbody_system.length.new_quantity(y)
+        result.z = nbody_system.length.new_quantity(z)
+        result.vx = nbody_system.speed.new_quantity(vx)
+        result.vy = nbody_system.speed.new_quantity(vy)
+        result.vz = nbody_system.speed.new_quantity(vz)
+        result.u = nbody_system.specific_energy.new_quantity(internal_energies)
+                
+        if not self.convert_nbody is None:
+            result = ParticlesWithUnitsConverted(result, self.convert_nbody.as_converter_from_si_to_nbody())
+            result = result.copy_to_memory()
+            
+        return result
+
