92646377ad560adba715f54fc6873fd52a9709c9
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-05-26 12:33:26 +0000

    Fix: example script was executed during import
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2167 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- examples/applications/mercury_example.py
+++ examples/applications/mercury_example.py
@@ -10,33 +10,34 @@ except ImportError:
     HAS_MATPLOTLIB = False
 
 
-instance=MercuryInterface()
-instance.initialize_code()
-mass=3.04043264264672381E-06
-dens=5.52
-x=2.42093942183383037E-01
-y=-9.87467766698604366E-01
-z=-4.54276292555233496E-06
-vx=1.64294055023289365E-02
-vy=4.03200725816140870E-03
-vz=1.13609607260006795E-08
-sx=sy=sz=0.
-celimit=20.
-pid,err=instance.new_orbiter(mass,dens,x,y,z,vx,vy,vz,sx,sy,sz,celimit)  
-instance.commit_particles()
-
-t_end=365.25*1e6
-time=0
-xx=[x]
-yy=[y]
-
-while time<t_end:
-    time=time+365.25*1e3
-    err=instance.evolve(time)
-    mass,dens,x,y,z,vx,vy,vz,sx,sy,sz,celimit,err=instance.get_orbiter_state(pid)
-    print x, y	
-    xx.append(x)
-    yy.append(y)
-
-instance.stop()
+if __name__=='__main__':
+    instance=MercuryInterface()
+    instance.initialize_code()
+    mass=3.04043264264672381E-06
+    dens=5.52
+    x=2.42093942183383037E-01
+    y=-9.87467766698604366E-01
+    z=-4.54276292555233496E-06
+    vx=1.64294055023289365E-02
+    vy=4.03200725816140870E-03
+    vz=1.13609607260006795E-08
+    sx=sy=sz=0.
+    celimit=20.
+    pid,err=instance.new_orbiter(mass,dens,x,y,z,vx,vy,vz,sx,sy,sz,celimit)  
+    instance.commit_particles()
+    
+    t_end=365.25*1e6
+    time=0
+    xx=[x]
+    yy=[y]
+    
+    while time<t_end:
+        time=time+365.25*1e3
+        err=instance.evolve(time)
+        mass,dens,x,y,z,vx,vy,vz,sx,sy,sz,celimit,err=instance.get_orbiter_state(pid)
+        print x, y	
+        xx.append(x)
+        yy.append(y)
+    
+    instance.stop()
 
