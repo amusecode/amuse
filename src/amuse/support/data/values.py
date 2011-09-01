8eb8a33b83f6dbb7f28496cc7507ddab4f532946
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-09-01 14:26:15 +0000

    typo in values.py
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2450 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/support/data/values.py
+++ src/amuse/support/data/values.py
@@ -2,4 +2,4 @@ import warnings
 
 warnings.warn("amuse.support.data.values has moved to amuse.support.units.quantities, use amuse.support.units.quantities instead", DeprecationWarning)
 
-from amuse.support.units.qantities import *
+from amuse.support.units.quantities import *
