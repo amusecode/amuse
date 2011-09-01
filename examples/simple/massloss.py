b777aa02ab606ab9e1162805e5a03730ec25f318
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-09-01 14:41:40 +0000

    remove unused and broken import
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2452 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- examples/simple/massloss.py
+++ examples/simple/massloss.py
@@ -1,10 +1,9 @@
 import numpy
 from amuse.community.hermite0.interface import Hermite
 from amuse.community.sse.interface import SSE
-from amuse.ext.solarsystem import Solarsystem
 from amuse.support.units import units
 from amuse.support.units import nbody_system
-
+from amuse.support.units.quantities import VectorQuantity
 from amuse.support.data import core
 
 from amuse.plot import *
