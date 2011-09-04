0c0cc625184d53fc0c74eb9ce18a729c83e73801
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-09-04 17:16:17 +0000

    added imports to data package
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2459 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/support/data/__init__.py
@@ -0,0 +1,15 @@
+"""
+This module provides access to all set handling 
+in AMUSE. The actual implementation is in the
+base, storage and particle modules.
+"""
+
+from amuse.support.data.base import *
+from amuse.support.data.memory_storage import *
+from amuse.support.data.particles import *
+from amuse.support.data import particle_attributes
+from amuse.support.data.grids import *
+from amuse.support.data import grid_attributes
+
+
+
