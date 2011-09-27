50ba261ceb39477466dead83ca3d94d964caec40
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-09-27 09:51:55 +0000

    packages moved
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2551 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/core_tests/test_constants.py
+++ test/core_tests/test_constants.py
@@ -9,7 +9,7 @@ class Test(amusetest.TestCase):
     
     def generate_constants_if_necessary(self):
         try:
-            from amuse.support.units import constants
+            from amuse.units import constants
         except ImportError:
             I = nist.Constants()
             I.generate_constants()
@@ -27,19 +27,19 @@ class Test(amusetest.TestCase):
         
     def test3(self):
         self.generate_constants_if_necessary()
-        from amuse.support.units import constants
+        from amuse.units import constants
         self.assertAlmostEqual(constants.h.value_in(units.J*units.s), 6.6e-34, 35)
         self.assertAlmostEqual(constants.c.value_in(units.m/units.s), 299792458.0, 7)
 
     def test4(self):
         self.generate_constants_if_necessary()
-        from amuse.support.units import constants
+        from amuse.units import constants
         self.assertAlmostEqual(constants.h, 6.6e-34 | units.J*units.s, 35)
         self.assertAlmostEqual(constants.c, 299792458.0 | units.m/units.s, 7)
 
     def test5(self):
         self.generate_constants_if_necessary()
-        from amuse.support.units import constants
+        from amuse.units import constants
         self.assertAlmostEqual(constants.Planck_length**2 / 
             (constants.hbar*constants.G/constants.c**3), 1.0 | units.none, 5)
         self.assertAlmostEqual(constants.Planck_mass**2 / 
