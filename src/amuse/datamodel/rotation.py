c8d42bda1280edfd7d75498a70aaf9740bf8119a
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2011-10-10 13:14:13 +0000

    rotation implemented
    git-svn-id: file:///home/amuse/svn/amuse/trunk@2590 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/datamodel/rotation.py
@@ -0,0 +1,35 @@
+import numpy
+def new_rotation_matrix(phi, theta, psi):
+    """
+    Return the rotation matrix, to rotate positions, around the x-axis (phi), y-axis (theta) and z-axis (psi).
+    See wikipedia for reference
+    """
+    cos = numpy.cos
+    sin = numpy.sin
+    
+    return numpy.array( (
+        (cos(theta)*cos(psi), -cos(phi)*sin(psi) + sin(phi)*sin(theta)*cos(psi),  sin(phi)*sin(psi) + cos(phi)*sin(theta)*cos(psi)) ,
+        (cos(theta)*sin(psi),  cos(phi)*cos(psi) + sin(phi)*sin(theta)*sin(psi), -sin(phi)*cos(psi) + cos(phi)*sin(theta)*sin(psi)) ,
+        (-sin(theta)        ,  sin(phi)*cos(theta)                             ,  cos(phi)*cos(theta))
+    ) )
+
+def rotated(positions, phi, theta, psi):
+    """
+    Return the positions, rotated by phi, theta and psi around the x, y and z axes
+    """
+    rotation_matrix = new_rotation_matrix(phi, theta, psi)
+    units = positions.unit
+    value = positions.value_in(units)
+    
+    result = []
+    for x in value:
+        result.append(numpy.dot(rotation_matrix, x))
+    return units.new_quantity(result)
+
+def rotate(particles, phi, theta, psi):
+    """
+    Rotate the positions and the velocities around 0,0,0.
+    """
+    particles.position = rotated(particles.position,  phi, theta, psi)
+    particles.velocity = rotated(particles.velocity,  phi, theta, psi)
+
---
+++ test/core_tests/test_rotation.py
@@ -0,0 +1,98 @@
+from amuse.test import amusetest
+import os.path
+import numpy
+from amuse.units import units
+from amuse.units import constants
+from amuse.units import nbody_system
+from amuse.units import quantities
+from amuse.ic.plummer import new_plummer_sphere
+from amuse.datamodel import rotation
+from amuse.datamodel import particles
+
+class TestRotations(amusetest.TestCase):
+
+    def test01(self):
+        x = rotation.new_rotation_matrix(0,0,0)
+        self.assertAlmostRelativeEquals(x, ((1,0,0),(0,1,0),(0,0,1)))
+
+        x = rotation.new_rotation_matrix(numpy.pi,0,0)
+        print x
+        self.assertAlmostRelativeEquals(x, ((1,0,0),(0,-1,0),(0,0,-1)))
+        x = rotation.new_rotation_matrix(0,numpy.pi,0)
+        self.assertAlmostRelativeEquals(x, ((-1,0,0),(0,1,0),(0,0,-1)))
+        x = rotation.new_rotation_matrix(0, 0, numpy.pi)
+        self.assertAlmostRelativeEquals(x, ((-1,0,0),(0,-1,0),(0,0,1)))
+    def test02(self):
+        x = rotation.new_rotation_matrix(numpy.pi / 2.0,0,0)
+        print x
+        self.assertAlmostRelativeEquals(x, ((1,0,0),(0,0, -1),(0,1,0)))
+        x = rotation.new_rotation_matrix(0,numpy.pi / 2.0,0)
+        print x
+        self.assertAlmostRelativeEquals(x, ((0,0,1),(0,1,0),(-1,0,0)))
+        x = rotation.new_rotation_matrix(0, 0, numpy.pi / 2.0)
+        print x
+        self.assertAlmostRelativeEquals(x, ((0,-1,0),(1,0,0),(0,0,1)))
+        x = rotation.new_rotation_matrix(numpy.pi / 2.0,numpy.pi / 2.0,0)
+        print x
+        self.assertAlmostRelativeEquals(x, ((0,1,0),(0,0,-1),(-1,0,0)))
+
+    def test03(self):
+        positions = [ [1.0, 2.0, 3.0 ] ] | units.m
+        rotated = rotation.rotated(positions, 0.0, 0.0, 0.0)
+    
+        print rotated
+    
+        self.assertAlmostRelativeEquals(rotated, [ [1.0, 2.0, 3.0 ] ] | units.m)
+        rotated = rotation.rotated(positions, numpy.pi, 0.0, 0.0)
+    
+        print rotated
+    
+        self.assertAlmostRelativeEquals(rotated, [ [1.0, -2.0, -3.0 ] ] | units.m)
+
+    def test04(self):
+        positions = [ [1.0, 2.0, 3.0 ] , [4.0, 5.0, 6.0] ] | units.m
+        rotated = rotation.rotated(positions, 0.0, 0.0, 0.0)
+    
+        print rotated
+    
+        self.assertAlmostRelativeEquals(rotated, [ [1.0, 2.0, 3.0 ], [4.0, 5.0, 6.0] ] | units.m)
+    
+        rotated = rotation.rotated(positions, numpy.pi, 0.0, 0.0)
+        print rotated
+    
+        self.assertAlmostRelativeEquals(rotated, [ [1.0, -2.0, -3.0 ], [4.0, -5.0, -6.0] ] | units.m)
+
+    def test05(self):
+        positions = [ [1.0, 2.0, 3.0 ] , [4.0, 5.0, 6.0] ] | units.m
+        rotated = rotation.rotated(positions,  numpy.pi/2, 0.0, 0.0)
+    
+        print rotated
+    
+        self.assertAlmostRelativeEquals(rotated, [ [1.0, -3.0, 2.0 ], [4.0, -6.0, 5.0] ] | units.m)
+
+    def test06(self):
+        particles = new_plummer_sphere(100)
+        kinetic_energy0 = particles.kinetic_energy()
+        potential_energy0 = particles.potential_energy()
+        particles.position = rotation.rotated(particles.position,  numpy.pi/2, 0.0, 0.0)
+        particles.velocity = rotation.rotated(particles.velocity,  numpy.pi/2, 0.0, 0.0)
+    
+    
+        kinetic_energy1 = particles.kinetic_energy()
+        potential_energy1 = particles.potential_energy()
+        self.assertAlmostRelativeEquals(kinetic_energy1, kinetic_energy0)
+        self.assertAlmostRelativeEquals(potential_energy1, potential_energy0)
+
+    def test07(self):
+        particles = new_plummer_sphere(100)
+        kinetic_energy0 = particles.kinetic_energy()
+        potential_energy0 = particles.potential_energy()
+        particles.position = rotation.rotated(particles.position,  numpy.pi/3, numpy.pi/2, 0.0)
+        particles.velocity = rotation.rotated(particles.velocity,  numpy.pi/3, numpy.pi/2, 0.0)
+    
+    
+        kinetic_energy1 = particles.kinetic_energy()
+        potential_energy1 = particles.potential_energy()
+        self.assertAlmostRelativeEquals(kinetic_energy1, kinetic_energy0)
+        self.assertAlmostRelativeEquals(potential_energy1, potential_energy0)
+
