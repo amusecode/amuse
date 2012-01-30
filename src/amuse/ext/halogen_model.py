84e9b1193ee00d01e1251c90974168011129cbd5
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-01-30 13:00:37 +0000

    Small changes to new_halogen_model + new test with black hole
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3038 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/ext/halogen_model.py
+++ src/amuse/ext/halogen_model.py
@@ -1,6 +1,8 @@
 from amuse.community.halogen.interface import Halogen
 from amuse.datamodel import ParticlesWithUnitsConverted
-def new_halogen_model(number_of_particles, unit_converter = None, do_scale = False, **keyword_arguments):
+
+def new_halogen_model(number_of_particles, convert_nbody = None, do_scale = False, 
+        redirection = 'null', **keyword_arguments):
     """
     Create an alpha-beta-gamma-model using Halogen with the given number of 
     particles. Returns a set of equal-mass particles self-consistently sampled 
@@ -16,13 +18,13 @@ def new_halogen_model(number_of_particles, unit_converter = None, do_scale = Fal
     help(Halogen().parameters) for an overview of the Halogen parameters.
 
     :argument number_of_particles: Number of particles to generate in the model
-    :argument unit_converter:  When given will convert the resulting set to SI units
+    :argument convert_nbody:  When given will convert the resulting set to SI units
     :argument do_scale: scale the result to exact nbody units (M=1, K=0.25, U=-0.5)
     :argument alpha: alpha parameter in density profile (see amuse/community/halogen/src/doc for details)
     :argument beta:  beta parameter in density profile (see amuse/community/halogen/src/doc for details)
     :argument gamma: gamma parameter in density profile (see amuse/community/halogen/src/doc for details)
     """
-    instance = Halogen(unit_converter = unit_converter)
+    instance = Halogen(unit_converter=convert_nbody, redirection=redirection)
     instance.parameters.number_of_particles = number_of_particles
     for (key, value) in keyword_arguments.iteritems():
         setattr(instance.parameters, key, value)
@@ -35,8 +37,8 @@ def new_halogen_model(number_of_particles, unit_converter = None, do_scale = Fal
     if do_scale:
         result.scale_to_standard()
     
-    if not unit_converter is None:
-        result = ParticlesWithUnitsConverted(result, unit_converter.as_converter_from_si_to_nbody())
+    if not convert_nbody is None:
+        result = ParticlesWithUnitsConverted(result, convert_nbody.as_converter_from_si_to_nbody())
         result = result.copy_to_memory()
     return result
 
--- test/ext_tests/test_halogen_model.py
+++ test/ext_tests/test_halogen_model.py
@@ -2,7 +2,8 @@ from amuse.test.amusetest import TestWithMPI
 
 from amuse.support.exceptions import AmuseException
 from amuse.ext.halogen_model import new_halogen_model
-from amuse.units import nbody_system
+from amuse.units import nbody_system, units
+
 class NewHalogenModelTests(TestWithMPI):
     
     def test1(self):
@@ -17,5 +18,26 @@ class NewHalogenModelTests(TestWithMPI):
         self.assertRaises(AmuseException, new_halogen_model, number_of_particles, expected_message = 
             "Error when calling 'commit_parameters' of a 'Halogen', errorcode is -2, error is "
             "'Missing or bad parameter for halo (see amuse/community/halogen/src/doc for details on required parameters).'")
+    
+    def test2(self):
+        number_of_particles = 1000
+        black_hole_mass = 1.0e6 | units.MSun
+        stellar_mass = number_of_particles | units.MSun
+        scale_radius = 0.1 | units.parsec
+        
+        
+        converter = nbody_system.nbody_to_si(black_hole_mass, scale_radius)
+        
+        particles = new_halogen_model(number_of_particles, convert_nbody=converter, 
+            alpha=1.0, beta=3.0, gamma=1.0, # NFW
+            random_seed=1.0, redirection='none',
+            black_hole_mass=black_hole_mass, total_mass=stellar_mass,
+            cutoff_radius=10.0*scale_radius, scale_radius=scale_radius)
+        
+        self.assertEquals(len(particles), number_of_particles + 1)
+        self.assertAlmostEquals(particles[-1].mass, black_hole_mass)
+        self.assertAlmostEquals(particles.total_mass(), black_hole_mass + stellar_mass)
+        self.assertAlmostRelativeEquals(particles.kinetic_energy(), 
+            2.27538127277e+43 | units.J, 10)
     
 
