321aead45c7ec12a67e319505c9eef7a6e21d124
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-03-07 11:21:42 +0000

    Added slowtest for GalactICs model to demonstrate typical usage
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3167 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/ext_tests/test_galactics_model.py
+++ test/ext_tests/test_galactics_model.py
@@ -1,8 +1,11 @@
-from amuse.test.amusetest import TestWithMPI
+import os.path
 
+from amuse.test.amusetest import TestWithMPI, get_path_to_results
 from amuse.support.exceptions import AmuseException
 from amuse.ext.galactics_model import new_galactics_model
 from amuse.units import nbody_system
+from amuse.io import write_set_to_file
+
 class NewGalactICsModelTests(TestWithMPI):
     
     def test1(self):
@@ -25,5 +28,16 @@ class NewGalactICsModelTests(TestWithMPI):
         self.assertTrue("halo_streaming_fraction" in new_galactics_model.__doc__)
         
         print new_galactics_model.__doc__
+    
+    def slowtest3(self):
+        print "Generate a model for M31, using defaults (200k halo, 50k bulge, 100k disk)"
+        halo_number_of_particles = 200000
+        particles = new_galactics_model(halo_number_of_particles, do_scale = True)
+        
+        self.assertEquals(len(particles), 350000)
+        self.assertAlmostEquals(particles.total_mass(), 1.0 | nbody_system.mass)
+        self.assertAlmostEquals(particles.kinetic_energy(), 0.25 | nbody_system.energy)
+        
+        write_set_to_file(particles, os.path.join(get_path_to_results(), 'M31_galactICs.amuse'), 'amuse')
     
 
