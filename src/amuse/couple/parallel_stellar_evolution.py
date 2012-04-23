f1244697694d129bd73ac2b403c2d9f60a92d31b
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-04-23 09:13:54 +0000

    Ticket #297: parallel stellar evolution update: more parallel, tests more stable, handle exceptions, mergers and models, individual options
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3336 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/couple/parallel_stellar_evolution.py
+++ src/amuse/couple/parallel_stellar_evolution.py
@@ -1,68 +1,132 @@
+import sys
+import traceback
 import numpy
 import threading
 
 from amuse.units import units
 from amuse.datamodel import ParticlesSuperset
 from amuse.support.exceptions import AmuseException
+from amuse.community.interface.se import merge_colliding_in_stellar_evolution_code
+
+
+def _execute_all_threads(threads):
+    for x in threads:
+        x.start()
+    for x in threads:
+        x.join()
+    result = [x.get_result() for x in threads]
+    if not result == [None]*len(threads):
+        return result
+
 
 class ParallelStellarEvolution(object):
 
-    def __init__(self, stellar_evolution_class, number_of_workers=1, **options):
+    def __init__(self, stellar_evolution_class, number_of_workers=1, individual_options=None, 
+            execute_all_threads_func=_execute_all_threads, **options):
         self.code_factory = stellar_evolution_class
         self.number_of_workers = number_of_workers
         self.model_time = 0.0 | units.Myr
-        self.code_instances = [stellar_evolution_class(**options) for i in range(number_of_workers)]
-        self.particles = ParallelParticlesSuperset([code.particles for code in self.code_instances])
+        self._execute_all_threads_func = execute_all_threads_func
+        
+        if individual_options is None:
+            options_list = [options] * number_of_workers
+        else:
+            options_list = [options.copy() for i in range(number_of_workers)]
+            for individual, shared in zip(individual_options, options_list):
+                shared.update(individual)
+        
+        threads = [ThreadWithResult(target=stellar_evolution_class, kwargs=options_list[i]) for i in range(number_of_workers)]
+        self.code_instances = self._execute_all_threads_func(threads)
+        
+        self.particles = ParallelParticlesSuperset(
+            [code.particles for code in self.code_instances], 
+            execute_all_threads_func=self._execute_all_threads_func)
         self.parameters = ParallelParameters(self.code_instances)
     
-    def run_threaded(self, function_name, args=()):
-        threads=[]
-        for code in self.code_instances:
-            threads.append(threading.Thread(target=getattr(code, function_name), args=args))
-        for x in threads:
-            x.start()
-        for x in threads:
-            x.join()
+    def _run_threaded(self, function_name, args=()):
+        threads = [ThreadWithResult(target=getattr(code, function_name), args=args) for code in self.code_instances]
+        return self._execute_all_threads_func(threads)
     
     def initialize_code(self):
-        self.run_threaded("initialize_code")
+        self._run_threaded("initialize_code")
     
     def commit_parameters(self):
-        self.run_threaded("commit_parameters")
+        self._run_threaded("commit_parameters")
     
     def recommit_parameters(self):
-        self.run_threaded("recommit_parameters")
+        self._run_threaded("recommit_parameters")
     
     def commit_particles(self):
-        self.run_threaded("commit_particles")
+        self._run_threaded("commit_particles")
     
     def recommit_particles(self):
-        self.run_threaded("recommit_particles")
+        self._run_threaded("recommit_particles")
     
     def evolve_model(self, end_time):
-        self.run_threaded("evolve_model", args=(end_time,))
+        self._run_threaded("evolve_model", args=(end_time,))
         self.model_time = end_time
     
     def cleanup_code(self):
-        self.run_threaded("cleanup_code")
+        self._run_threaded("cleanup_code")
     
     def stop(self):
-        self.run_threaded("stop")
+        self._run_threaded("stop")
+    
+    def merge_colliding(self, *args, **kwargs):
+        return merge_colliding_in_stellar_evolution_code(self, *args, **kwargs)
+    
+    def new_particle_from_model(self, internal_structure, current_age, key=None):
+        index = self.particles.next_index_of_code_instance_for_new_particle_from_model()
+        return self.code_instances[index].new_particle_from_model(internal_structure, current_age, key=key)
+
+
+class ThreadWithResult(threading.Thread):
+    
+    def __init__(self, target=None, args=(), kwargs=dict()):
+        self.__target = target
+        self.__args = args
+        self.__kwargs = kwargs
+        self.result = None
+        self.caught_exception = False
+        threading.Thread.__init__(self, target=target, args=args, kwargs=kwargs)
+    
+    def run(self):
+        try:
+            self.result = self.__target(*self.__args, **self.__kwargs)
+        except Exception:
+            self.caught_exception = True
+            self.result = sys.exc_info()
+    
+    def get_result(self):
+        if self.caught_exception:
+            traceback.print_exception(*self.result)
+            raise self.result[1]
+        return self.result
     
 
 class ParallelParticlesSuperset(ParticlesSuperset):
     
-    def __init__(self, particle_sets):
+    def __init__(self, particle_sets, execute_all_threads_func=_execute_all_threads):
         ParticlesSuperset.__init__(self, particle_sets)
         self._private.number_of_particles = 0
         self._private.number_of_sets = len(particle_sets)
+        self._private.execute_all_threads_func = execute_all_threads_func
     
     def add_particles_to_store(self, keys, attributes = [], values = []):
-        slices = [slice((i+self._private.number_of_particles) % self._private.number_of_sets, len(keys), 
+        slices = [slice((i-self._private.number_of_particles) % self._private.number_of_sets, len(keys), 
             self._private.number_of_sets) for i in range(self._private.number_of_sets)]
-        for particle_set, one_slice in zip(self._private.particle_sets, slices):
-            particle_set.add_particles_to_store(keys[one_slice], attributes, [v[one_slice] for v in values])
+        threads = [
+            ThreadWithResult(
+                target=particle_set.add_particles_to_store, 
+                args=(keys[one_slice], attributes, [v[one_slice] for v in values])
+            ) for particle_set, one_slice in zip(self._private.particle_sets, slices) if len(keys[one_slice])]
+        self._private.execute_all_threads_func(threads)
         self._private.number_of_particles += len(keys)
+    
+    def next_index_of_code_instance_for_new_particle_from_model(self):
+        next_index = -self._private.number_of_particles % self._private.number_of_sets
+        self._private.number_of_particles += 1
+        return next_index
     
 class ParallelParameters(object):
     
--- test/ext_tests/test_parallel_stellar_evolution.py
+++ test/ext_tests/test_parallel_stellar_evolution.py
@@ -1,9 +1,11 @@
+import os
+import os.path
 import numpy
 
 from amuse.units import units
 from amuse.datamodel import Particles
 from amuse.support.exceptions import AmuseException
-from amuse.test.amusetest import TestCase
+from amuse.test.amusetest import TestCase, get_path_to_results
 
 from amuse.community.mesa.interface import MESA
 from amuse.community.evtwin.interface import EVtwin
@@ -11,30 +13,44 @@ from amuse.community.sse.interface import SSE
 
 from amuse.couple.parallel_stellar_evolution import ParallelStellarEvolution
 
+
+def execute_all_threads_serial(threads):
+    for x in threads:
+        x.run()
+    result = [x.get_result() for x in threads]
+    if not result == [None]*len(threads):
+        return result
+
+default_options = dict(execute_all_threads_func = execute_all_threads_serial) # Always thread-safe
+#~default_options = dict() # Really really parallel
+
+
 class TestParallelStellarEvolution(TestCase):
     
-    code_factory = EVtwin
+    code_factory = MESA
     
     def test1(self):
         print "Testing ParallelStellarEvolution initialization"
-        instance = ParallelStellarEvolution(self.code_factory, number_of_workers=4)
+        instance = ParallelStellarEvolution(self.code_factory, number_of_workers=3, **default_options)
         instance.initialize_code()
         instance.cleanup_code()
         instance.stop()
     
     def test2(self):
         print "Testing ParallelStellarEvolution particles"
-        instance = ParallelStellarEvolution(self.code_factory, number_of_workers=3)
+        instance = ParallelStellarEvolution(self.code_factory, number_of_workers=2, **default_options)
         instance.initialize_code()
         instance.commit_parameters()
-        particles = Particles(7)
+        
+        particles = Particles(5)
         particles.mass = range(1, 1+len(particles)) | units.MSun
         incode = instance.particles.add_particles(particles)
         instance.commit_particles()
         self.assertAlmostEqual(incode.mass, range(1, 1+len(particles)) | units.MSun)
         print "Note that the order of instance.particles is different from the",
-        print "original particle order, since particles are distributed over 3 processes"
-        self.assertAlmostEqual(instance.particles.mass, [1,4,7, 2,5, 3,6] | units.MSun)
+        print "original particle order, since particles are distributed over 2 processes"
+        self.assertAlmostEqual(instance.particles.mass, [1,3,5, 2,4] | units.MSun)
+        
         instance.stop()
         
     def slowtest3(self):
@@ -47,7 +63,7 @@ class TestParallelStellarEvolution(TestCase):
         self.assertAlmostEqual(inserial.mass, range(1, 1+len(particles)) | units.MSun)
         serial.evolve_model(0.2 | units.Myr)
         
-        parallel = ParallelStellarEvolution(self.code_factory, number_of_workers=3)
+        parallel = ParallelStellarEvolution(self.code_factory, number_of_workers=3, **default_options)
         inparallel = parallel.particles.add_particles(particles)
         self.assertAlmostEqual(inparallel.mass, range(1, 1+len(particles)) | units.MSun)
         parallel.evolve_model(0.2 | units.Myr)
@@ -62,10 +78,46 @@ class TestParallelStellarEvolution(TestCase):
     
     def test4(self):
         print "Testing ParallelStellarEvolution parameters"
-        parallel = ParallelStellarEvolution(self.code_factory, number_of_workers=3)
+        parallel = ParallelStellarEvolution(self.code_factory, number_of_workers=3, **default_options)
         parallel.parameters.metallicity = 0.01
         self.assertEqual(parallel.parameters.metallicity, 0.01)
         for code in parallel.code_instances:
             self.assertEqual(code.parameters.metallicity, 0.01)
+        parallel.stop()
+    
+    def test5(self):
+        print "Testing ParallelStellarEvolution individual options"
+        base_name = os.path.join(get_path_to_results(), "parallel_stellar_evolution_out_")
+        for filename in [base_name+str(i) for i in range(3)]:
+            if os.path.exists(filename):
+                os.remove(filename)
+        
+        parallel = ParallelStellarEvolution(self.code_factory, number_of_workers=3, 
+            individual_options=[dict(redirect_file=base_name+str(i)) for i in range(3)], redirection="file", **default_options)
+        
+        for filename in [base_name+str(i) for i in range(3)]:
+            self.assertTrue(os.path.exists(filename))
+        
+        parallel.stop()
+    
+    def test6(self):
+        print "Testing ParallelStellarEvolution exception handling"
+        if self.code_factory == MESA:
+            expected_message = ("Error when calling 'evolve_for' of a 'MESA', errorcode is -12, error is 'Evolve terminated: Maximum age reached.'")
+        elif self.code_factory == EVtwin:
+            expected_message = ("Error when calling 'evolve_for' of a 'EVtwin', errorcode is 5, error is 'PRINTB -- age greater than limit'")
+        else:
+            self.skip("Skipping test: {0} has no parameter max_age_stop_condition".format(self.code_factory))
+        
+        parallel = ParallelStellarEvolution(self.code_factory, number_of_workers=2, **default_options)
+        parallel.parameters.max_age_stop_condition = 0.1 | units.Myr
+        
+#~        parallel.particles.add_particles(Particles(2, mass=[1,2]|units.MSun)) # Test speed-up:
+        parallel.particles.add_particles(Particles(1, mass=1|units.MSun))
+        
+        self.assertRaises(AmuseException, parallel.evolve_model, 1.0|units.Myr, 
+            expected_message = expected_message)
+        self.assertTrue((parallel.particles.age >= 0.1 | units.Myr).all())
+        self.assertTrue((parallel.particles.age-parallel.particles.time_step < 0.1 | units.Myr).all())
         parallel.stop()
     
