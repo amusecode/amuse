1023f2f3d31c83c0e62ef1f74484924cb8dea825
galtay <galtay@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-05-09 15:47:57 +0000

    back in action with the no goto code
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3395 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/community/sphray/src/amuse_mainloop.F90
+++ src/amuse/community/sphray/src/amuse_mainloop.F90
@@ -80,158 +80,170 @@ contains
     !=========================
     snaps: do snapn = GV%StartSnapNum, GV%EndSnapNum
     
-       if(size(psys%src).EQ.0) then
+       ! if there are no sources update the time to the end
+       !-------------------------------------------------------
+       if (size(psys%src).EQ.0) then
 
           GV%itime = GV%itime + PLAN%snap(snapn)%SrcRays
           call set_time_elapsed_from_itime( GV )
           print*, PLAN%snap(snapn)%SrcRays, PLAN%snap(snapn)%SrcRays,GV%time_elapsed_myr
-          goto 123
-       endif   
+
+       ! otherwise trace rays
+       !-------------------------------------------------------
+       else   
          
               
-       !  build oct tree.  only need to do this once per snap (for now)
-       !----------------------------------------------------------------
-       call buildtree(psys,tree,MB,GV%PartPerCell)
-       GV%MB = GV%MB + MB
-       call setparticleorder(psys, tree)             
-       call prepare_raysearch(psys, globalraylist)
-             
-       if (GV%raystats) then
-          write(GV%raystatlun) PLAN%snap(snapn)%SrcRays, raystatbuffsize 
-       endif
-       
-       if(GV%JustInit) then
-          write(str,"(A,F10.2)") "total memory allocated [MB] = ", GV%MB
-          call mywrite(str,verb)
-          call mywrite("just initializing", verb)
-          call mywrite("",verb)
-          stop
-       end if
-
-       if (GV%DoInitialOutput) then
-          GV%OutputIndx = 0
-          call output_total_snap(psys)      
-          GV%OutputIndx = 1
-       end if
-              
-       ! begin ray tracing 
-       !------------------------- 
-       src_rays: do rayn = one, PLAN%snap(snapn)%SrcRays
-
-          GV%rayn                = GV%rayn + 1
-          GV%src_rayn            = GV%src_rayn + 1
-          GV%TotalSourceRaysCast = GV%TotalSourceRaysCast + 1                
+          !  build oct tree.  only need to do this once per snap (for now)
+          !----------------------------------------------------------------
+          call buildtree(psys,tree,MB,GV%PartPerCell)
+          GV%MB = GV%MB + MB
+          call setparticleorder(psys, tree)             
+          call prepare_raysearch(psys, globalraylist)
           
-          !  select a source randomly (weighted by their luminosity)
-          rn = genrand_real1() * psys%src(size(psys%src))%Lcdf
-          srcn=1
-          do while(psys%src(srcn)%Lcdf.LT.rn)
-             srcn=srcn+1
-             if(srcn.GT.size(psys%src)) then
-                write(*,*) srcn, rn, psys%src(size(psys%src))%Lcdf, size(psys%src)
-                stop "src num > number of sources in mainloop.f90"
-             endif
-          enddo
-                    
-          !  create a source ray and calc the impacts
-          call src_ray_make( ray, psys%src(srcn), GV%rayn, GV%dt_s, GV%Lunit, psys%box )
-          
-          GV%itime = GV%itime + 1
-          call set_time_elapsed_from_itime( GV )
-          GV%IonizingPhotonsPerSec = GV%TotalPhotonsCast / GV%time_elapsed_s
-
-          if(mod(rayn,PLAN%snap(snapn)%SrcRays/100).EQ.0) &
-              print*, rayn, PLAN%snap(snapn)%SrcRays,GV%time_elapsed_myr
-
-
-          globalraylist%ray = ray
-          call trace_ray(globalraylist%ray, globalraylist, psys, tree) 
-          
-          GV%TotalPhotonsCast = GV%TotalPhotonsCast + globalraylist%ray%pini
-          
-          srcray = .true.
-          call update_raylist(globalraylist,psys%par,psys%box,srcray)
-                    
           if (GV%raystats) then
-             
-             raystatcnt = raystatcnt + 1
-             
-             raystats(raystatcnt)%srcn  = srcn
-             raystats(raystatcnt)%start = globalraylist%ray%start  
-             raystats(raystatcnt)%ryd   = globalraylist%ray%freq
-             
-             if (raystatcnt == raystatbuffsize) then
-                write(GV%raystatlun) raystats
-                flush(GV%raystatlun)
-                raystatcnt = 0
-             end if
-                          
+             write(GV%raystatlun) PLAN%snap(snapn)%SrcRays, raystatbuffsize 
+          endif
+          
+          if(GV%JustInit) then
+             write(str,"(A,F10.2)") "total memory allocated [MB] = ", GV%MB
+             call mywrite(str,verb)
+             call mywrite("just initializing", verb)
+             call mywrite("",verb)
+             stop
           end if
           
-                              
+          if (GV%DoInitialOutput) then
+             GV%OutputIndx = 0
+             call output_total_snap(psys)      
+             GV%OutputIndx = 1
+          end if
           
-          !        output routines
-          !------------------------
-          
-          !        check if this time step requires a small ionization frac output
-          !        these are small outputs done often to monitor certain quantites
-!          if( mod(GV%rayn,GV%IonFracOutRays)==0 ) then
-!             call ion_frac_out(psys, tree )
-!          end if
-          
-          
-          ! check if this time step requires a full output
-          if ( GV%OutputIndx <= GV%NumTotOuts ) then
+          ! begin ray tracing 
+          !------------------------- 
+          src_rays: do rayn = one, PLAN%snap(snapn)%SrcRays
              
-             ! set correct time marker unit
-             if ( trim(GV%OutputTiming) == "standard" ) then
+             GV%rayn                = GV%rayn + 1
+             GV%src_rayn            = GV%src_rayn + 1
+             GV%TotalSourceRaysCast = GV%TotalSourceRaysCast + 1                
+             
+             !  select a source randomly (weighted by their luminosity)
+             rn = genrand_real1() * psys%src(size(psys%src))%Lcdf
+             srcn=1
+             do while(psys%src(srcn)%Lcdf.LT.rn)
+                srcn=srcn+1
+                if(srcn.GT.size(psys%src)) then
+                   write(*,*) srcn, rn, psys%src(size(psys%src))%Lcdf, size(psys%src)
+                   stop "src num > number of sources in mainloop.f90"
+                endif
+             enddo
+             
+             !  create a source ray and calc the impacts
+             call src_ray_make( ray, psys%src(srcn), GV%rayn, GV%dt_s, GV%Lunit, psys%box )
+             
+             GV%itime = GV%itime + 1
+             call set_time_elapsed_from_itime( GV )
+             GV%IonizingPhotonsPerSec = GV%TotalPhotonsCast / GV%time_elapsed_s
+             
+             if(mod(rayn,PLAN%snap(snapn)%SrcRays/100).EQ.0) &
+                  print*, rayn, PLAN%snap(snapn)%SrcRays,GV%time_elapsed_myr
+             
+             
+             globalraylist%ray = ray
+             call trace_ray(globalraylist%ray, globalraylist, psys, tree) 
+             
+             GV%TotalPhotonsCast = GV%TotalPhotonsCast + globalraylist%ray%pini
+             
+             srcray = .true.
+             call update_raylist(globalraylist,psys%par,psys%box,srcray)
+             
+             if (GV%raystats) then
                 
-                outmark = GV%start_time_code + GV%time_elapsed_code
+                raystatcnt = raystatcnt + 1
                 
-             else if ( trim(GV%OutputTiming) == "forced" ) then
+                raystats(raystatcnt)%srcn  = srcn
+                raystats(raystatcnt)%start = globalraylist%ray%start  
+                raystats(raystatcnt)%ryd   = globalraylist%ray%freq
                 
-                if (trim(GV%ForcedUnits) == "mwionfrac") then
-                   outmark = GV%mwionfrac
-                else 
-                   outmark = GV%time_elapsed_code
+                if (raystatcnt == raystatbuffsize) then
+                   write(GV%raystatlun) raystats
+                   flush(GV%raystatlun)
+                   raystatcnt = 0
                 end if
                 
-             else
-                
-                write(*,*) "output type ", trim(GV%OutputTiming), "not recognized"
-                stop 
-                
-             end if
-
-
-             
-             ! check outmark against tabulated output "times"
-             if ( outmark >= PLAN%OutputTimes(GV%OutputIndx) ) then
-                call output_total_snap(psys)
-                GV%OutputIndx = GV%OutputIndx + 1 
              end if
              
-          end if
-          
-          
-          ! if we are on the last ray and we havent gotten to the last 
-          ! output, do a full output.
-          if ( snapn == GV%EndSnapNum ) then
+             
+             
+             !        output routines
+             !------------------------
+             
+             !        check if this time step requires a small ionization frac output
+             !        these are small outputs done often to monitor certain quantites
+             !          if( mod(GV%rayn,GV%IonFracOutRays)==0 ) then
+             !             call ion_frac_out(psys, tree )
+             !          end if
+             
+             
+             ! check if this time step requires a full output
              if ( GV%OutputIndx <= GV%NumTotOuts ) then
-                if ( GV%src_rayn==PLAN%snap(snapn)%SrcRays ) then
-                   write(*,*) "doing an output on the last ray"
+                
+                ! set correct time marker unit
+                if ( trim(GV%OutputTiming) == "standard" ) then
+                   
+                   outmark = GV%start_time_code + GV%time_elapsed_code
+                   
+                else if ( trim(GV%OutputTiming) == "forced" ) then
+                   
+                   if (trim(GV%ForcedUnits) == "mwionfrac") then
+                      outmark = GV%mwionfrac
+                   else 
+                      outmark = GV%time_elapsed_code
+                   end if
+                   
+                else
+                   
+                   write(*,*) "output type ", trim(GV%OutputTiming), "not recognized"
+                   stop 
+                   
+                end if
+                
+                
+                
+                ! check outmark against tabulated output "times"
+                if ( outmark >= PLAN%OutputTimes(GV%OutputIndx) ) then
                    call output_total_snap(psys)
+                   GV%OutputIndx = GV%OutputIndx + 1 
+                end if
+                
+             end if
+             
+             
+             ! if we are on the last ray and we havent gotten to the last 
+             ! output, do a full output.
+             if ( snapn == GV%EndSnapNum ) then
+                if ( GV%OutputIndx <= GV%NumTotOuts ) then
+                   if ( GV%src_rayn==PLAN%snap(snapn)%SrcRays ) then
+                      write(*,*) "doing an output on the last ray"
+                      call output_total_snap(psys)
+                   end if
                 end if
              end if
-          end if
+             
+             
+          end do src_rays
           
-          
-       end do src_rays
-       
-       ! free up the memory from the globalraylist.
-       call kill_raylist(globalraylist)
+          ! free up the memory from the globalraylist.
+          call kill_raylist(globalraylist)
 
-123 call non_photo_update_all(psys%par)
+       endif
+
+
+       ! at this point particles are synchronized.  if there are sources, 
+       ! particles are evolved with GHI=0 for a dt equal to the last time
+       ! they were hit by a ray to the final time.  if there are no sources
+       ! each particle is updated over the whole time
+       call non_photo_update_all(psys%par)
+
 
     end do snaps
     
