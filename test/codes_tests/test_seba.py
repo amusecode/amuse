748de5b88effedd95074ad565f6b64a1af3d2e0b
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-05-21 13:53:24 +0000

    Fixes for SeBa tests
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3447 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/codes_tests/test_seba.py
+++ test/codes_tests/test_seba.py
@@ -10,13 +10,16 @@ class TestSeBaInterface(TestWithMPI):
     def test1(self):
         instance = self.new_instance_of_an_optional_code(SeBaInterface)
             
-        endtime, mass, radius, luminosity, temperature, error = instance.evolve_star(1, 4600, 0.02)
+        endtime, mass, radius, luminosity, temperature, time_step, stellar_type, error = instance.evolve_star(1, 4600, 0.02)
         self.assertEquals(error, 0)
         self.assertTrue( endtime <= 4600.0)
+        self.assertAlmostRelativeEqual(endtime, 4600.0, 4)
         self.assertAlmostRelativeEqual(mass, 1.0, 6)
         self.assertAlmostRelativeEqual(radius, 0.9856, 4)
         self.assertAlmostRelativeEqual(luminosity, 0.9585, 4)
         self.assertAlmostRelativeEqual(temperature, 5751, 4)
+        self.assertAlmostRelativeEqual(time_step, 1089.3, 4)
+        self.assertEqual(stellar_type, 1)
         
 class TestSeBa(TestWithMPI):
 
@@ -24,13 +27,15 @@ class TestSeBa(TestWithMPI):
         
         instance = self.new_instance_of_an_optional_code(SeBa)
             
-        endtime, mass, radius, luminosity, temperature = instance.evolve_star(1 | units.MSun, 4600 | units.Myr, 0.02)
+        endtime, mass, radius, luminosity, temperature, time_step, stellar_type = instance.evolve_star(1 | units.MSun, 4600 | units.Myr, 0.02)
         
         self.assertTrue( endtime <= 4600 | units.Myr)
         self.assertAlmostRelativeEqual(mass, 1.0 | units.MSun, 4)
         self.assertAlmostRelativeEqual(radius, 0.9856 | units.RSun, 4)
         self.assertAlmostRelativeEqual(luminosity, 0.9585 | units.LSun, 4)
         self.assertAlmostRelativeEqual(temperature, 5751 | units.K, 4)
+        self.assertAlmostRelativeEqual(time_step, 1089.3 | units.Myr, 4)
+        self.assertEqual(stellar_type, 1 | units.stellar_type)
 
         
     def test2(self):
