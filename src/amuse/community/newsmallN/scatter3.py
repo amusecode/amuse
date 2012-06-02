ee8bf2b3f3da46e6265d8a65ed42a3517e12ae4e
steve <steve@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-06-02 02:07:05 +0000

    Minor output reformatting.
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3475 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/community/newsmallN/scatter3.py
+++ src/amuse/community/newsmallN/scatter3.py
@@ -359,7 +359,7 @@ if __name__ == '__main__':
         scatter3(init, accuracy_parameter, delta_t, t_end)
         if nscatter > 1: print '\n--------------------\n'
 
-    print 'time1 =', time1, ' time2 =', time2, \
-          ' time3 =', time3, ' time4 =', time4
+    print 'timing: inst', time1, ' init', time2, \
+	  ' evol', time3, ' over', time4
 
     kep.stop()
