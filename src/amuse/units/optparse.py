5000908e9f35051199e9c83cf6deb6ab28c302cd
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-06-11 10:00:53 +0000

    unit aware option parser for amuse
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3507 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/units/optparse.py
@@ -0,0 +1,124 @@
+from __future__ import absolute_import
+
+import optparse
+import textwrap
+from amuse.units import quantities
+
+def check_builtin_unit(option, opt, value):
+    (cvt, what) = optparse._builtin_cvt[option.type]
+    try:
+        result = cvt(value)
+        if option.unit is None:
+            return result
+        else:
+            return quantities.new_quantity(result, option.unit)
+            
+    except ValueError:
+        raise OptionValueError(
+            _("option %s: invalid %s value: %r") % (opt, what, value))
+
+
+class Option(optparse.Option):
+    TYPE_CHECKER = {
+        "int"    : check_builtin_unit,
+        "long"   : check_builtin_unit,
+        "float"  : check_builtin_unit,
+        "complex": check_builtin_unit,
+        "choice" : optparse.check_choice,
+    }
+    ATTRS = optparse.Option.ATTRS + ['unit',]
+    
+    def convert_value(self, opt, value):
+        if value is not None:
+            if self.nargs == 1:
+                return self.check_value(opt, value)
+            else:
+                return tuple([self.check_value(opt, v) for v in value])
+
+class IndentedHelpFormatter(optparse.IndentedHelpFormatter):
+    
+    def __init__(self,
+             indent_increment=2,
+             max_help_position=24,
+             width=None,
+             short_first=1):
+        optparse.IndentedHelpFormatter.__init__(
+            self, indent_increment, max_help_position, width, short_first)
+            
+        self.unit_tag = "%unit"
+        
+    def format_option(self, option):
+        # The help for each option consists of two parts:
+        #   * the opt strings and metavars
+        #     eg. ("-x", or "-fFILENAME, --file=FILENAME")
+        #   * the user-supplied help string
+        #     eg. ("turn on expert mode", "read data from FILENAME")
+        #
+        # If possible, we write both of these on the same line:
+        #   -x      turn on expert mode
+        #
+        # But if the opt string list is too long, we put the help
+        # string on a second line, indented to the same column it would
+        # start in if it fit on the first line.
+        #   -fFILENAME, --file=FILENAME
+        #           read data from FILENAME
+        result = []
+        opts = self.option_strings[option]
+        opt_width = self.help_position - self.current_indent - 2
+        if len(opts) > opt_width:
+            opts = "%*s%s\n" % (self.current_indent, "", opts)
+            indent_first = self.help_position
+        else:                       # start help on same line as opts
+            opts = "%*s%-*s  " % (self.current_indent, "", opt_width, opts)
+            indent_first = 0
+        result.append(opts)
+            
+        if option.help:
+            help_text = self.expand_default(option)
+            help_text = self.expand_unit(option)
+            help_lines = textwrap.wrap(help_text, self.help_width)
+            result.append("%*s%s\n" % (indent_first, "", help_lines[0]))
+            result.extend(["%*s%s\n" % (self.help_position, "", line)
+                           for line in help_lines[1:]])
+        elif opts[-1] != "\n":
+            result.append("\n")
+        return "".join(result)
+    
+    def expand_unit(self, option):
+        if self.parser is None or not self.unit_tag:
+            return option.help
+
+        if option.unit is None:
+            return option.help
+        
+        unit = option.unit
+
+        return option.help.replace(self.unit_tag, str(unit))
+    
+    
+    
+class OptionParser(optparse.OptionParser):
+    
+    def __init__(self,
+             usage=None,
+             option_list=None,
+             option_class=Option,
+             version=None,
+             conflict_handler="error",
+             description=None,
+             formatter=None,
+             add_help_option=True,
+             prog=None,
+             epilog=None):
+        
+        
+        if formatter is None:
+            formatter = IndentedHelpFormatter()
+            
+        optparse.OptionParser.__init__(
+            self, usage, 
+            option_list, option_class, 
+            version, conflict_handler, 
+            description, formatter,
+            add_help_option, prog, epilog
+        )
---
+++ test/core_tests/test_optparse.py
@@ -0,0 +1,52 @@
+from amuse.test import amusetest
+
+import numpy
+import sys
+
+
+from amuse.support.exceptions import AmuseException
+from amuse.units.quantities import *
+from amuse.units import si
+from amuse.units import units
+from amuse.units import nbody_system
+
+from amuse import datamodel
+from amuse.units import optparse
+
+class TestQuantities(amusetest.TestCase):
+
+    def test1(self):
+        x = optparse.OptionParser()
+        x.add_option('-m', unit = units.MSun, dest = "mass", type = float)
+        options, args = x.parse_args(['-m', '2.0'])
+        self.assertAlmostRelativeEquals(options.mass, 2.0 | units.MSun)
+        
+    def test2(self):
+        x = optparse.OptionParser()
+        x.add_option('-m', unit = units.MSun, default = 1 | units.MSun, dest = "mass", type = float)
+        options, args = x.parse_args(['bla'])
+        self.assertAlmostRelativeEquals(options.mass, 1.0 | units.MSun)
+        self.assertEquals(args[0], 'bla')
+        
+    def test3(self):
+        x = optparse.OptionParser()
+        x.add_option('-m', unit = units.MSun, default = '1.5', dest = "mass", type = float)
+        options, args = x.parse_args(['bla'])
+        self.assertAlmostRelativeEquals(options.mass, 1.5 | units.MSun)
+        self.assertEquals(args[0], 'bla')
+        
+    def test4(self):
+        x = optparse.OptionParser()
+        x.add_option('-m', unit = units.MSun, help = "(unit: %unit)", default = '1.5', dest = "mass", type = float)
+        helpstr = x.format_help()
+        
+        print helpstr
+        self.assertTrue('(unit: MSun)' in helpstr)
+        
+        x = optparse.OptionParser()
+        x.add_option('-m', unit = nbody_system.mass, help = "(unit: %unit)", default = '1.5', dest = "mass", type = float)
+        helpstr = x.format_help()
+        
+        print helpstr
+        self.assertTrue('(unit: mass)' in helpstr)
+
