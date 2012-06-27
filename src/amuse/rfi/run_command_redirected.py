f7b159a87a9726495ca13e0703f90838ed96da8a
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-06-27 11:26:06 +0000

    redirection to nul on windows
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3555 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/rfi/run_command_redirected.py
+++ src/amuse/rfi/run_command_redirected.py
@@ -5,22 +5,31 @@ import os.path
 import time
 import signal
 
+def translate_filename_for_os(filename):
+    if sys.platform == 'win32':
+        if filename == '/dev/null':
+            return 'nul'
+        else:
+            return filename
+    else:
+        return filename
+
 if __name__ == '__main__':
 
     if sys.argv[1] == 'none':
         stdout = None
     else:
-        fname=sys.argv[1]
+        fname=translate_filename_for_os(sys.argv[1])
         stdout = open(fname,'w')
         
     if sys.argv[2] == 'none':
         stderr = None
     else:
-        fname=sys.argv[2]
+        fname=translate_filename_for_os(sys.argv[2])
         stderr = open(fname,'w')
     
     
-    stdin = open('/dev/null','r')
+    stdin = open(translate_filename_for_os('/dev/null'),'r')
     
     returncode = call(
         sys.argv[3:], 
