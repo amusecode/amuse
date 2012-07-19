69ee755a005c56b20f1bef49ccfa61f63f22eb1e
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-07-19 10:50:39 +0000

    removed copy of seba for integrating full seba svn
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3698 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/community/seba/src/sbin/copy0.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for file in `find include -name config.h` ; do
-    cp -p $STARLAB/$file $file
-done
--- src/amuse/community/seba/src/sstar/starclass/statics.C
+++
@@ -1,5 +0,0 @@
-/* Implementation of all statics, when combining this with Starlab this file probably needs to be removed. */
-
-#include "star.h"
-
-seba_counters* star::sc = new seba_counters;
