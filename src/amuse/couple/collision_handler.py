dbdc770038098473cb2c90322d4f7841f92a225a
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-08-09 15:25:46 +0000

    Added CollisionHandler + tests
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3822 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/couple/collision_handler.py
@@ -0,0 +1,109 @@
+import inspect
+
+from amuse.units import units
+from amuse.datamodel import Particle, Particles
+from amuse.support.exceptions import AmuseException
+
+
+class CollisionHandler(object):
+    """
+    Generic class for handling stellar collisions.
+    
+    
+    """
+
+    def __init__(
+            self, 
+            collision_code, 
+            collision_code_arguments = dict(),
+            collision_code_parameters = dict(),
+            gravity_code = None, 
+            stellar_evolution_code = None, 
+            verbose = False,
+            **options
+        ):
+        
+        if collision_code.stellar_evolution_code_required and stellar_evolution_code is None:
+            raise AmuseException("{0} requires a stellar evolution code: "
+                "CollisionHandler(..., stellar_evolution_code=x)".format(collision_code.__class__.__name__))
+        
+        self.collision_code = collision_code
+        self.collision_code_arguments = collision_code_arguments
+        self.collision_code_parameters = collision_code_parameters
+        
+        self.gravity_code = gravity_code
+        self.stellar_evolution_code = stellar_evolution_code
+        self.verbose = verbose
+        self.options = options
+    
+    def handle_collisions(self, primaries, secondaries):
+        result = Particles()
+        for primary, secondary in zip(primaries.as_set(), secondaries.as_set()):
+            result.add_particle(self.handle_collision(primary, secondary))
+        return result
+    
+    def handle_collision(self, primary, secondary):
+        colliders = (primary + secondary).copy_to_memory()
+        
+        if self.verbose:
+            print "Handling collision between stars with masses {0}.".format(colliders.mass)
+        
+        if inspect.isclass(self.collision_code):
+            collision_code = self.collision_code(**self.collision_code_arguments)
+            if hasattr(collision_code, "initialize_code"):
+                collision_code.initialize_code()
+            for par, value in self.collision_code_parameters.iteritems():
+                setattr(collision_code.parameters, par, value)
+            if hasattr(collision_code, "commit_parameters"):
+                collision_code.commit_parameters()
+        else:
+            collision_code = self.collision_code
+        
+        handle_collision_args = self.options.copy()
+        if collision_code.stellar_evolution_code_required:
+            handle_collision_args["stellar_evolution_code"] = self.stellar_evolution_code
+#~        if collision_code.gravity_code_required:
+#~            handle_collision_args["gravity_code"] = self.gravity_code
+        
+        merge_product = collision_code.handle_collision(primary, secondary, **handle_collision_args)
+        
+        if self.verbose:
+            print "{0} concluded with return value:\n{1}".format(self.collision_code.__class__.__name__, merge_product)
+        
+        stellar_model = None
+        if not self.stellar_evolution_code is None:
+            if hasattr(self.stellar_evolution_code, "new_particle_from_model"):
+                stellar_model = merge_product.internal_structure()
+                self.stellar_evolution_code.new_particle_from_model(
+                    stellar_model, 
+                    0.0 | units.Myr, 
+                    key = merge_product.key
+                )
+            else:
+                self.stellar_evolution_code.particles.add_particle(merge_product)
+            self.stellar_evolution_code.particles.remove_particles(colliders)
+            if self.verbose:
+                print "Colliders have been replaced by merge product in {0}.".format(self.stellar_evolution_code.__class__.__name__)
+        
+        if not self.gravity_code is None:
+            new_grav_particle = Particle(key=merge_product.key)
+            if stellar_model is None:
+                new_grav_particle.mass = merge_product.mass
+                if hasattr(merge_product, "radius"):
+                    new_grav_particle.radius = merge_product.radius
+                elif hasattr(colliders, "radius"):
+                    new_grav_particle.radius = max(colliders.radius)
+            else:
+                new_grav_particle.mass = stellar_model.mass[-1]
+                new_grav_particle.radius = stellar_model.radius[-1]
+            new_grav_particle.position = colliders.center_of_mass()
+            new_grav_particle.velocity = colliders.center_of_mass_velocity()
+            self.gravity_code.particles.add_particle(new_grav_particle)
+            self.gravity_code.particles.remove_particles(colliders)
+            if self.verbose:
+                print "Colliders have been replaced by merge product in {0}.".format(self.gravity_code.__class__.__name__)
+        
+        return merge_product
+    
+
+
---
+++ test/ext_tests/test_collision_handler.py
@@ -0,0 +1,262 @@
+from amuse.units import units
+from amuse.datamodel import Particles, Particle
+from amuse.support.exceptions import AmuseException
+from amuse.test.amusetest import TestCase
+
+from amuse.ext.sticky_spheres import StickySpheres
+from amuse.couple.collision_handler import CollisionHandler
+
+
+class CollisionCodeForTesting(object):
+    
+    class ParametersForTesting(object):
+        def __init__(self):
+            self.mass_unit = units.kg
+    
+    stellar_evolution_code_required = False
+    
+    def __init__(self, next_mass = 1 | units.kg):
+        self.next_mass = next_mass
+        self.parameters = self.ParametersForTesting()
+    
+    def handle_collision(self, primary, secondary, stellar_evolution_code=None):
+        set = Particles(1)
+        result = set[0]
+        result.mass = self.next_mass.as_quantity_in(self.parameters.mass_unit)
+        self.next_mass += 1 | units.kg
+        
+        if not stellar_evolution_code is None:
+            se_colliders = (primary + secondary).get_intersecting_subset_in(stellar_evolution_code.particles)
+            result.radius = se_colliders.radius.sum()
+            
+            def internal_structure(set, particle=None):
+                return dict(mass=result.mass, radius=result.radius)
+            
+            set.add_function_attribute("internal_structure", None, internal_structure)
+        
+        return result
+    
+
+class GravityCodeForTesting(object):
+    
+    def __init__(self):
+        self.particles = Particles(6)
+        self.particles.mass = 1 | units.MSun
+        self.particles.radius = 3 | units.RSun
+        self.particles.position = [[i,2*i,3*i] for i in range(6)] | units.AU
+        self.particles.velocity = [[i,i**2,i**3] for i in range(6)] | units.km / units.s
+    
+
+class StellarEvolutionCodeForTesting(object):
+    
+    def __init__(self, particles=Particles(6)):
+        particles.mass = 1 | units.MSun
+        particles.radius = range(1, len(particles)+1) | units.RSun
+        self.particles = particles
+    
+
+class StellarEvolutionCodeWithInternalStructureForTesting(object):
+    
+    def __init__(self, particles=Particles(6)):
+        particles.mass = 1 | units.MSun
+        particles.radius = range(1, len(particles)+1) | units.RSun
+        particles.type = "native star" | units.string
+        self.particles = particles
+    
+    def new_particle_from_model(self, internal_structure, current_age, key=None):
+        tmp_star = Particle(key=key)
+        tmp_star.mass = internal_structure["mass"]
+        tmp_star.radius = internal_structure["radius"]
+        tmp_star.type = "new particle from model" | units.string
+        return self.particles.add_particle(tmp_star)
+    
+
+
+class TestCollisionHandler(TestCase):
+    
+    def test1(self):
+        print "Test CollisionHandler with collision code class (creates new instance for each collision)"
+        colliders = Particles(2)
+        handler = CollisionHandler(CollisionCodeForTesting)
+        
+        result = handler.handle_collision(colliders[0], colliders[1])
+        self.assertTrue(isinstance(result, Particle))
+        self.assertEqual(result.mass, 1 | units.kg)
+        
+        result = handler.handle_collision(colliders[0], colliders[1])
+        self.assertEqual(result.mass, 1 | units.kg)
+    
+    def test2(self):
+        print "Test CollisionHandler with collision code instance (same instance for each collision)"
+        colliders = Particles(2)
+        handler = CollisionHandler(CollisionCodeForTesting())
+        
+        result = handler.handle_collision(colliders[0], colliders[1])
+        self.assertTrue(isinstance(result, Particle))
+        self.assertEqual(result.mass, 1 | units.kg)
+        
+        result = handler.handle_collision(colliders[0], colliders[1])
+        self.assertEqual(result.mass, 2 | units.kg)
+    
+    def test3(self):
+        print "Test CollisionHandler with collision code class, arguments and parameters"
+        colliders = Particles(2)
+        handler = CollisionHandler(
+            CollisionCodeForTesting, 
+            collision_code_arguments=dict(next_mass=5|units.kg),
+            collision_code_parameters=dict(mass_unit=units.g)
+        )
+        
+        result = handler.handle_collision(colliders[0], colliders[1])
+        self.assertTrue(isinstance(result, Particle))
+        self.assertEqual(result.mass, 5 | units.kg)
+        self.assertTrue(result.mass.unit is units.g)
+        
+        result = handler.handle_collision(colliders[0], colliders[1])
+        self.assertEqual(result.mass, 5 | units.kg)
+        self.assertTrue(result.mass.unit is units.g)
+        
+        handler.collision_code_arguments = dict(next_mass=42|units.kg)
+        handler.collision_code_parameters = dict(mass_unit=units.MSun)
+        result = handler.handle_collision(colliders[0], colliders[1])
+        self.assertEqual(result.mass, 42 | units.kg)
+        self.assertTrue(result.mass.unit is units.MSun)
+    
+    def test4(self):
+        print "Test handle_collisions"
+        colliders = Particles(8)
+        handler = CollisionHandler(CollisionCodeForTesting)
+        
+        result = handler.handle_collisions(colliders[:4], colliders[4:])
+        self.assertTrue(isinstance(result, Particles))
+        self.assertEqual(len(result), 4)
+        self.assertEqual(result.mass, [1, 1, 1, 1] | units.kg)
+    
+    def test5(self):
+        print "Test CollisionHandler with gravity code"
+        gravity = GravityCodeForTesting()
+        self.assertEqual(len(gravity.particles), 6)
+        
+        handler = CollisionHandler(CollisionCodeForTesting, gravity_code=gravity)
+        merged = handler.handle_collisions(gravity.particles[::2], gravity.particles[1::2])
+        self.assertTrue(isinstance(merged, Particles))
+        self.assertEqual(len(merged), 3)
+        self.assertEqual(merged.mass, [1, 1, 1] | units.kg)
+        
+        self.assertEqual(len(gravity.particles), 3)
+        self.assertEqual(gravity.particles.mass, [1, 1, 1] | units.kg)
+        self.assertEqual(gravity.particles.radius, [3, 3, 3] | units.RSun)
+        self.assertAlmostEqual(gravity.particles.position, 
+            [[0.5, 1.0, 1.5], [2.5, 5.0, 7.5], [4.5, 9.0, 13.5]] | units.AU)
+        self.assertAlmostEqual(gravity.particles.velocity, 
+            [[0.5, 0.5, 0.5], [2.5, 6.5, 17.5], [4.5, 20.5, 94.5]] | units.km / units.s)
+    
+    def test6(self):
+        print "Test CollisionHandler with stellar evolution code, type I"
+        stellar_evolution = StellarEvolutionCodeForTesting()
+        self.assertEqual(len(stellar_evolution.particles), 6)
+        
+        collision_code = CollisionCodeForTesting()
+        collision_code.stellar_evolution_code_required = True
+        
+        self.assertRaises(AmuseException, CollisionHandler, collision_code, expected_message=
+             "CollisionCodeForTesting requires a stellar evolution code: "
+             "CollisionHandler(..., stellar_evolution_code=x)")
+        
+        handler = CollisionHandler(collision_code, stellar_evolution_code=stellar_evolution)
+        merged = handler.handle_collisions(stellar_evolution.particles[::2], stellar_evolution.particles[1::2])
+        self.assertTrue(isinstance(merged, Particles))
+        self.assertEqual(len(merged), 3)
+        self.assertEqual(merged.mass, [1, 2, 3] | units.kg)
+        
+        self.assertEqual(len(stellar_evolution.particles), 3)
+        self.assertEqual(stellar_evolution.particles.mass, [1, 2, 3] | units.kg)
+        self.assertEqual(stellar_evolution.particles.radius, [3, 7, 11] | units.RSun)
+    
+    def test7(self):
+        print "Test CollisionHandler with stellar evolution code, type II"
+        stellar_evolution = StellarEvolutionCodeWithInternalStructureForTesting()
+        self.assertEqual(len(stellar_evolution.particles), 6)
+        self.assertEqual(stellar_evolution.particles.type, ["native star"]*6  | units.string)
+        
+        collision_code = CollisionCodeForTesting()
+        collision_code.stellar_evolution_code_required = True
+        
+        self.assertRaises(AmuseException, CollisionHandler, collision_code, expected_message=
+             "CollisionCodeForTesting requires a stellar evolution code: "
+             "CollisionHandler(..., stellar_evolution_code=x)")
+        
+        handler = CollisionHandler(collision_code, stellar_evolution_code=stellar_evolution)
+        merged = handler.handle_collisions(stellar_evolution.particles[::2], stellar_evolution.particles[1::2])
+        self.assertTrue(isinstance(merged, Particles))
+        self.assertEqual(len(merged), 3)
+        self.assertEqual(merged.mass, [1, 2, 3] | units.kg)
+        
+        self.assertEqual(len(stellar_evolution.particles), 3)
+        self.assertEqual(stellar_evolution.particles.mass, [1, 2, 3] | units.kg)
+        self.assertEqual(stellar_evolution.particles.radius, [3, 7, 11] | units.RSun)
+        self.assertEqual(stellar_evolution.particles.type, ["new particle from model"]*3 | units.string)
+    
+    def test8(self):
+        print "Test CollisionHandler with stellar evolution and gravity code"
+        gravity = GravityCodeForTesting()
+        self.assertEqual(len(gravity.particles), 6)
+        
+        stellar_evolution = StellarEvolutionCodeForTesting(particles=gravity.particles.copy_to_memory())
+        self.assertEqual(len(stellar_evolution.particles), 6)
+        
+        collision_code = CollisionCodeForTesting()
+        collision_code.stellar_evolution_code_required = True
+        
+        self.assertRaises(AmuseException, CollisionHandler, collision_code, expected_message=
+             "CollisionCodeForTesting requires a stellar evolution code: "
+             "CollisionHandler(..., stellar_evolution_code=x)")
+        
+        handler = CollisionHandler(
+            collision_code, 
+            stellar_evolution_code=stellar_evolution, 
+            gravity_code=gravity
+        )
+        merged = handler.handle_collisions(stellar_evolution.particles[::2], stellar_evolution.particles[1::2])
+        self.assertTrue(isinstance(merged, Particles))
+        self.assertEqual(len(merged), 3)
+        self.assertEqual(merged.mass, [1, 2, 3] | units.kg)
+        
+        self.assertEqual(len(stellar_evolution.particles), 3)
+        self.assertEqual(stellar_evolution.particles.mass, [1, 2, 3] | units.kg)
+        self.assertEqual(stellar_evolution.particles.radius, [3, 7, 11] | units.RSun)
+        
+        self.assertEqual(len(gravity.particles), 3)
+        self.assertEqual(gravity.particles.mass, [1, 2, 3] | units.kg)
+        self.assertEqual(gravity.particles.radius, [3, 7, 11] | units.RSun)
+        self.assertAlmostEqual(gravity.particles.position, 
+            [[0.5, 1.0, 1.5], [2.5, 5.0, 7.5], [4.5, 9.0, 13.5]] | units.AU)
+        self.assertAlmostEqual(gravity.particles.velocity, 
+            [[0.5, 0.5, 0.5], [2.5, 6.5, 17.5], [4.5, 20.5, 94.5]] | units.km / units.s)
+    
+    def test9(self):
+        print "Test CollisionHandler with gravity code and StickySpheres collision code"
+        gravity = GravityCodeForTesting()
+        self.assertEqual(len(gravity.particles), 6)
+        gravity.particles.mass = [1, 1, 2, 2, 3, 3,] | units.MSun
+        gravity.particles.radius = range(101, 107) | units.RSun
+        
+        collision_code = StickySpheres(mass_loss=0.1)
+        
+        handler = CollisionHandler(
+            collision_code, 
+            gravity_code=gravity
+        )
+        merged = handler.handle_collisions(gravity.particles[::2], gravity.particles[1::2])
+        self.assertTrue(isinstance(merged, Particles))
+        self.assertEqual(len(merged), 3)
+        self.assertEqual(merged.mass, 0.9 * ([2, 4, 6] | units.MSun))
+        
+        self.assertEqual(len(gravity.particles), 3)
+        self.assertEqual(gravity.particles.mass, 0.9 * ([2, 4, 6] | units.MSun))
+        self.assertEqual(gravity.particles.radius, [102, 104, 106] | units.RSun)
+        self.assertAlmostEqual(gravity.particles.position, 
+            [[0.5, 1.0, 1.5], [2.5, 5.0, 7.5], [4.5, 9.0, 13.5]] | units.AU)
+        self.assertAlmostEqual(gravity.particles.velocity, 
+            [[0.5, 0.5, 0.5], [2.5, 6.5, 17.5], [4.5, 20.5, 94.5]] | units.km / units.s)
+    
