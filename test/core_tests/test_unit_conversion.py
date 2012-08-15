c402f5c66e391d2f46362a836c8dfd11b6743ba5
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-08-15 13:50:06 +0000

    Added test for as_string_in function for quantities
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3867 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/core_tests/test_unit_conversion.py
+++ test/core_tests/test_unit_conversion.py
@@ -88,7 +88,14 @@ class TestUnitConversions(amusetest.TestCase):
         self.assertRaises(core.IncompatibleUnitsException, vel1.as_quantity_in, s / m, 
             expected_message = "Cannot express m / s in s / m, the units do not have the same bases")
     
-    
+    def test12(self):
+        self.assertEquals((1234 | g).as_string_in(g), '1234.0 g')
+        self.assertEquals((1234 | g).as_string_in(kg), '1.234 kg')
+        self.assertEquals((1.234 | kg).as_string_in(g), '1234.0 g')
+        self.assertEquals((1.0 | km * s**-1).as_string_in(m / s), '1000.0 m / s')
+        self.assertEquals((1.0 | km * s**-1).as_string_in(s**-1 * m), '1000.0 s**-1 * m')
+        self.assertEquals((1.0 | km / s).as_string_in((10*J/g)**0.5), '10.0 (10 * J / g)**0.5')
+
 class TestNonNumericUnits(amusetest.TestCase):
     def test1(self):
         string1 = "string" | string
