177c2631a258471e78ce09b38400b483484bfa99
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-08-20 07:40:29 +0000

    refactor turotial
    git-svn-id: file:///home/amuse/svn/amuse/trunk@3884 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- support/distribute/binbuild/build/convert_simple_examples_to_notebooks.py
+++ support/distribute/binbuild/build/convert_simple_examples_to_notebooks.py
@@ -4,18 +4,26 @@ import os.path
 import ast
 from IPython.nbformat import current as notebook_format
 
+from optparse import OptionParser
+
 def iter_python_files(path):
     globpath = os.path.join(path, '[a-zA-Z]*.py')
     return glob.iglob(globpath)
 
-def convert_file(path, outputdir):
+def convert_file(path, outputdir, is_convert_simple = False):
     filename = os.path.basename(path)
-    notebookname, _ = os.path.splitext(filename)
-    with open(path, 'r') as stream:
-        sourcestring = stream.read()
-        
-    notebook = new_notebook_from_string(notebookname, filename, sourcestring)
     
+    notebookname, _ = os.path.splitext(filename)
+    if is_convert_simple:
+        with open(path, 'r') as stream:
+            notebook = notebook_format.read(stream, u'py')
+    else:
+        with open(path, 'r') as stream:
+            sourcestring = stream.read()
+        
+        
+        notebook = new_notebook_from_string(notebookname, filename, sourcestring)
+        
     with open(os.path.join(outputdir, notebookname+'.ipynb'),'w') as stream:
         notebook_format.write(notebook, stream, u'json')
 
@@ -89,7 +97,38 @@ class DetermineBlocks(ast.NodeVisitor):
             
     def end(self):
         self.blocks.append( (self.kind, self.start_lineno, None) )
-        
-if __name__ == '__main__':
-    for x in iter_python_files('examples/simple'):
-        convert_file(x, 'output')
+    
+def new_option_parser():
+    result = OptionParser()
+    result.add_option(
+        "-s", "--simple",
+        dest="is_convert_simple", 
+        action="store_true", 
+        default=False,
+        help="use simple conversion, do not parse the file and create cells"
+    )
+    result.add_option(
+        "-o", "--output", 
+        default = "output",
+        dest="outputdir",
+        help="directory to put the converted ipython notebook files in"
+    )    
+    result.add_option(
+        "-i", "--input", 
+        default = ".",
+        dest="inputdir",
+        help="directory to read the python files from"
+    )
+    return result
+                
+
+def main(inputdir = ".", outputdir = "output", is_convert_simple = False):
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
+    for x in iter_python_files(inputdir):
+        convert_file(x, outputdir, is_convert_simple)
+
+    
+if __name__ == "__main__":
+    options, arguments = new_option_parser().parse_args()
+    main(**options.__dict__)
