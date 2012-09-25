697cc7b56f2cae3e303c7e8789a4e29a696483c7
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-09-25 07:51:37 +0000

    simpler script plus correct spaces
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4024 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- examples/simple/save_and_load_particles.py
+++ examples/simple/save_and_load_particles.py
@@ -7,40 +7,34 @@ available to read and write to a variety of formats.
 """
 
 from amuse.units import nbody_system
-from amuse.io import write_set_to_file
-
 from amuse.ic.plummer import new_plummer_model
+from amuse.plot import plot, native_plot
+
+# the imports needed to read and write a set
+from amuse.io import write_set_to_file
+from amuse.io import read_set_from_file
+
+import os.path
+
 if __name__ in ('__main__', '__plot__'):
+    if os.path.exists('plummer128.hdf'):
+        os.remove('plummer128.hdf')
+        
+    # generate a particle set
+    plummer=new_plummer_model(128)
 
-# generate a particle set
-  plummer=new_plummer_model(128)
+    # write the set to file 'testfile'
+    # the third argument is the file format, 'amuse' is an hdf5 based format
+    # that saves all information. Other formats
+    # available are e.g. csv, txt, gadget, starlab
+    write_set_to_file(plummer,'plummer128.hdf','amuse')
+    del plummer
 
-#write the set to file 'testfile'
-# the third argument is the file format, 'amuse' is an hdf5 based format
-# that saves all information. Other formats
-# available are e.g. csv, txt, gadget, starlab
-  write_set_to_file(plummer,'plummer128','amuse')
-  del plummer
+    # reading back the file
+    particles=read_set_from_file('plummer128.hdf','amuse')
 
-# reading back the file
-  from amuse.io import read_set_from_file
-  particles=read_set_from_file('plummer128','amuse')
-
-#plotting
-  from amuse.plot import *
-  plot(particles.x,particles.y,'r.')
-  native_plot.xlim(-5,5)
-  native_plot.ylim(-5,5)
-  native_plot.show()
-
-#running a simulation
-  from amuse.community.phiGRAPE.interface import PhiGRAPE
-  phi=PhiGRAPE()
-  phi.particles.add_particles(particles)
-
-  phi.evolve_model( 1| nbody_system.time)
-
-#saving the result
-  write_set_to_file(phi.particles,'evolvedplummer','amuse')
-
-  phi.stop()
+    # plotting
+    plot(particles.x, particles.y,'r.')
+    native_plot.xlim(-5,5)
+    native_plot.ylim(-5,5)
+    native_plot.show()
