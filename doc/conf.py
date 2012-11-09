38f1ba0a68a48af245f21f2e6adcc1f24930085c
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2012-11-09 14:34:49 +0000

    small changed to look of the documentation
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4131 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- doc/conf.py
+++ doc/conf.py
@@ -101,7 +101,7 @@ exclude_trees = ['build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
