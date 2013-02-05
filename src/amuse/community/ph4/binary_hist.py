2ca469b9330b3195c2aa82cf513e871c637307b8
awhitehead <awhitehead@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2013-02-05 11:39:47 +0000

    Script that generates a histogram of the binary energies.
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4256 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/community/ph4/binary_hist.py
@@ -0,0 +1,31 @@
+from pylab import *
+import sys
+import re
+
+if len(sys.argv) != 3:
+    print "usage: python binary_hist.py <input filename> <time of histogram>"
+    sys.exit(1)
+else:
+    fname = sys.argv[1]
+    time = float(sys.argv[2])
+    f = open(fname, "r")
+    inblock = False
+    EkTs = []
+    for line in f:
+        if re.search("%%% time= (\d+\.\d*)", line):
+            if float(re.search("%%% time= (\d+\.\d*)", line).group(1)) == time:
+                inblock = True
+        if inblock and re.search("%%% Multiple.*E/kT=(-?\d+\.\d+)", line):
+            EkTs.append(float(re.search("%%% Multiple.*E/kT=(-?\d+\.\d+)",
+                                        line).group(1)))
+
+        if inblock and re.search("%%% Emul/E", line):
+            inblock = False
+    f.close()
+
+    if len(EkTs) > 0:
+        hist(EkTs)
+        show()
+    else:
+        print "No binaries found at time = %f." % time
+
