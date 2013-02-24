bb0023eb5d0e8e74679b0bd4f149d68c72e4ebcf
pelupes <pelupes@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2013-02-24 14:34:18 +0000

    test changed to reflect name change
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4375 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/ext_tests/test_composition_methods.py
+++ test/ext_tests/test_composition_methods.py
@@ -39,7 +39,7 @@ def run_harmonic_oscillator(dt,method):
 
 class TestSymplecticCompositions(amusetest.TestCase):
   def test1(self):
-    from amuse.ext.symplectic_composition import LEAPFROG
+    from amuse.ext.composition_methods import LEAPFROG
     dt1=.1
     data=run_harmonic_oscillator(dt1,method=LEAPFROG)  
     de1=max(data['de'])    
@@ -49,7 +49,7 @@ class TestSymplecticCompositions(amusetest.TestCase):
     order=int(numpy.log(de2/de1)/numpy.log(dt2/dt1)+0.5)
     self.assertEqual(order,2)
   def test2(self):
-    from amuse.ext.symplectic_composition import SPLIT_4TH_S_M6
+    from amuse.ext.composition_methods import SPLIT_4TH_S_M6
     dt1=.5
     data=run_harmonic_oscillator(dt1,method=SPLIT_4TH_S_M6)  
     de1=max(data['de'])    
@@ -59,7 +59,7 @@ class TestSymplecticCompositions(amusetest.TestCase):
     order=int(numpy.log(de2/de1)/numpy.log(dt2/dt1)+0.5)
     self.assertEqual(order, 4)
   def test3(self):
-    from amuse.ext.symplectic_composition import SPLIT_4TH_S_M5
+    from amuse.ext.composition_methods import SPLIT_4TH_S_M5
     dt1=.5
     data=run_harmonic_oscillator(dt1,method=SPLIT_4TH_S_M5)  
     de1=max(data['de'])    
@@ -69,7 +69,7 @@ class TestSymplecticCompositions(amusetest.TestCase):
     order=int(numpy.log(de2/de1)/numpy.log(dt2/dt1)+0.5)
     self.assertEqual(order, 4)        
   def test4(self):
-    from amuse.ext.symplectic_composition import SPLIT_4TH_S_M4
+    from amuse.ext.composition_methods import SPLIT_4TH_S_M4
     dt1=.5
     data=run_harmonic_oscillator(dt1,method=SPLIT_4TH_S_M4)  
     de1=max(data['de'])    
@@ -79,7 +79,7 @@ class TestSymplecticCompositions(amusetest.TestCase):
     order=int(numpy.log(de2/de1)/numpy.log(dt2/dt1)+0.5)
     self.assertEqual(order, 4)
   def test5(self):
-    from amuse.ext.symplectic_composition import SPLIT_6TH_SS_M11
+    from amuse.ext.composition_methods import SPLIT_6TH_SS_M11
     dt1=.5
     data=run_harmonic_oscillator(dt1,method=SPLIT_6TH_SS_M11)  
     de1=max(data['de'])    
@@ -89,7 +89,7 @@ class TestSymplecticCompositions(amusetest.TestCase):
     order=int(numpy.log(de2/de1)/numpy.log(dt2/dt1)+0.5)
     self.assertEqual(order, 6)
   def test6(self):
-    from amuse.ext.symplectic_composition import SPLIT_6TH_SS_M13
+    from amuse.ext.composition_methods import SPLIT_6TH_SS_M13
     dt1=.5
     data=run_harmonic_oscillator(dt1,method=SPLIT_6TH_SS_M13)  
     de1=max(data['de'])    
@@ -99,7 +99,7 @@ class TestSymplecticCompositions(amusetest.TestCase):
     order=int(numpy.log(de2/de1)/numpy.log(dt2/dt1)+0.5)
     self.assertEqual(order, 6)                
   def test7(self):
-    from amuse.ext.symplectic_composition import SPLIT_8TH_SS_M21
+    from amuse.ext.composition_methods import SPLIT_8TH_SS_M21
     dt1=1.
     data=run_harmonic_oscillator(dt1,method=SPLIT_8TH_SS_M21)  
     de1=max(data['de'])    
@@ -109,7 +109,7 @@ class TestSymplecticCompositions(amusetest.TestCase):
     order=int(numpy.log(de2/de1)/numpy.log(dt2/dt1)+0.5)
     self.assertEqual(order, 8)
   def test8(self):
-    from amuse.ext.symplectic_composition import SPLIT_10TH_SS_M35
+    from amuse.ext.composition_methods import SPLIT_10TH_SS_M35
     dt1=1.
     data=run_harmonic_oscillator(dt1,method=SPLIT_10TH_SS_M35)  
     de1=max(data['de'])    
