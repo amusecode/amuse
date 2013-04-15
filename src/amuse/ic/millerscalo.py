62bcc4b9ca6f14241ba45f240342cac93222b3db
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2013-04-15 13:50:59 +0000

    Added __all__ to IMF files to fix the Python help()
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4541 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/ic/millerscalo.py
+++ src/amuse/ic/millerscalo.py
@@ -5,3 +5,4 @@ This module contains a function used to generate realisations of the
 Miller & Scalo (1979) initial mass function (IMF).
 """
 from amuse.ic.brokenimf import new_miller_scalo_mass_distribution
+__all__ = ["new_miller_scalo_mass_distribution"]
--- src/amuse/ic/scalo.py
+++ src/amuse/ic/scalo.py
@@ -5,3 +5,4 @@ This module contains a function used to generate realisations of the Scalo (1986
 initial mass function (IMF).
 """
 from amuse.ic.brokenimf import new_scalo_mass_distribution
+__all__ = ["new_scalo_mass_distribution"]
