4b49d96092bf02a604933d55ca946e169087de0b
pelupes <pelupes@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2013-05-06 15:52:10 +0000

    fi runinfo parser (thanks to mher)
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4579 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/io/fi_io.py
+++ src/amuse/io/fi_io.py
@@ -125,6 +125,46 @@ class FiFileFormatProcessor(base.FortranFileFormatProcessor):
         self.load_header(file)
         self.load_body(file)
         return self.gas,self.dark,self.star
+
+def parse_old_runinfo_file(path):
+    """parses the old runinfo file and returns its contents as a dict"""
+    
+    dict_ret = dict()
+    
+    for i, line in enumerate(open(path)):
+        
+        line = line.strip()
+        
+        if len(line) == 0 or line[0] == 'C':
+            continue
+        
+        if i == 1:
+            continue
+        
+        if i == 2:
+            key = 'datadir'
+            value = line
+            
+        if i >= 3:
+            
+            lineSplt = line.split()
+            
+            if ' - ' in line:
+                if  '..' not in line:
+                    value = True
+                    key = ''.join(lineSplt[3::])
+                else:
+                    continue
+            else:
+                value = lineSplt[0]
+                key = '-'.join(lineSplt[1::])
+                
+            print value, key
+
+            dict_ret[key] = value
+    
+    
+    return dict_ret    
         
 if __name__=="__main__":
     p=base.read_set_from_file("test2",format=FiFileFormatProcessor)  
