505a4a70ee6caf6fc1ea3577da7169069ec2a6ec
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2013-07-10 09:12:39 +0000

    upgraded zlib version number
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4726 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- doc/install/install.py
+++ doc/install/install.py
@@ -548,7 +548,7 @@ class InstallMatplotlib(InstallPrerequisites):
               (
                 'zlib' ,                   #name to refer by
                 [],                         #names of prerequisites (unused)
-                '1.2.7' ,                   #version string
+                '1.2.8' ,                   #version string
                 'zlib-', '.tar.gz',        #pre- and postfix for filename
                 'http://zlib.net/', #download url, filename is appended
                 self.basic_build             #method to use for building - same as for FFTW should work
