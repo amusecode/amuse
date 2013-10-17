b15a97aabcf41eace9178181d11c9b2f7e189b57
spz <spz@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2013-10-17 10:46:25 +0000

    Changed reference order and **-them
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4866 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/support/literature.py
+++ src/amuse/support/literature.py
@@ -27,9 +27,9 @@ LiteratureReference = namedtuple(
 
 class TrackLiteratureReferences(object):
     """
-        .. [#] Pelupessy, F. I. et al., 2013, The Astrophysical Multipurpose Software Environment, Astronomy and Astrophysics 557, 84
-        .. [#] Portegies Zwart, S. et al., 2013, Multi-physics Simulations Using a Hierarchical Interchangeable Software Interface, Computer Physics Communications 183, 456-468
-        .. [#] Portegies Zwart, S. et al., 2009, A multiphysics and multiscale software environment for modeling astrophysical systems, *New Astronomy*, **Volume 14**, **Issue 4**, 369-378
+        .. [#] ** Portegies Zwart, S. et al., 2013, Multi-physics Simulations Using a Hierarchical Interchangeable Software Interface, Computer Physics Communications 183, 456-468 [2013CoPhC.183..456P]
+        .. [#] ** Pelupessy, F. I. et al., 2013, The Astrophysical Multipurpose Software Environment, Astronomy and Astrophysics 557, 84 [2013A&A...557A..84P]
+        .. [#] Portegies Zwart, S. et al., 2009, A multiphysics and multiscale software environment for modeling astrophysical systems, *New Astronomy*, **Volume 14**, **Issue 4**, 369-378 [2009NewA...14..369P]
 
     """
     INSTANCE = None
