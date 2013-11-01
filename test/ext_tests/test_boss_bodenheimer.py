6ad7db93ac12ffea1afe2a7800adcb83d18bc78e
vriesn <vriesn@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2013-11-01 12:27:32 +0000

    Fixed tests for Boss Bodenheimer molecular cloud
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4912 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/ext_tests/test_boss_bodenheimer.py
+++ test/ext_tests/test_boss_bodenheimer.py
@@ -10,21 +10,23 @@ numpy.random.seed(1234567)
 
 class BossBodenheimerTests(amusetest.TestCase):
     def test1(self):
+        numpy.random.seed(1234)
         mc=bb79_cloud(targetN=1000).result
         self.assertEqual(len(mc),1000)
         ek=mc.kinetic_energy()
         ep=mc.potential_energy(G=nbody_system.G)
         eth=mc.thermal_energy()
-        self.assertAlmostRelativeEqual(eth/ep, -0.25,2)
-        self.assertAlmostRelativeEqual(ek/ep, -0.2,1)
+        self.assertAlmostEqual(eth/ep, -0.25, 2)
+        self.assertAlmostEqual(ek/ep, -0.2, 2)
 
     def test2(self):
+        numpy.random.seed(1234)
         convert=nbody_system.nbody_to_si(1. | units.MSun,3.2e16| units.cm)
         mc=bb79_cloud(targetN=1000,convert_nbody=convert).result
         self.assertEqual(len(mc),1000)
         ek=mc.kinetic_energy()
         ep=mc.potential_energy()
         eth=mc.thermal_energy()
-        self.assertAlmostRelativeEqual(eth/ep, -0.25,2)
-        self.assertAlmostRelativeEqual(ek/ep, -0.2,2)
+        self.assertAlmostEqual(eth/ep, -0.25, 2)
+        self.assertAlmostEqual(ek/ep, -0.2, 2)
 
