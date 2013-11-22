bc1033a73b6fd0bbca9d91e95697f944412c0281
pelupes <pelupes@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2013-11-22 10:22:01 +0000

    fix scaling, documentation, allow split results, of mameclot
    git-svn-id: file:///home/amuse/svn/amuse/trunk@4951 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/community/mameclot/interface.py
+++ src/amuse/community/mameclot/interface.py
@@ -2,7 +2,7 @@ import numpy
 import os
 
 from amuse.community import LiteratureReferencesMixIn
-from amuse.datamodel import Particles
+from amuse.datamodel import Particles,ParticlesWithUnitsConverted
 
 from subprocess import Popen, PIPE
 
@@ -26,12 +26,25 @@ class mameclot(LiteratureReferencesMixIn):
     l_hat = l_orb/<r_vir*sigma>
     eta => defines the relative radii: r2/r1 = q^eta, for example:
     eta=[-0.33/0/0.33/0.5] for equal [t_rh/r_h/rho_h/Sigma_h]
-
-    Mass, pos[3], vel[3] are written to standard output
-    System diagnostics are written to the standard error
     
     Relevant references:
         .. [#] Gieles, M., https://github.com/mgieles/mameclot
+
+    :argument targetN: Number of particles
+    :argument convert_nbody: When given will convert the resulting set to SI units
+    :argument cluster_model: cluster model to generate ["Henon"]
+    :argument mass_ratio: mass ratio, zero means only 1 cluster [0]
+    :argument size_eta_parameter: eta, relative cluster sizes: r2/r1 = q^eta [0.333]
+    :argument imf: IMF [single mass]
+    :argument angular_momentum_signs: Angular momentum in z-direction, -, + or x ["xx"]
+    :argument fraction_of_max_rot_energy: Fraction of maximum rotational energy   [1],
+    :argument OsipkovMerritt_anisotropy_radius_r0: Osipkov-Merritt anisotropy radius in units of r_0 [999]
+    :argument cutoff_radius_halfmass_radius: Cut-off radius in units of r_h [20]
+    :argument physical_radius: Physical scale [1| units.parsec]
+    :argument distance: Distance between 2 clusters in N-body units [20 | nbody_system.length]
+    :argument orbital_energy: Dimensionless orbital energy of two-cluster system [0]
+    :argument orbital_angular_momentum: Dimensionless orbital angular momentum two-cluster system [4]
+    :argument seed: random seed 123456
     """
     
     available_cluster_models={"Dehnen" : 0, "Hernquist":1 ,"Jaffe":2,"Henon": 3, "Plummer" :4 }
@@ -42,13 +55,13 @@ class mameclot(LiteratureReferencesMixIn):
         imf="single mass", angular_momentum_signs="xx", fraction_of_max_rot_energy=1,
         OsipkovMerritt_anisotropy_radius_r0=999,cutoff_radius_halfmass_radius=20,physical_radius=1| units.parsec,
         distance=20 | nbody_system.length,orbital_energy=0,orbital_angular_momentum=0,seed=123456,
-        convert_nbody=None):
+        convert_to_physical=False):
         
         LiteratureReferencesMixIn.__init__(self)
 
         self._bin_path = os.path.dirname(os.path.abspath(__file__))
         self._particles=None
-        self.convert_nbody=convert_nbody
+        self.convert_to_physical=convert_to_physical
         self._exec="mameclot_worker"
         
         self.targetN=targetN
@@ -84,8 +97,7 @@ class mameclot(LiteratureReferencesMixIn):
         arguments.extend([ "-s", str(self.seed) ])                              
         return arguments
                 
-    @property
-    def result(self):
+    def make_model(self):
     
         call=[self._exec]+self.arguments()
         
@@ -98,6 +110,26 @@ class mameclot(LiteratureReferencesMixIn):
         print err
         
         outsplit=out.strip().split("\n")
+        errsplit=err.strip().split("\n")
+        
+        if self.mass_ratio==0:
+          nline=errsplit[6].split()
+          mline=errsplit[7].split()
+          rline=errsplit[8].split()
+          N1=int(nline[2])
+          N2=0
+          mscale=(float(mline[4])/float(mline[2])) | units.MSun
+          rscale=(float(rline[4])/float(mline[2])) | units.parsec
+        else:
+          nline=errsplit[8].split()
+          n2line=errsplit[22].split()
+          mline=errsplit[9].split()
+          rline=errsplit[10].split()
+          N1=int(nline[2])
+          N2=int(n2line[2])
+          mscale=(float(mline[4])/float(mline[2])) | units.MSun
+          rscale=(float(rline[4])/float(mline[2])) | units.parsec
+        print N1,N2
         
         N=len( outsplit)
         
@@ -123,20 +155,76 @@ class mameclot(LiteratureReferencesMixIn):
   
         parts.move_to_center()
   
-        if not self.convert_nbody is None:
-            parts = datamodel.ParticlesWithUnitsConverted(result, self.convert_nbody.as_converter_from_si_to_generic())
+        if self.convert_to_physical:
+            print "mass scale:", mscale
+            print "length scale:", rscale  
+
+            convert_nbody=nbody_system.nbody_to_si(mscale,rscale)
+            parts = ParticlesWithUnitsConverted(parts, convert_nbody.as_converter_from_si_to_generic())
             parts = parts.copy()
   
-        return parts
+        self._all=parts
+        self._cluster1=parts[:N1]
+        self._cluster2=parts[N1:]
 
+
+    @property
+    def result(self):
+        self.make_model()
+        return self._all
+
+    @property
+    def result_split(self):
+        self.make_model()
+        return self._all,self._cluster1,self._cluster2
+
+def new_mameclot_model(*args,**keyword_arguments):
+    """
+
+    MAMECLOT: MAke ME a CLuster Or Two
+  
+    Construct initial conditions of 1 (or 2) cluster(s) for N-body simulations 
+  
+    A 2 cluster system with orbit in the x-y plane is made if the mass ratio q>0 
+    Five equilibrium models are available, a cut-off radius can be provided
+    The velocities can be isotropic or radially anisotropic (a la Osipkov-Merritt)
+    Optional angular momentum by aligning the angular momentum vectors along z
+    System is scaled to N-body units with G = M = -4E = 1 (Heggie & Mathieu 1986)
+    In the case of a two-body system E = mu*e_orb + E_1 + E_2 (mu = reduced mass) 
+    The 2-body orbit is computed from input q, e_hat and l_hat (and eta if q<1):
+    
+    e_hat = e_orb/0.5<sigma^2> 
+    l_hat = l_orb/<r_vir*sigma>
+    eta => defines the relative radii: r2/r1 = q^eta, for example:
+    eta=[-0.33/0/0.33/0.5] for equal [t_rh/r_h/rho_h/Sigma_h]
+
+    :argument targetN: Number of particles
+    :argument convert_nbody:  When given will convert the resulting set to SI units
+    :argument cluster_model: cluster model to generate ["Henon"]
+    :argument mass_ratio: mass ratio, zero means only 1 cluster [0]
+    :argument size_eta_parameter:eta: Relative cluster sizes: r2/r1 = q^eta [0.333]
+    :argument imf: IMF ["single mass"]
+    :argument angular_momentum_signs: Angular momentum in z-direction, -, + or x ["xx"]
+    :argument fraction_of_max_rot_energy: Fraction of maximum rotational energy   [1],
+    :argument OsipkovMerritt_anisotropy_radius_r0: Osipkov-Merritt anisotropy radius in units of r_0 [999]
+    :argument cutoff_radius_halfmass_radius: Cut-off radius in units of r_h [20]
+    :argument physical_radius: Physical scale [1| units.parsec]
+    :argument distance: Distance between 2 clusters in N-body units [20 | nbody_system.length]
+    :argument orbital_energy: Dimensionless orbital energy of two-cluster system [0]
+    :argument orbital_angular_momentum: Dimensionless orbital angular momentum two-cluster system [4]
+    :argument seed: random seed 123456
+    """
+    uc = mameclot(*args, **keyword_arguments)
+    return uc.result
 
 if __name__=="__main__":
     from matplotlib import pyplot
     
-    cluster=mameclot().result
+    clusters,cluster1,cluster2=mameclot(mass_ratio=0.25,convert_to_physical=False).result_split
     
-    print cluster.total_mass()
-    print len(cluster)
+    print cluster1.total_mass()
+    print len(cluster2)
     
-    pyplot.plot(cluster.x.number,cluster.y.number,"r.")
+    pyplot.plot(cluster1.x.number,cluster1.y.number,"r.")
+    pyplot.plot(cluster2.x.number,cluster2.y.number,"g.")
     pyplot.show()
--- test/ext_tests/test_mameclot.py
+++ test/ext_tests/test_mameclot.py
@@ -11,4 +11,18 @@ class TestPlummer(amusetest.TestCase):
     
           self.assertAlmostEqual(cluster.total_mass().number, 1.0)
           self.assertEqual(len(cluster),10000)
+          
+    def test2(self):
+          clusters,c1,c2=mameclot().result_split
+    
+          self.assertAlmostEqual((c1.total_mass()+c2.total_mass()).number, 1.0)
+          self.assertEqual(len(c1),10000)          
+          self.assertEqual(len(c2),0)
+
+    def test3(self):
+          clusters,c1,c2=mameclot(mass_ratio=0.25).result_split
+    
+          self.assertAlmostEqual((c1.total_mass()+c2.total_mass()).number, 1.0)
+          self.assertEqual(len(c1),8000)          
+          self.assertEqual(len(c2),2000)                    
 
