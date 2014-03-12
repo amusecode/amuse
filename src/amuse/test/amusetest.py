79afdcef952f2b081e42e85205c53c317032421a
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2014-03-12 14:06:17 +0000

    fix test for older python versions
    git-svn-id: file:///home/amuse/svn/amuse/trunk@5220 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/test/amusetest.py
+++ src/amuse/test/amusetest.py
@@ -148,7 +148,13 @@ class TestCase(unittest.TestCase):
         if not a >= b:
             standardMsg = '%s not greater than or equal to %s' % (safe_repr(a), safe_repr(b))
             self.fail(self._formatMessage(msg, standardMsg))
-
+    
+    def assertLess(self, a, b, msg=None):
+        """Just like self.assertTrue(a < b), but with a nicer default message."""
+        if not a < b:
+            standardMsg = '%s not less than %s' % (safe_repr(a), safe_repr(b))
+            self.fail(self._formatMessage(msg, standardMsg))
+    
     def run(self, result=None):
         if hasattr(unittest, 'SkipTest'):
             return unittest.TestCase.run(self, result)
