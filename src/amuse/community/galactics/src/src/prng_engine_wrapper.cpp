4c236feffc5865946c8add069103aec7853d35db
vanelteren <vanelteren@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2014-06-05 09:41:52 +0000

    bugfix for galactics code
    git-svn-id: file:///home/amuse/svn/amuse/trunk@5440 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/community/galactics/src/src/prng_engine_wrapper.cpp
+++ src/amuse/community/galactics/src/src/prng_engine_wrapper.cpp
@@ -1,10 +1,36 @@
-#include <random>
-#include <functional>
+//#include <random>
+//#include <functional>
+#include <limits>
+#include <cmath>
+#include <iostream>
 
 #include "prng_engine.hpp"
 
 #define SEED  1234321
 #define SKIP  100000
+
+/*
+ * copied from standard c++ library
+ */
+template<typename _RealType,
+        typename _UniformRandomNumberGenerator>
+    _RealType
+    generate_canonical(_UniformRandomNumberGenerator& __urng)
+    {
+      const size_t __b = static_cast<size_t>(std::numeric_limits<_RealType>::digits);
+      const long double __r = static_cast<long double>(__urng.max())
+                - static_cast<long double>(__urng.min()) + 1.0L;
+      const size_t __log2r = std::log(__r) / std::log(2.0L);
+      size_t __k = std::max<size_t>(1UL, (__b + __log2r - 1UL) / __log2r);
+      _RealType __sum = _RealType(0);
+      _RealType __tmp = _RealType(1);
+      for (; __k != 0; --__k)
+      {
+          __sum += _RealType(__urng() - __urng.min()) * __tmp;
+          __tmp *= __r;
+      }
+      return __sum / __tmp;
+    }
 
 sitmo::prng_engine & global_urng()
 {
@@ -20,7 +46,6 @@ extern "C" void ran_seed(long long i)
 
 extern "C" float ran1(int * idum)
 {
-     static std::uniform_real_distribution<float> d(0.,1.);
-     return d( global_urng() );
+     return generate_canonical<float, sitmo::prng_engine>( global_urng() );
 }
  
