84cfe4ce7fe9f2944fe14ee0fef462843e57ec95
pelupes <pelupes@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2014-11-07 08:50:17 +0000

    krome, a chemical evolution code
    git-svn-id: file:///home/amuse/svn/amuse/trunk@5796 6e832d89-e7d1-4ad1-8ca1-873beecccf65

---
+++ src/amuse/community/krome/src/amuse_helpers.f90
@@ -0,0 +1,289 @@
+module chem_mod
+  use krome_main !use krome (mandatory)
+  use krome_user !use utility (for krome_idx_* constants and others)
+  implicit none
+
+  type particle_type
+    integer :: id
+    double precision :: density
+    double precision :: temperature
+    double precision :: ionrate
+    double precision :: abundances(krome_nmols)
+  end type
+
+  type(particle_type), allocatable :: particles(:)
+  
+  double precision :: tcurrent  ! time unit = yr
+  
+  integer :: nparticle
+  integer :: tot_id
+
+  integer, parameter :: NMAX=1000000
+
+  logical :: particles_searcheable=.FALSE.
+  integer, allocatable :: pid(:)
+
+contains
+
+  function chem_initialize()  result(ret)
+    integer :: ret
+    tcurrent=0.
+    nparticle=0
+    tot_id=0
+    if(.not.allocated(particles)) allocate(particles(NMAX))
+    particles(:)%density=0.
+    
+    call krome_init()
+    
+    ret=0
+  end function
+
+  function chem_end() result(ret)
+     integer :: ret 
+     if(allocated(particles)) deallocate(particles)
+     ret=0
+  end function
+
+  function chem_commit_parameters() result(ret)
+    integer :: ret
+    ret=0
+  end function
+
+  function chem_commit_particles() result (ret)
+    integer :: ret
+    integer :: n
+    integer :: i
+ 
+    particles_searcheable=.FALSE.
+    nparticle=clean_particles(particles)
+    ret=0
+  end function
+
+  function chem_model_time(outtime) result(ret)
+    integer :: ret
+    double precision :: outtime
+    ret=0
+    outtime=tcurrent
+  end function
+
+  function evolve_chem_model(tend) result(ret)
+    integer :: ret
+    double precision :: tend,dt
+    integer :: i,iret
+    ret=0
+    dt=tcurrent-tend
+    do i=1,nparticle
+      iret=evolve_1_particle(particles(i),dt)
+      ret=min(iret,ret)
+    enddo
+    tcurrent=tend
+  end function
+
+  function evolve_1_particle(particle,dt) result(ret)
+    integer :: ret
+    type(particle_type) :: particle
+    double precision :: tend,dt
+    double precision :: n(krome_nmols),T
+
+    n=particle%abundances*particle%density
+    T = particle%temperature
+    call krome(n, T, dt)
+    particle%temperature=T
+    particle%abundances=n/particle%density
+    ret=0
+  end function
+
+  function get_particle_abundance(id, aid, abundance) result(ret)
+    integer :: ret
+    integer :: id,index,aid
+    double precision :: abundance
+    index=find_particle(id)
+    if(index.LT.0) then
+      ret=index
+      return
+    endif
+    abundance=particles(index)%abundances(aid)
+    ret=0
+  end function
+
+  function set_particle_abundance(id, aid, abundance) result(ret)
+    integer :: ret
+    integer :: id,index,aid
+    double precision :: abundance
+    index=find_particle(id)
+    if(index.LT.0) then
+      ret=index
+      return
+    endif
+    particles(index)%abundances(aid)=abundance
+    ret=0
+  end function
+
+  function set_particle_state(id,density,temperature,ionrate) result(ret)
+    integer :: ret
+    integer :: id,index
+    double precision :: density, temperature, ionrate
+
+    index=find_particle(id)
+    if(index.LT.0) then
+      ret=index
+      return
+    endif
+
+    particles(index)%density=density
+    particles(index)%temperature=temperature
+    particles(index)%ionrate=ionrate
+
+  end function
+
+  function get_particle_state(id,density,temperature,ionrate) result(ret)
+    integer :: ret
+    integer :: id,index
+    double precision :: density, temperature, ionrate
+
+    index=find_particle(id)
+
+    if(index.LT.0) then
+      ret=index
+      return
+    endif
+
+    density=particles(index)%density
+    temperature=particles(index)%temperature
+    ionrate=particles(index)%ionrate
+    ret=0
+
+  end function
+
+  function add_particle(id,density,temperature,ionrate) result(ret)
+  integer :: ret
+  integer :: i,id
+  double precision :: density, temperature,ionrate
+  particles_searcheable=.FALSE.
+  id=new_id()  
+  i=nparticle+1
+
+  if(i.GT.NMAX) then
+    ret=-1
+    return
+  endif
+  particles(i)%id=id
+  particles(i)%density=density
+  particles(i)%temperature=temperature
+  particles(i)%ionrate=ionrate
+  particles(i)%abundances=0
+  nparticle=nparticle+1
+  ret=0
+  end function
+
+  function remove_particle(id) result(ret)
+  integer :: ret
+  integer :: i,id
+  i=find_particle(id)
+  if(i.LE.0) then
+    ret=i
+    return
+  endif
+  if(particles(i)%density.LT.0) then
+    ret=-4
+    return
+  endif
+  particles(i)%density=-1.
+  ret=0
+  end function
+
+function clean_particles(par) result(np)
+  integer :: left,right,np
+  type(particle_type), allocatable :: par(:)
+  type(particle_type) :: tmp
+  left=1
+  if(.NOT.allocated(par)) then
+    np = 0 
+    return  
+  endif
+  right=size(par)
+  if(right.EQ.0) then
+    np=0
+    return
+  endif 
+  do while(.TRUE.)
+    do while(par(left)%density.GT.0.AND.left.LT.right)
+      left=left+1
+    enddo
+    do while(par(right)%density.LE.0.AND.left.LT.right)
+      right=right-1  
+    enddo
+    if(left.LT.right) then
+      tmp=par(left)
+      par(left)=par(right)
+      par(right)=tmp
+    else
+      exit
+    endif
+  enddo
+  if(par(left)%density.GT.0) left=left+1
+  np=left-1
+end function
+
+function find_particle(id_) result(index)
+  use hashMod
+  type(hash_type),save ::  hash
+  integer id_,index
+  integer, save :: nbod=0
+  
+  if(.NOT.particles_searcheable) then
+    nbod=nparticle
+    if(allocated(pid)) deallocate(pid)
+    allocate(pid(nbod))
+    pid(1:nbod)=particles(1:nbod)%id
+    call initHash(nbod/2+1,nbod, pid,hash)
+    particles_searcheable=.TRUE.
+  endif
+  
+
+  index=find(id_,pid,hash)
+
+  if(index.LE.0) then   
+    index=-1
+    return
+  endif
+  if(index.GT.nbod) then
+    index=-2
+    return
+  endif
+  if(pid(index).NE.id_) then   
+    index=-3
+    return
+  endif
+  
+end function
+
+subroutine extend_particles(buf,n)
+  type(particle_type), allocatable, intent (inout) :: buf(:)
+  type(particle_type), allocatable :: tmpbuf(:)
+  integer :: n,m
+
+  m=0
+  if(allocated(buf)) then
+    m=min(n,size(buf)) 
+    allocate(tmpbuf(m))
+    tmpbuf(1:m)=buf(1:m)
+    deallocate(buf)
+  endif
+    
+  allocate(buf(n))
+
+  if(m.GT.0 .and. allocated(tmpbuf)) then
+    buf(1:m)=tmpbuf(1:m)
+    deallocate(tmpbuf)
+  endif   
+  
+end subroutine
+
+function new_id()
+  integer new_id
+  tot_id=tot_id+1
+  new_id=tot_id
+end function
+
+end module
---
+++ src/amuse/community/krome/test_krome.py
@@ -0,0 +1,481 @@
+import os.path
+import numpy
+from amuse.test.amusetest import TestWithMPI
+
+from amuse.community.krome.interface import KromeInterface,Krome,solar_abundances
+from amuse.units import units
+from amuse.datamodel import Particles
+
+from amuse.io import read_set_from_file
+
+default_options = dict(redirection="none")
+#default_options=dict(debugger="gdb")
+
+class TestKromeInterface(TestWithMPI):
+
+    def test1(self):
+        print "Test 1: initialization"
+
+        instance = KromeInterface(**default_options)
+        self.assertEqual(0, instance.initialize_code())
+        self.assertEqual(0, instance.commit_parameters())
+        self.assertEqual(0, instance.cleanup_code())
+        instance.stop()
+
+    def test2(self):
+        print "Test 1: add particle, get state"
+
+        instance = KromeInterface(**default_options)
+        self.assertEqual(0, instance.initialize_code())
+        self.assertEqual(0, instance.commit_parameters())
+
+        dens=1.e5
+        t=500.
+        ion=1.e-11
+        id,err=instance.new_particle(dens,t,ion)
+
+        self.assertEqual(err,0)
+      
+        self.assertEqual(instance.commit_particles(),0)
+
+        dens_,t_,ion_,err=instance.get_state(id)
+
+        self.assertEqual(err,0)
+
+        self.assertEqual(dens_,dens)
+        self.assertEqual(t_,t)
+        self.assertEqual(ion_,ion)
+
+        self.assertEqual(0, instance.cleanup_code())
+
+        instance.stop()
+        
+    def test3(self):
+        print "Test 1: add 2 particles, get state"
+
+        instance = KromeInterface(**default_options)
+        self.assertEqual(0, instance.initialize_code())
+        self.assertEqual(0, instance.commit_parameters())
+
+        dens=[1.e5,2.e5]
+        t=[500.,550]
+        ion=[1.e-11,2.e-11]
+        id,err=instance.new_particle(dens,t,ion)
+
+        self.assertEqual(err,0)
+      
+        self.assertEqual(instance.commit_particles(),0)
+
+        for i in range(2):
+          dens_,t_,ion_,err=instance.get_state(id[i])
+
+          self.assertEqual(err,0)
+
+          self.assertEqual(dens_,dens[i])
+          self.assertEqual(t_,t[i])
+          self.assertEqual(ion_,ion[i])
+
+        self.assertEqual(0, instance.cleanup_code())
+
+        instance.stop()
+
+    def test4(self):
+        print "Test 1: add 100 particles, get state"
+
+        instance = KromeInterface(**default_options)
+        self.assertEqual(0, instance.initialize_code())
+        self.assertEqual(0, instance.commit_parameters())
+
+        dens=1.e5*numpy.random.random(100)
+        t=500.*numpy.random.random(100)
+        ion=1.e-11*numpy.random.random(100)
+        id,err=instance.new_particle(dens,t,ion)
+
+        self.assertEqual(err,0)
+      
+        self.assertEqual(instance.commit_particles(),0)
+
+        for i in range(100):
+          dens_,t_,ion_,err=instance.get_state(id[i])
+
+          self.assertEqual(err,0)
+
+          self.assertEqual(dens_,dens[i])
+          self.assertEqual(t_,t[i])
+          self.assertEqual(ion_,ion[i])
+
+        self.assertEqual(0, instance.cleanup_code())
+
+        instance.stop()
+
+    def test5(self):
+        print "Test 5: can we get species?"
+
+        instance = KromeInterface(**default_options)
+        
+        first,last,err=instance.get_firstlast_abundance()
+        self.assertEqual(err,0)
+        self.assertTrue(last-first > 0)
+        
+        for i in range(first,last+1):
+          name,err=instance.get_name_of_species(i)
+          self.assertEqual(err,0)
+          index,err=instance.get_index_of_species(name)
+          self.assertEqual(i,index)
+
+        instance.stop()
+
+    #~ def test5(self):
+        #~ print "Test 1: simple evolve test"
+#~ 
+        #~ instance = TDCInterface(**default_options)
+        #~ self.assertEqual(0, instance.initialize_code())
+        #~ self.assertEqual(0, instance.commit_parameters())
+#~ 
+        #~ dens=1.e5
+        #~ t=500.
+        #~ ion=1.e-11
+        #~ id,err=instance.new_particle(dens,t,ion)
+#~ 
+        #~ self.assertEqual(err,0)
+      #~ 
+        #~ self.assertEqual(instance.commit_particles(),0)
+#~ 
+        #~ dens_,t_,ion_,err=instance.get_state(id)
+#~ 
+        #~ self.assertEqual(err,0)
+#~ 
+        #~ self.assertEqual(dens_,dens)
+        #~ self.assertEqual(t_,t)
+        #~ self.assertEqual(ion_,ion)
+#~ 
+        #~ err=instance.evolve_model( 94.112609921226252)
+#~ 
+        #~ x,err=instance.get_abundance(id,1)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,0.38895405860649546,7)
+        #~ x,err=instance.get_abundance(id,2)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,.30526706600870018,7)
+        #~ x,err=instance.get_abundance(id,3)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,1.47184343246716795E-004,7)
+        #~ x,err=instance.get_abundance(id,4)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,7.08149953046615757E-008,7)
+        #~ x,err=instance.get_abundance(id,5)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x, 6.52897069295877081E-005,7)
+#~ 
+        #~ self.assertEqual(0, err)
+#~ 
+        #~ self.assertEqual(0, instance.cleanup_code())
+#~ 
+        #~ instance.stop()
+#~ 
+    #~ def test6(self):
+        #~ print "Test 1: simple evolve test 2"
+#~ 
+        #~ instance = TDCInterface(**default_options)
+        #~ self.assertEqual(0, instance.initialize_code())
+        #~ self.assertEqual(0, instance.commit_parameters())
+#~ 
+        #~ dens=1.e5
+        #~ t=500.
+        #~ ion=1.e-11
+        #~ id,err=instance.new_particle(dens,t,ion)
+#~ 
+        #~ self.assertEqual(err,0)
+      #~ 
+        #~ self.assertEqual(instance.commit_particles(),0)
+#~ 
+        #~ dens_,t_,ion_,err=instance.get_state(id)
+#~ 
+        #~ self.assertEqual(err,0)
+#~ 
+        #~ self.assertEqual(dens_,dens)
+        #~ self.assertEqual(t_,t)
+        #~ self.assertEqual(ion_,ion)
+#~ 
+        #~ err=instance.evolve_model( 50.)
+        #~ err=instance.evolve_model( 94.112609921226252)
+#~ 
+        #~ x,err=instance.get_abundance(id,1)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,0.38895405860649546,3)
+        #~ x,err=instance.get_abundance(id,2)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,.30526706600870018,3)
+        #~ x,err=instance.get_abundance(id,3)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,1.47184343246716795E-004,3)
+        #~ x,err=instance.get_abundance(id,4)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,7.08149953046615757E-008,3)
+        #~ x,err=instance.get_abundance(id,5)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x, 6.52897069295877081E-005,3)
+#~ 
+        #~ self.assertEqual(0, err)
+#~ 
+        #~ self.assertEqual(0, instance.cleanup_code())
+#~ 
+        #~ instance.stop()
+#~ 
+    #~ def test7(self):
+        #~ print "Test 1: simple evolve test 3"
+#~ 
+        #~ instance = TDCInterface(**default_options)
+        #~ self.assertEqual(0, instance.initialize_code())
+        #~ self.assertEqual(0, instance.commit_parameters())
+#~ 
+        #~ dens=1.e5
+        #~ t=500.
+        #~ ion=1.e-11
+        #~ id1,err=instance.new_particle(dens,t,ion)
+        #~ id2,err=instance.new_particle(dens,t,ion)
+#~ 
+        #~ self.assertEqual(err,0)
+      #~ 
+        #~ self.assertEqual(instance.commit_particles(),0)
+#~ 
+        #~ err=instance.evolve_model( 94.112609921226252)
+#~ 
+        #~ x,err=instance.get_abundance(id1,1)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,0.38895405860649546,7)
+        #~ x,err=instance.get_abundance(id1,2)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,.30526706600870018,7)
+        #~ x,err=instance.get_abundance(id1,3)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,1.47184343246716795E-004,7)
+        #~ x,err=instance.get_abundance(id1,4)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,7.08149953046615757E-008,7)
+        #~ x,err=instance.get_abundance(id1,5)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x, 6.52897069295877081E-005,7)
+#~ 
+        #~ x,err=instance.get_abundance(id2,1)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,0.38895405860649546,7)
+        #~ x,err=instance.get_abundance(id2,2)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,.30526706600870018,7)
+        #~ x,err=instance.get_abundance(id2,3)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,1.47184343246716795E-004,7)
+        #~ x,err=instance.get_abundance(id2,4)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,7.08149953046615757E-008,7)
+        #~ x,err=instance.get_abundance(id2,5)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x, 6.52897069295877081E-005,7)
+#~ 
+        #~ self.assertEqual(0, err)
+#~ 
+        #~ self.assertEqual(0, instance.cleanup_code())
+#~ 
+        #~ instance.stop()
+#~ 
+    #~ def test8(self):
+        #~ print "Test 1: simple evolve test 4"
+#~ 
+        #~ instance = TDCInterface(**default_options)
+        #~ self.assertEqual(0, instance.initialize_code())
+        #~ self.assertEqual(0, instance.commit_parameters())
+#~ 
+        #~ dens=1.e5
+        #~ t=500.
+        #~ ion=1.e-11
+        #~ id1,err=instance.new_particle(dens,t,ion)
+        #~ id2,err=instance.new_particle(2*dens,t,ion)
+#~ 
+        #~ self.assertEqual(err,0)
+      #~ 
+        #~ self.assertEqual(instance.commit_particles(),0)
+        #~ 
+        #~ err=instance.evolve_model( 94.112609921226252)
+#~ 
+        #~ x,err=instance.get_abundance(id1,3)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,1.47184343246716795E-004,7)
+        #~ x,err=instance.get_abundance(id2,3)
+        #~ self.assertEqual(0,err)
+        #~ self.assertAlmostRelativeEqual(x,6.7890303322737115e-05,7)
+#~ 
+        #~ instance.stop()
+#~ 
+#~ 
+#~ class TestTDC(TestWithMPI):
+    #~ 
+    #~ def is_fortan_version_up_to_date(self):
+        #~ try:
+            #~ from amuse import config
+            #~ is_configured = hasattr(config, 'compilers')
+            #~ if is_configured:
+                #~ is_configured = hasattr(config.compilers, 'gfortran_version')
+        #~ except ImportError:
+            #~ is_configured = False
+    #~ 
+        #~ if is_configured:
+            #~ if not config.compilers.gfortran_version:
+                #~ return True
+            #~ 
+            #~ try:
+                #~ parts = [int(x) for x in config.compilers.gfortran_version.split('.')]
+            #~ except:
+                #~ parts = []
+                #~ 
+            #~ if len(parts) < 2:
+                #~ return True
+                #~ 
+            #~ return parts[0] >= 4 and parts[1] > 1
+        #~ else:
+            #~ return True
+            #~ 
+    #~ def check_fortran_version(self):
+        #~ if not self.is_fortan_version_up_to_date():
+            #~ self.skip('cannot compile, fortran module names cannot be resolved correctly in this gfortran version')
+            #~ 
+    #~ def setUp(self):
+        #~ super(TestWithMPI, self).setUp()
+        #~ self.check_fortran_version()
+    #~ 
+    #~ def test0(self):
+        #~ print "test1: basic startup and flow"
+        #~ instance=TDC()
+        #~ self.assertEquals(instance.get_name_of_current_state(), 'UNINITIALIZED')
+        #~ instance.initialize_code()
+        #~ self.assertEquals(instance.get_name_of_current_state(), 'INITIALIZED')
+        #~ instance.commit_parameters()
+        #~ self.assertEquals(instance.get_name_of_current_state(), 'EDIT')
+        #~ instance.commit_particles()
+        #~ self.assertEquals(instance.get_name_of_current_state(), 'RUN')
+#~ 
+        #~ instance.cleanup_code()
+        #~ instance.stop()
+#~ 
+    #~ def test1(self):
+        #~ print "test1: adding particles"
+#~ 
+        #~ instance=TDC()
+#~ 
+        #~ parts=self.makeparts(5)
+                #~ 
+        #~ self.assertEqual(len(instance.particles),0)
+        #~ instance.particles.add_particles(parts)
+        #~ self.assertEqual(len(instance.particles),len(parts))
+#~ 
+        #~ self.assertEquals(instance.get_name_of_current_state(), 'EDIT')
+#~ 
+        #~ part2=instance.particles.copy()
+#~ 
+        #~ self.assertAlmostRelativeEquals(parts.number_density,part2.number_density,12)
+        #~ self.assertAlmostRelativeEquals(parts.temperature,part2.temperature,12)
+        #~ self.assertAlmostRelativeEquals(parts.ionrate,part2.ionrate,12)
+#~ 
+        #~ self.assertAlmostRelativeEquals(part2.abundances, [
+#~ 0.29910782e0,
+#~ 0.35040982e0 ,
+#~ 1.0037100e-08 ,
+#~ 7.8517447e-14 ,
+ #~ 1.6757061e-10 ,
+ #~ 9.1403564e-15 ,
+   #~ 1.90e-7 ,
+   #~ 0.00019687628e0, 
+  #~ 2.3368063e-14 ,
+  #~ 2.3042991e-27 ,
+  #~ 1.5078215e-06 ,
+   #~ 1.1743749e-10 ,
+  #~ 1.4642501e-15 ,
+    #~ 4.6442955e-09, 
+   #~ 1.2046541e-13 ,
+   #~ 1.1560869e-07 ,
+  #~ 2.0393864e-14 ,
+   #~ 2.2347066e-10 ,
+   #~ 9.9354275e-09 ,
+   #~ 2.8124190e-09 ,
+  #~ 3.2763012e-21 ,
+   #~ 5.5126847e-15 ,
+   #~ 6.2356408e-17 ,
+  #~ 1.2307295e-15 ,
+  #~ 1.0298243e-15 ,
+   #~ 8.6555075e-13 ,
+   #~ 9.9986328e-05 ,
+   #~ 2.1199001e-15 ,
+   #~ 9.8358943e-10 ,
+   #~ 0.10000000e0 ,
+  #~ 2.9180687e-11 ,
+  #~ 4.6013858e-17 ,
+   #~ 5.5236247e-08 ,
+   #~ 1.7576375e-07 ,
+  #~ 2.6748393e-20 ],12)
+#~ 
+        #~ instance.cleanup_code()
+        #~ instance.stop()
+#~ 
+    #~ def test2(self):
+        #~ print "test2: adding particles w abund."
+#~ 
+        #~ instance=TDC()
+#~ 
+        #~ parts=self.makeparts(5)
+        #~ 
+        #~ parts.abundances=numpy.zeros((5,35))        
+#~ 
+        #~ for i in range(5):
+          #~ parts[i].abundances=(numpy.array(range(35))+1)/36.
+ #~ 
+        #~ instance.particles.add_particles(parts)
+#~ 
+        #~ channel=parts.new_channel_to(instance.particles)
+        #~ channel.copy() 
+#~ 
+#~ 
+        #~ part2=instance.particles.copy()
+#~ 
+        #~ self.assertAlmostRelativeEquals(parts.number_density,part2.number_density,12)
+        #~ self.assertAlmostRelativeEquals(parts.temperature,part2.temperature,12)
+        #~ self.assertAlmostRelativeEquals(parts.ionrate,part2.ionrate,12)
+#~ 
+        #~ for i in range(5):  
+          #~ self.assertAlmostRelativeEquals(part2[i].abundances,(numpy.array(range(35))+1)/36. ,12)
+#~ 
+        #~ instance.cleanup_code()
+        #~ instance.stop()
+#~ 
+    #~ def test3(self):
+        #~ print "test3: evolve test"
+#~ 
+        #~ instance=TDC(**default_options)
+#~ 
+        #~ parts=Particles(5)
+        #~ parts.number_density=1.e5 | units.cm**-3
+        #~ parts.temperature=500 | units.K
+        #~ parts.ionrate=1.e-11 | units.s**-1
+        #~ parts.abundances=numpy.zeros((5,35))        
+#~ 
+        #~ instance.particles.add_particles(parts)
+ #~ 
+        #~ my_yr=24*3600*365. *units.s
+#~ 
+        #~ instance.evolve_model( 94.112609921226252 | my_yr )
+#~ 
+        #~ for i in range(5):  
+          #~ self.assertAlmostRelativeEqual(instance.particles[i].abundances[0:5],
+               #~ [0.38895405860649546,.30526706600870018,1.47184343246716795E-004,
+                #~ 7.08149953046615757E-008,6.52897069295877081E-005],7)
+#~ 
+        #~ instance.stop()
+#~ 
+#~ 
+    #~ def makeparts(self,N):
+        #~ parts=Particles(N)
+        #~ numpy.random.seed(1234567)
+        #~ parts.number_density=(numpy.random.random(N)*1.e5+1.e5)| units.cm**-3
+        #~ parts.temperature=(numpy.random.random(N)*500+100)| units.K
+        #~ parts.ionrate=(numpy.random.random(N)*1.e-11+1.e-17)| units.s**-1
+        #~ return parts
+#~ 
