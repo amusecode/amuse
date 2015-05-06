e8c500a2ef995aa9e6063dcda68bf5f19952dad3
vdhelm <vdhelm@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2015-05-06 14:09:58 +0000

    Added some useful little functions.
    git-svn-id: file:///home/amuse/svn/amuse/trunk@6018 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/support/console.py
+++ src/amuse/support/console.py
@@ -13,13 +13,13 @@ class UnsupportedPrintingStrategyException(AmuseException):
 
 
 class PrintingStrategy(object):
-    
+
     def __init__(self):
         pass
-    
+
     def convert_quantity(self, quantity):
         return quantity
-    
+
     def quantity_to_string(self, quantity):
         quantity = self.convert_quantity(quantity)
         result = self.string_prefix()
@@ -30,33 +30,33 @@ class PrintingStrategy(object):
         result += string_of_unit
         result += self.string_suffix()
         return result
-    
+
     def string_prefix(self):
         return ""
-    
+
     def string_number(self, quantity):
         return ""
-        
+
     def string_separator(self):
         return " "
-    
+
     def string_unit(self, quantity):
         return ""
-    
+
     def string_suffix(self):
         return ""
-    
+
     def old_numbers_to_string(self, number):
         if hasattr(number, "__iter__"):
             return '[' + ', '.join([str(x) for x in number]) + ']'
         return str(number)
-    
+
     def numbers_to_string(self, quantity, precision=None):
         if precision is None:
             fmt = "%s"
         else:
             fmt = "%#."+str(precision)+"g"
-        
+
         if quantity.is_vector():
             def _traverse_vector(vector):
                 if len(vector.shape) > 1:
@@ -66,7 +66,7 @@ class PrintingStrategy(object):
             return _traverse_vector(quantity.number)
         else:
             return fmt % quantity.number
-    
+
     @classmethod
     def register(cls):
         """
@@ -74,72 +74,72 @@ class PrintingStrategy(object):
         in the :func:`set_printing_strategy` function.
         """
         add_printing_strategy(cls)
-    
+
 
 
 class DefaultPrintingStrategy(PrintingStrategy):
-    
+
     provided_strategy_names = ['default', 'with_units']
-    
+
     def string_number(self, quantity):
         return self.numbers_to_string(quantity)
-    
+
     def string_unit(self, quantity):
         return str(quantity.unit)
-    
+
 
 
 
 class SimplePrintingStrategy(PrintingStrategy):
-    
+
     provided_strategy_names = ['simple']
-    
+
     def string_number(self, quantity):
         factor, print_unit = quantity.unit.to_factor_and_reduced_form()
         return self.numbers_to_string(quantity * factor)
-    
+
     def string_unit(self, quantity):
         factor, print_unit = quantity.unit.to_factor_and_reduced_form()
         return str(print_unit)
-        
+
 class NoUnitsPrintingStrategy(PrintingStrategy):
-    
+
     provided_strategy_names = ['no_unit', 'no_units']
-    
+
     def string_number(self, quantity):
         return self.numbers_to_string(quantity)
-    
+
 
 
 class FormalPrintingStrategy(PrintingStrategy):
-    
+
     provided_strategy_names = ['formal',]
-    
+
     def string_prefix(self):
         return "<quantity "
-    
+
     def string_number(self, quantity):
         return self.numbers_to_string(quantity)
-    
+
     def string_separator(self):
         return " | "
-    
+
     def string_unit(self, quantity):
         return str(quantity.unit)
-    
+
     def string_suffix(self):
         return ">"
-    
+
 
 
 class NBodyPrintingStrategy(PrintingStrategy):
-    
+
     provided_strategy_names = ['nbody',]
-    
+
     def __init__(self, nbody_converter = None, ignore_converter_exceptions = False):
         self.ignore_converter_exceptions = ignore_converter_exceptions
         self.nbody_converter = nbody_converter
-    
+
     def convert_quantity(self, quantity):
         if is_not_nbody_unit(quantity.unit):
             if self.nbody_converter:
@@ -148,14 +148,14 @@ class NBodyPrintingStrategy(PrintingStrategy):
                 raise AmuseException("Unable to convert {0} to N-body units. No "
                     "nbody_converter given".format(quantity.unit))
         return quantity
-    
+
     def string_number(self, quantity):
         return self.numbers_to_string(quantity)
-    
+
 
 
 class PrintingStrategyWithPreferredUnits(PrintingStrategy):
-    
+
     def convert_quantity(self, quantity):
         if has_nbody_unit(quantity.unit):
             if self.nbody_converter:
@@ -164,68 +164,68 @@ class PrintingStrategyWithPreferredUnits(PrintingStrategy):
                 raise AmuseException("Unable to convert {0} to SI units. No "
                     "nbody_converter given".format(quantity.unit))
         return _quantity_in_preferred_units(self.preferred_units, quantity)
-    
+
     def string_number(self, quantity):
         return self.numbers_to_string(quantity)
-    
+
     def string_unit(self, quantity):
         if self.print_units:
             return str(quantity.unit)
         else:
             return ""
-    
+
 
 
 class AstroPrintingStrategy(PrintingStrategyWithPreferredUnits):
-    
+
     provided_strategy_names = ['astro',]
-    
+
     def __init__(self, nbody_converter = None, print_units = True, ignore_converter_exceptions = None):
-        self.ignore_converter_exceptions = (print_units if (ignore_converter_exceptions is None) 
+        self.ignore_converter_exceptions = (print_units if (ignore_converter_exceptions is None)
             else ignore_converter_exceptions)
         self.nbody_converter = nbody_converter
         self.print_units = print_units
         from amuse.units import units
         self.preferred_units = [units.MSun, units.Myr, units.parsec, units.J]
-    
+
 
 
 class SIPrintingStrategy(PrintingStrategyWithPreferredUnits):
-    
+
     provided_strategy_names = ['SI', 'si', 'MKS', 'mks']
-    
+
     def __init__(self, nbody_converter = None, print_units = True, ignore_converter_exceptions = None):
-        self.ignore_converter_exceptions = (print_units if (ignore_converter_exceptions is None) 
+        self.ignore_converter_exceptions = (print_units if (ignore_converter_exceptions is None)
             else ignore_converter_exceptions)
         self.nbody_converter = nbody_converter
         self.print_units = print_units
         from amuse.units import units
         self.preferred_units = [units.m, units.kg, units.s, units.A, units.K, units.mol, units.cd]
-    
+
 
 
 class CGSPrintingStrategy(PrintingStrategyWithPreferredUnits):
-    
+
     provided_strategy_names = ['CGS', 'cgs']
-    
+
     def __init__(self, nbody_converter = None, print_units = True, ignore_converter_exceptions = None):
-        self.ignore_converter_exceptions = (print_units if (ignore_converter_exceptions is None) 
+        self.ignore_converter_exceptions = (print_units if (ignore_converter_exceptions is None)
             else ignore_converter_exceptions)
         self.nbody_converter = nbody_converter
         self.print_units = print_units
         from amuse.units import units
         self.preferred_units = [units.cm, units.g, units.s, units.A, units.K, units.mol, units.cd]
-    
+
 
 
 class CustomPrintingStrategy(PrintingStrategyWithPreferredUnits):
-    
+
     provided_strategy_names = ['custom',]
-    
+
     def __init__(self, nbody_converter=None, print_units=True, preferred_units=None,
             precision=None, prefix="", separator=" ", suffix="",
             ignore_converter_exceptions=None):
-        self.ignore_converter_exceptions = (print_units if (ignore_converter_exceptions is None) 
+        self.ignore_converter_exceptions = (print_units if (ignore_converter_exceptions is None)
             else ignore_converter_exceptions)
         self.nbody_converter = nbody_converter
         self.print_units = print_units
@@ -234,16 +234,16 @@ class CustomPrintingStrategy(PrintingStrategyWithPreferredUnits):
         self.prefix = prefix
         self.separator = separator
         self.suffix = suffix
-    
+
     def string_prefix(self):
         return self.prefix
-    
+
     def string_number(self, quantity):
         return self.numbers_to_string(quantity, precision = self.precision)
-    
+
     def string_separator(self):
         return self.separator
-    
+
     def string_suffix(self):
         return self.suffix
 
@@ -295,6 +295,9 @@ def get_current_printing_strategy():
     return current_printing_strategy.__class__
 
 
+def set_preferred_units(*units):
+    set_printing_strategy("custom", preferred_units=units)
+
 def _get_printing_strategy_factory(strategy):
     if isinstance(strategy, basestring):
         if not strategy in registered_printing_strategies:
@@ -307,7 +310,7 @@ def add_printing_strategy(class_of_the_printing_strategy):
     """
     Register the specified class, so that it can be used
     by the :func:`set_printing_strategy` function.
-    
+
     Do not call this method directly, instead use :func:`PrintingStrategy.register`
     """
     for x in class_of_the_printing_strategy.provided_strategy_names:
@@ -315,7 +318,7 @@ def add_printing_strategy(class_of_the_printing_strategy):
 
 
 class _Defaults(options.OptionalAttributes):
-    
+
     @options.option(sections=['output',])
     def printing_strategy(self):
         return 'default'
--- src/amuse/test/amusetest.py
+++ src/amuse/test/amusetest.py
@@ -22,32 +22,32 @@ def safe_repr(obj, short=False):
     if not short or len(result) < _MAX_LENGTH:
         return result
     return result[:_MAX_LENGTH] + ' [truncated]...'
-    
+
 class SkipTest(exceptions.AmuseException):
     pass
-    
+
 class TestCase(unittest.TestCase):
     PRECISION = int(round(numpy.log10(2.0/(numpy.finfo(numpy.double).eps))))-1
-    
+
     def setUp(self):
         literature.TrackLiteratureReferences.suppress_output()
-        
+
     def _check_comparable(self, first, second):
         if is_quantity(first) is not is_quantity(second):
             # One exception: quantity with none_unit CAN be compared with non-quantity:
             if not to_quantity(first).unit == to_quantity(second).unit:
                 raise TypeError("Cannot compare quantity: {0} with non-quantity: {1}.".format(*(first,second)
                     if isinstance(first, Quantity) else (second,first)))
-    
+
     def _convert_to_numeric(self, first, second, in_units):
         if in_units:
             return (first.value_in(in_units), second.value_in(in_units))
         elif is_quantity(first) or is_quantity(second):
-            return (to_quantity(first).value_in(to_quantity(second).unit), 
+            return (to_quantity(first).value_in(to_quantity(second).unit),
                 to_quantity(second).value_in(to_quantity(second).unit))
         else:
             return (first, second)
-    
+
     def _convert_to_vectors(self, first, second):
         x = first if is_quantity(first) else numpy.array(first)
         y = second if is_quantity(second) else numpy.array(second)
@@ -57,8 +57,8 @@ class TestCase(unittest.TestCase):
         except:
             #raise
             raise TypeError("Arguments do not have compatible shapes for broadcasting")
-        
-    
+
+
     def _raise_exceptions_if_any(self, failures, first, second, err_fmt_string, msg, *args):
         if len(failures) == 1:
             if failures[0]:
@@ -69,28 +69,28 @@ class TestCase(unittest.TestCase):
                             for (i,b) in enumerate(failures) if b]
             err = '\n'.join(err_list)
             raise self.failureException(msg or err)
-    
-    
+
+
     def failUnlessEqual(self, first, second, msg = None, in_units = None):
         self._check_comparable(first, second)
         first_num, second_num = self._convert_to_numeric(first, second, in_units)
         failures = numpy.array(first_num != second_num).flatten()
         self._raise_exceptions_if_any(failures, first, second, '{0} != {1}', msg)
-        
+
     assertEqual = failUnlessEqual
     assertEquals = failUnlessEqual
-    
-    def failUnlessAlmostEqual(self, first, second, places=7, msg=None, in_units=None):                             
-        """Fail if the two objects are unequal as determined by their                                
-           difference rounded to the given number of decimal places                                  
-           (default 7) and comparing to zero.                                                        
-                                                                                                     
-           Note that decimal places (from zero) are usually not the same                             
-           as significant digits (measured from the most signficant digit).                          
-                                                                                                     
-           The AmuseTestcase differs from the defaulat unittest.Testcase in that                     
-           it implements an abs function that can handle quantities. 
-           
+
+    def failUnlessAlmostEqual(self, first, second, places=7, msg=None, in_units=None):
+        """Fail if the two objects are unequal as determined by their
+           difference rounded to the given number of decimal places
+           (default 7) and comparing to zero.
+
+           Note that decimal places (from zero) are usually not the same
+           as significant digits (measured from the most signficant digit).
+
+           The AmuseTestcase differs from the defaulat unittest.Testcase in that
+           it implements an abs function that can handle quantities.
+
            If the base unit for the test (in_units) is not supplied, the
            difference of the two objects is evaluated in units of [second.unit].
         """
@@ -98,49 +98,56 @@ class TestCase(unittest.TestCase):
         first_num, second_num = self._convert_to_numeric(first, second, in_units)
         failures = numpy.array(numpy.round(second_num - first_num, places) != 0).flatten()
         self._raise_exceptions_if_any(failures, first, second, '{0} != {1} within {2} places', msg, places)
-        
-    assertAlmostEqual =  failUnlessAlmostEqual                                                    
+
+    assertAlmostEqual =  failUnlessAlmostEqual
     assertAlmostEquals =  failUnlessAlmostEqual
-    
-    
+
+
     def failUnlessAlmostRelativeEqual(self, first, second, places=None, msg=None):
         self._check_comparable(first, second)
         first_num, second_num = self._convert_to_numeric(first, second, None)
-        
+
         if places is None:
             places = self.PRECISION
-            
+
         if places <= 0:
             places = self.PRECISION + places
-        
+
         maxRelativeError = 0.1 ** places
-        
+
         diff = numpy.abs(second_num - first_num)
-        
+
         is_one_zero = second_num * first_num == 0.0
         is_failure_for_one_zero =  diff >= (maxRelativeError)
-        
+
         first_for_div = first_num + (is_one_zero * 1.0)
         second_for_div = second_num + (is_one_zero * 1.0)
-        
-        relative_error = numpy.maximum(numpy.abs(diff /(first_for_div)), numpy.abs(diff /(second_for_div)))        
+
+        relative_error = numpy.maximum(numpy.abs(diff /(first_for_div)), numpy.abs(diff /(second_for_div)))
         is_failure_for_both_nonzero = relative_error >= maxRelativeError
         failures = numpy.where(is_one_zero, is_failure_for_one_zero, is_failure_for_both_nonzero)
-        
+
         failures = failures.flatten()
-        
+
         self._raise_exceptions_if_any(failures, first, second, "{0!r} != {1!r} within {2!r} places", msg, places)
-        
+
     assertAlmostRelativeEqual = failUnlessAlmostRelativeEqual
     assertAlmostRelativeEquals = failUnlessAlmostRelativeEqual
-    
+
     def assertIsOfOrder(self, first, second, msg=None):
         ratio = first*1.0/second
         if isinstance(ratio, Quantity):
             raise self.failureException,(msg or "Units of {0!r} and {1!r} do not match.".format(first, second))
         failures = numpy.array(numpy.round(numpy.log10(ratio)) != 0).flatten()
         self._raise_exceptions_if_any(failures, first, second, '{0} is not of order {1}', msg)
-    
+
+    def assertIsWithinFraction(self, first, second, fraction=0.05, msg=None):
+        ratio = first*1.0/second
+        if isinstance(ratio, Quantity):
+            raise self.failureException,(msg or "Units of {0!r} and {1!r} do not match.".format(first, second))
+        failures = numpy.array(abs(ratio) < 0).flatten()
+        self._raise_exceptions_if_any(failures, first, second, '{0} is not of order {1}', msg)
+
     def assertRaises(self, exception, callable, *list_args, **keyword_args):
         if 'expected_message' in keyword_args:
             exception_message = keyword_args.pop('expected_message')
@@ -152,23 +159,23 @@ class TestCase(unittest.TestCase):
                 raise self.failureException("Exception '{0}' was not raised.".format(exception))
         else:
             unittest.TestCase.assertRaises(self, exception, callable, *list_args, **keyword_args)
-            
+
     def assertGreaterEqual(self, a, b, msg=None):
         """Just like self.assertTrue(a >= b), but with a nicer default message."""
         if not a >= b:
             standardMsg = '%s not greater than or equal to %s' % (repr(a), repr(b))
             self.fail(self._formatMessage(msg, standardMsg))
-    
+
     def assertLess(self, a, b, msg=None):
         """Just like self.assertTrue(a < b), but with a nicer default message."""
         if not a < b:
             standardMsg = '%s not less than %s' % (repr(a), repr(b))
             self.fail(self._formatMessage(msg, standardMsg))
-    
+
     def run(self, result=None):
         if hasattr(unittest, 'SkipTest'):
             return unittest.TestCase.run(self, result)
-            
+
         if result is None:
             result = self.defaultTestResult()
         result.startTest(self)
@@ -203,31 +210,31 @@ class TestCase(unittest.TestCase):
             except:
                 result.addError(self, sys.exc_info())
                 ok = False
-                
+
             if ok:
                 result.addSuccess(self)
         finally:
             result.stopTest(self)
-            
-    
+
+
     def skip(self, reason):
         try:
             from nose.plugins import skip
             raise skip.SkipTest(reason)
-        except ImportError:   
+        except ImportError:
             if hasattr(unittest, 'SkipTest'):
                 raise unittest.SkipTest(reason)
             else:
                 raise SkipTest(reason)
-            
+
     def get_path_to_results(self):
         return get_path_to_results()
-    
+
     def get_amuse_root_dir(self):
         return get_amuse_root_dir()
 
-            
-    
+
+
 
 
 class TestWithMPI(TestCase):
@@ -235,38 +242,38 @@ class TestWithMPI(TestCase):
         TestCase.setUp(self)
         from amuse.rfi.core import is_mpd_running
         self.assertTrue(is_mpd_running(), "MPICH2 mpd deamon process not running, cannot run this test as it requires MPI")
-            
+
     def tearDown(self):
         from amuse.rfi.core import stop_interfaces
         #leave the distributed code alone, stop the rest
         stop_interfaces(exceptions= ['DistributedAmuseInterface'])
-    
+
     def new_instance(self, factory, *arguments, **kwarguments):
         try:
             return factory(*arguments, **kwarguments)
         except Exception as message:
             if os.path.exists(os.path.join(os.path.dirname(inspect.getfile(factory)),'src')):
-                raise            
+                raise
             self.skip("Tried to instantiate a new object of the code with type '{0}', but this code is not available".format(factory))
-         
-            
+
+
     def new_instance_of_an_optional_code(self, factory, *arguments, **kwarguments):
         try:
             return factory(*arguments, **kwarguments)
         except Exception as message:
             self.skip("Tried to instantiate a new object of the optional code with type '{0}', but this code is not available".format(factory))
 
-    
+
     def can_compile_modules(self):
         return TestDefaults().can_run_tests_to_compile_modules
-        
+
     def check_can_compile_modules(self):
         if not self.can_compile_modules():
             self.skip('will not run tests that compile codes')
-            
-    
+
+
 class TestDefaults(options.OptionalAttributes):
-        
+
     @options.option(sections=['test'])
     def path_to_results(self):
         name_of_testresults_directory = self.name_of_testresults_directory
@@ -278,13 +285,13 @@ class TestDefaults(options.OptionalAttributes):
                 if not os.access(os.getcwd(),  os.W_OK):
                     raise Exception("the current directory must be writable for amuse tests")
                 return os.getcwd()
-        
+
         amuse_root_dir = self.amuse_root_dir
         test_results_dir = os.path.join(amuse_root_dir, self.name_of_testresults_directory)
         if os.path.exists(test_results_dir):
             try:
                 f = open('test.txt','w')
-                f.close()                    
+                f.close()
                 return test_results_dir
             except IOError as ex:
                 if not os.access(os.getcwd(),  os.W_OK):
@@ -298,11 +305,11 @@ class TestDefaults(options.OptionalAttributes):
     @options.option(sections=['test'])
     def name_of_testresults_directory(self):
         return 'test_results'
-    
+
     @options.option(sections=['data'])
     def amuse_root_dir(self):
         if 'AMUSE_DIR' in os.environ:
-            return os.environ['AMUSE_DIR']    
+            return os.environ['AMUSE_DIR']
         previous = None
         result = os.path.abspath(__file__)
         while not os.path.exists(os.path.join(result,'build.py')):
@@ -311,7 +318,7 @@ class TestDefaults(options.OptionalAttributes):
                 return os.path.dirname(os.path.dirname(__file__))
             previous = result
         return result
-    
+
     @options.option(type='boolean',sections=['test'])
     def can_run_tests_to_compile_modules(self):
         return True
@@ -323,4 +330,4 @@ def get_path_to_results():
 
 def get_amuse_root_dir():
     return TestDefaults().amuse_root_dir
-    
+
