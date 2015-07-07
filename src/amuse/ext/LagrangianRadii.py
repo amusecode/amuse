39241ef092a8945b46b9cc28a6bb1de868700756
spz <spz@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2015-07-07 13:44:19 +0000

    Have Lagrangian radii prevent affecting the cluster's com
    git-svn-id: file:///home/amuse/svn/amuse/trunk@6080 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- src/amuse/ext/LagrangianRadii.py
+++ src/amuse/ext/LagrangianRadii.py
@@ -22,22 +22,22 @@ MassFraction = [0.005, 0.01, 0.02, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 1.0] \
 # at the moment (April 2010) this is not yet implemented, but the alternative:
 # star.position.length()
 # is slow, due to the implicite square-root
-def distance_sq(stars) :
-    return stars.x**2  + stars.y**2  +stars.z**2
+def distance_sq(stars, com) :
+    return (stars.x-com[0])**2  + (stars.y-com[1])**2  + (stars.z-com[2])**2
 
 def LagrangianRadii(stars, verbose = 0, massf = MassFraction) :
 
     com = stars.center_of_mass()
-    stars.position = stars.position - com
+    #stars.position = stars.position - com
 
     vcom = stars.center_of_mass_velocity()
-    stars.velocity = stars.velocity - vcom
+    #stars.velocity = stars.velocity - vcom
 
     n_stars = len(stars)
     # Next line is a potential performance bottleneck, becasue
     # the for loop is not in numpy but explicit
     # old but slow: d2 = numpy.array([distance_sq(star) for star in stars])
-    d2 = distance_sq(stars)
+    d2 = distance_sq(stars, com)
     m = stars.mass/stars.mass.sum()
     d2m = zip(d2, m)
     d2m.sort(key = operator.itemgetter(0))
