59722010a79e8a939db3c0d1ff219b813393698c
pelupes <pelupes@6e832d89-e7d1-4ad1-8ca1-873beecccf65>
2015-10-02 15:34:07 +0000

    make test mor erobust by using lower noise base_grid. Note: transfer molecular_cloud to spherical_model
    git-svn-id: file:///home/amuse/svn/amuse/trunk@6127 6e832d89-e7d1-4ad1-8ca1-873beecccf65

--- test/ext_tests/test_molecular_cloud.py
+++ test/ext_tests/test_molecular_cloud.py
@@ -5,13 +5,14 @@ import numpy.random
 from amuse.test import amusetest
 from amuse.units import units, nbody_system
 from amuse.ext.molecular_cloud import molecular_cloud, ism_cube
+from amuse.ext.evrard_test import sobol_unit_cube
 
 numpy.random.seed(1234567)
 
 class MolecularCloudTests(amusetest.TestCase):
     def test1(self):
-        mc=molecular_cloud(targetN=1000).result
-        self.assertEqual(len(mc),1000)
+        mc=molecular_cloud(targetN=1000,base_grid=sobol_unit_cube).result
+        self.assertEqual(len(mc),995)
         ek=mc.kinetic_energy()
         ep=mc.potential_energy(G=nbody_system.G)
         eth=mc.thermal_energy()
@@ -19,8 +20,8 @@ class MolecularCloudTests(amusetest.TestCase):
         self.assertAlmostRelativeEqual(ek/ep, -1.,2)
 
     def test2(self):
-        mc=molecular_cloud(targetN=1000,ethep_ratio=0.05).result
-        self.assertEqual(len(mc),1000)
+        mc=molecular_cloud(targetN=1000,ethep_ratio=0.05,base_grid=sobol_unit_cube).result
+        self.assertEqual(len(mc),995)
         ek=mc.kinetic_energy()
         ep=mc.potential_energy(G=nbody_system.G)
         eth=mc.thermal_energy()
@@ -28,8 +29,8 @@ class MolecularCloudTests(amusetest.TestCase):
         self.assertAlmostRelativeEqual(ek/ep, -1.,2)
 
     def test3(self):
-        mc=molecular_cloud(targetN=1000,ekep_ratio=2.).result
-        self.assertEqual(len(mc),1000)
+        mc=molecular_cloud(targetN=1000,ekep_ratio=2.,base_grid=sobol_unit_cube).result
+        self.assertEqual(len(mc),995)
         ek=mc.kinetic_energy()
         ep=mc.potential_energy(G=nbody_system.G)
         eth=mc.thermal_energy()
