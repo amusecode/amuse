10fd9b6f30fac727d6cdfe68871f25b6d36df430
Arjen van Elteren <vanelteren@strw.leidenuniv.nl>
2015-10-30 12:01:51 +0100

    small documentation change

--- doc/conf.py
+++ doc/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'AMUSE'
-copyright = u'2009, 2010, 2011 The AMUSE Team'
+copyright = u'2009-2015 The AMUSE Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
