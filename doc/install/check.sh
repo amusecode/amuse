064472a82a73f8e4c2a643e3a967a5b6017338f7
Arjen van Elteren <vanelteren@strw.leidenuniv.nl>
2016-01-07 11:24:35 +0100

    delete debug files

--- doc/install/check.sh
+++
@@ -1,5 +0,0 @@
-for COMPUTER in aduaderdiep bakkerskil blekekil buurserbeek dedemsvaart dodder  kiekpoel negenoord oolderplas polderveld schenk slufter spaarne wantij zandkreek zoom zuidplas
-do
-    echo $COMPUTER
-    $COMPUTER --foreground which python
-done
--- doc/install/connect.sh
+++
@@ -1,4 +0,0 @@
-for COMPUTER in aduaderdiep bakkerskil balyon blekekil buurserbeek dedemsvaart dodder geeuw hals kiekpoel negenoord oolderplas polderveld schenk slufter spaarne wantij zandkreek zoom zuidplas
-do
-   ssh $COMPUTER.strw.leidenuniv.nl ls 
-done
