afdbffa21f19e148322f1dc887d693b7afdf6410
Thomas Wijnen <tpgwijnen@gmail.com>
2016-04-05 22:30:04 +0200

    removed the wrongly commented print statement in timestep.c

--- src/amuse/community/gadget2/src/timestep.c
+++ src/amuse/community/gadget2/src/timestep.c
@@ -299,13 +299,7 @@ void advance_and_find_timesteps(void)
 					      0.0001 * soundspeed / SphP[i].Hsml);
 	      tau = 2.5 * SphP[i].Hsml / soundspeed;
 	      SphP[i].DAlphaDt = f_fac*dmax(-SphP[i].DivVel, 0) * (2.0 - SphP[i].Alpha) - (SphP[i].Alpha - .1)/tau;
-
-	      //printf("f_fac = %g, tau = %g, Alpha = %g, DivVel = %g, DtAlpha = %g \n",
-		f_fac,
-		tau,
-		SphP[i].Alpha,
-		SphP[i].DivVel,
-		SphP[i].DAlphaDt);	
+	
 	    /* change this to input the maximum the viscosity can get to. */      
 	    /*  SphP[i].DAlphaDt = f_fac*dmax(-SphP[i].DivVel, 0) * (All.ArtBulkViscConst - SphP[i].Alpha) - (SphP[i].Alpha - .1)/tau;*/
 #endif	
