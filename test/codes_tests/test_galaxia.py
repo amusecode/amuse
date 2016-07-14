3fc2455c09fd4a1ffa2b67317bdc498a30711393
ipelupessy <pelupes@strw.leidenuniv.nl>
2016-07-14 14:10:02 +0200

    add another galaxia test

--- test/codes_tests/test_galaxia.py
+++ test/codes_tests/test_galaxia.py
@@ -7,3 +7,10 @@ class TestBarAndSpirals3D(amusetest.TestCase):
     def test1(self):
       galaxy= BarAndSpirals3D()
       self.assertEqual(galaxy.model_time, 0. | units.yr)
+
+    def test2(self):
+      galaxy= BarAndSpirals3D()
+      vc1= galaxy.get_velcirc(10. | units.kpc, 0. | units.kpc,0. | units.kpc)
+      vc2= galaxy.get_velcirc(0. | units.kpc, 10. | units.kpc,0. | units.kpc)
+      self.assertEqual(vc1,vc2)
+      self.assertAlmostEqual(vc1, 219.324003066 | units.kms)
