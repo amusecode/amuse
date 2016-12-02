99636be4d0c1557a3882d2d4c8f43d827cca610c
Gijs Nelemans <gijs.nelemans@ligo.org>
2016-12-02 15:54:19 +0100

    Test

--- src/amuse/community/seba/src/dstar/Normalisation.C
+++ src/amuse/community/seba/src/dstar/Normalisation.C
@@ -33,12 +33,18 @@
 ////                 
 //// Options:   -M    upper primary mass limit [100[Msun]]
 ////            -m    lower limit to primary mass [0.1[Msun]]
-////            -x    mass function exponent in case of power law [-2.35]
+////            -x    mass function exponent in case of power law [-2.35] or mean mass 
+////                                                                for log-normal IMF
+////            -d        sigma for log-normal IMF
 ////            -F/f  mass function option: 0) Equal mass
 ////                                        1) Power-law [default]
 ////                                        2) Miller & Scalo
 ////                                        3) Scalo
-////                                        4) Kroupa
+////                                        4) Kroupa/
+////                                        5) GdeMarchi
+////                                        6) Kroupa, Tout & Gilmore 1991
+////                                        7) TwoComponent (uses -h, -l and -u)
+////                                        8) LogNormal (uses -x (for mu) and -d)
 ////            Option -F requires one of the following strings:
 ////                      (mf_Power_Law, Miller_Scalo, Scalo, Kroupa)
 ////                   -f requires the appropriate interger (see mkmass.C)
@@ -64,7 +70,7 @@
 ////             -q   minimum mass ratio [0]
 ////                    extra option: q_min<0 : q_min=0.1/selected primary mass
 ////             -w   exponent for a power-law distribution  
-////             -P/p eccentricity option: 0) constant mass ratio
+////             -P/p mass ratio option: 0) constant mass ratio
 ////                                       1) Flat_q
 ////                                       2) Power Law 
 ////                                       3) Hogeveen (1992)
@@ -81,6 +87,7 @@
 ////                        -R the number of binaries indicated.
 ////                        oterwise one binary is simulated with
 ////                        -M, -m, -a, -e as initial conditions.
+////            -T or -t  binary end time. [13500] Myr
 ////            -s Random seed
 ////            -z select metallicity of binaries to be simulated. [0.02] Solar
 ////
@@ -95,7 +102,7 @@
 //	version 3.3	Simon Portegies Zwart, Cambridge, March 1999
 //
 
-#include "node.h" 
+#include "dyn.h" 
 #include "double_star.h"
 #include "main_sequence.h"
 //#include "dstar_to_dyn.h"
@@ -105,20 +112,27 @@
 #ifdef TOOLBOX
 
 local bool read_binary_params(ifstream& in, real &m_prim, 
-			      real &m_sec, real &sma, real &ecc) {
+			      real &m_sec, real &sma, real &ecc, real &z) {
 
-  if(in.eof())
-    return false;
-
-  // reading from input file
-  int id, time, tpp, tps; 
-  real Rlp, Rls;
-
-  in >>id >> time >> sma >> ecc 
-     >> tpp >> m_prim >> Rlp >> tps >> m_sec >> Rls;
-
-      PRC(m_prim);PRC(m_sec);PRC(sma);PRL(ecc);
-
+    m_prim = 1000;
+    m_sec = 1000;
+    sma = 0;
+    ecc = 0;
+    z = 1;
+    while (m_prim>100.0 || m_sec>100.0 || ecc<0 || ecc>1 || z < 0.0001 || z > 0.03){   
+      if(in.eof())
+        return false;
+    
+      // reading from input file
+    //  int id, tpp, tps; 
+    //  real time, Rlp, Rls;
+    
+      in >> sma >> ecc >> m_prim >> m_sec >> z;
+      //  in >>id >> time >> sma >> ecc 
+      //     >> tpp >> m_prim >> Rlp >> tps >> m_sec >> Rls;
+    }
+    
+    PRC(m_prim);PRC(m_sec);PRC(sma);PRC(ecc);PRL(z);
   return true;
 }
 
@@ -126,10 +140,12 @@ local bool read_binary_params(ifstream& in, real &m_prim,
  *  binev  --
  *-----------------------------------------------------------------------------
  */
-local bool  evolve_binary(node * bi,
+local bool  evolve_binary(dyn * bi,
                           real start_time, real end_time,
 			  bool stop_at_merger_or_disruption,
-			  bool stop_at_remnant_formation) { 
+			  bool stop_at_remnant_formation,
+			  char* SeBa_outfile) { 
+
 
   double_star* ds = dynamic_cast(double_star*, 
 				 bi->get_starbase());
@@ -138,15 +154,20 @@ local bool  evolve_binary(node * bi,
   real dt, time=start_time;
   ds->evolve_element(time);
   
-  ds->dump("SeBa.data", true);
+  //char SeBa_outfile[] = "SeBa.data";
+  //char SeBa_outfile[] = SeBa_Filename();
+  ds->dump(SeBa_outfile, true);
 
   if (!bi->is_root() &&
       bi->get_parent()->is_root()) 
 
     do {
 
-      dt = ds->get_evolve_timestep() + cnsts.safety(minimum_timestep);
-      time = min(time+dt, end_time);
+      //dt = ds->get_evolve_timestep() + cnsts.safety(minimum_timestep);
+      dt =
+      Starlab::max(ds->get_evolve_timestep(),cnsts.safety(minimum_timestep));
+
+      time = Starlab::min(time+dt, end_time);
 
       ds->evolve_element(time);
 
@@ -162,8 +183,10 @@ local bool  evolve_binary(node * bi,
     }
     while (time<end_time);
 
-  ds->dump("SeBa.data", true);
+  ds->dump(SeBa_outfile, true);
+  ds->set_star_story(NULL);
     
+  rmtree(bi, false);
   return true;
 
 }
@@ -174,6 +197,7 @@ int main(int argc, char ** argv) {
     bool R_flag = false;
     bool F_flag = false;
     bool I_flag = false;
+    bool O_flag = false;//doesn't work fully as "SeBa.data" is written in the code multiple times
     bool P_flag = false;
     bool U_flag = false;
     bool G_flag = false;
@@ -208,9 +232,10 @@ int main(int argc, char ** argv) {
 
     char *mfc = new char[64];
     mass_function mf = mf_Power_Law;
-    real m_min = 0.1, m_min_std = 0.1;
+    real m_min = 0.1, m_min_std = 0.08;
     real m_max = 100, m_max_std = 100;
     real m_exp = -2.35;
+    real sigma = 0;
     char *qfc = new char[64];
     mass_ratio_distribution qf = Flat_q;
     real q_min = 0, q_min_std = 0;
@@ -231,18 +256,20 @@ int main(int argc, char ** argv) {
     real end_time   = 1e4;
 
     char* input_filename;
+    char* output_filename;
+    output_filename = "SeBa.data";
 
     int input_seed=0; 
     char seedlog[64];
     char paramlog[90];
 
-    check_help();
+    //check_help();
 
 //    seba_counters* new_seba_counters = new seba_counters;
 
     extern char *poptarg;
     int c;
-    char* param_string = "n:N:RDSM:m:x:F:f:A:a:y:G:g:E:e:v:U:u:Q:q:T:I:w:P:p:n:s:z:";
+    const char *param_string = "n:N:RDSM:m:x:d:F:f:A:a:y:G:g:E:e:v:U:u:Q:q:T:t:I:O:w:P:p:n:s:z:";
 
     while ((c = pgetopt(argc, argv, param_string)) != -1)
 	switch(c) {
@@ -257,6 +284,8 @@ int main(int argc, char ** argv) {
             case 'm': m_min = atof(poptarg);
 		      break;
             case 'x': m_exp = atof(poptarg);
+		      break;
+            case 'd': sigma = atof(poptarg);
 		      break;
 	    case 'F': F_flag = true;
 		      strcpy(mfc, poptarg);
@@ -289,10 +318,14 @@ int main(int argc, char ** argv) {
 		      break;
             case 'q': q_min = atof(poptarg);
 		      break;
+            case 't': 
             case 'T': end_time = atof(poptarg);
 		      break;
             case 'I': I_flag = true;
-		      strcpy(input_filename, poptarg);
+		      input_filename = poptarg;
+		      break;
+            case 'O': O_flag = true;
+		      output_filename = poptarg;
 		      break;
             case 'w': q_exp = atof(poptarg);
 		      break;
@@ -344,7 +377,7 @@ int main(int argc, char ** argv) {
     actual_seed = srandinter(input_seed);
     sprintf(seedlog, "         random number generator seed = %d",actual_seed);
 
-    ifstream infile(input_filename);
+    ifstream infile(input_filename, ios::in);
     if(I_flag) {
       if (!infile) cerr << "error: couldn't create file "
 	                << input_filename <<endl;
@@ -353,7 +386,7 @@ int main(int argc, char ** argv) {
 
 
 
-    print_initial_binary_distributions(m_min, m_max, mf, m_exp,
+    print_initial_binary_distributions(m_min, m_max, mf, m_exp, sigma,
 			       q_min, q_max, qf, q_exp,
 			       a_min, a_max, af, a_exp,
 			       e_min, e_max, ef, e_exp);
@@ -362,15 +395,14 @@ int main(int argc, char ** argv) {
     for (int i=0; i<n; i++) {
 
       if(I_flag) {
-
-	if(read_binary_params(infile, m_prim, m_sec, sma, ecc)) 
+	if(read_binary_params(infile, m_prim, m_sec, sma, ecc, metal)) 
 	  n=i+2;
 	else
 	  break;
 
       }
       else if (random_initialization) 
-	mkrandom_binary(m_min_std, m_max_std, mf, m_exp,
+	mkrandom_binary(m_min_std, m_max_std, mf, m_exp, sigma,
 			q_min_std, q_max_std, qf, q_exp,
 			a_min_std, a_max_std, af, a_exp,
 			e_min_std, e_max_std, ef, e_exp,
@@ -409,7 +441,7 @@ int main(int argc, char ** argv) {
     M_mean_single = M_p/n_run;
     M_mean_double = M_tot/n_run;
 
-    cout << "In total mass spectrum (0.1 -- 100 Msun): " <<endl;
+    cout << "In total mass spectrum (0.08 -- 100 Msun): " <<endl;
     cout << "Total number of binaries  : " << n << endl;
     cout << "Total mass in primaries   : " << M_p_all << endl;
     cout << "Total mass in secondaries : " << M_s_all << endl;
