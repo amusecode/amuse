a99c28eb580986f5f71c64aa41bd4d71aec3d1be
F.I. Pelupessy <f.i.pelupessy@casema.nl>
2017-04-18 12:00:57 +0200

    remove etics from minimal

--- src/amuse/community/etics/src/ic.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-namespace etics {
-    namespace ic {
-        void hernquist(int N, int Seed, Particle **ParticleList);
-        void plummer(int N, int Seed, Particle **ParticleList);
-    }
-}
