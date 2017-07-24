ecab4d7635b815b0f3a38dd44d6cb219d1641120
Steve McMillan <steve@physics.drexel.edu>
2017-07-24 14:35:30 -0400

    Missing comma...

--- src/amuse/community/ph4/interface.py
+++ src/amuse/community/ph4/interface.py
@@ -584,7 +584,7 @@ class ph4(GravitationalDynamics,GravityFieldCode):
 
         object.add_method_parameter(
             "get_initial_timestep_median", # getter name in interface.cc
-            "set_initial_timestep_median"  # setter name in interface.cc
+            "set_initial_timestep_median",  # setter name in interface.cc
             "initial_timestep_median",     # python parameter name
             "initial timestep median factor", # description
             default_value = 0.0
