d423338803ecdeebcc0c1a1f9cc60c22b44b36f3
Steve McMillan <steve@physics.drexel.edu>
2017-09-25 18:38:35 -0400

    Updated scrit.

--- examples/textbook/plot_gravity.py
+++ examples/textbook/plot_gravity.py
@@ -1,14 +1,29 @@
+import os
 from matplotlib import pyplot
 from amuse.plot import scatter, xlabel, ylabel
 from amuse.io import read_set_from_file
 
-def main(filename):
+def main(filename=None):
+    if filename is None: return
+    try:
+        amusedir = os.environ['AMUSE_DIR']
+    except:
+        print 'Environment variable AMUSE_DIR not set'
+        amusedir = '.'
+    filename = amusedir+'/examples/textbook/'+filename
+
     pyplot.figure(figsize=(12,12))
     particles = read_set_from_file(filename, "amuse")
     for si in particles.history:
         scatter(si.x, si.y, s=100)
     xlabel("x")
     ylabel("y")
+
+    save_file = "plot_gravity.png"
+    pyplot.savefig(save_file)
+    print "\nSaved figure in file", save_file,'\n'
+    pyplot.show()
+
     pyplot.show()
 
 def new_option_parser():
