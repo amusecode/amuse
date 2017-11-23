56cb8d862f0b1162053a78eb2de342f04a36f674
Steve McMillan <steve@physics.drexel.edu>
2017-11-23 02:00:29 -0500

    Tweaked to conform, but still broken...

--- examples/textbook/kira.py
+++ examples/textbook/kira.py
@@ -23,27 +23,34 @@ def new_binary_orbit(mass1, mass2, semi_major_axis,
     total_mass = mass1 + mass2
     mass_fraction_particle_1 = mass1 / (total_mass)
     
-#    binary = Particles(keys=range(keyoffset, keyoffset+2))
     binary = Particles(2)
     binary[0].mass = mass1
     binary[1].mass = mass2
     
     mu = constants.G * total_mass
     
-    velocity_perihelion = numpy.sqrt( mu / semi_major_axis  * ((1.0 + eccentricity)/(1.0 - eccentricity)))
+    velocity_perihelion \
+        = numpy.sqrt( mu / semi_major_axis * ((1.0 + eccentricity)
+                                               /(1.0 - eccentricity)))
     radius_perihelion = semi_major_axis * (1.0 - eccentricity)
     print velocity_perihelion
     
-    binary[0].position = ((1.0 - mass_fraction_particle_1) * radius_perihelion * [1.0,0.0,0.0])
-    binary[1].position = -(mass_fraction_particle_1 * radius_perihelion * [1.0,0.0,0.0])
+    binary[0].position = ((1.0 - mass_fraction_particle_1) \
+                           * radius_perihelion * [1.0,0.0,0.0])
+    binary[1].position = -(mass_fraction_particle_1 \
+                            * radius_perihelion * [1.0,0.0,0.0])
     
-    binary[0].velocity = ((1.0 - mass_fraction_particle_1) * velocity_perihelion * [0.0,1.0,0.0])
-    binary[1].velocity = -(mass_fraction_particle_1 * velocity_perihelion * [0.0,1.0,0.0])
+    binary[0].velocity = ((1.0 - mass_fraction_particle_1) \
+                           * velocity_perihelion * [0.0,1.0,0.0])
+    binary[1].velocity = -(mass_fraction_particle_1 \
+                            * velocity_perihelion * [0.0,1.0,0.0])
 
     return binary
 
-# see Eggleton 1999 Equation 1.6.3 (in private copy of his book).
-def random_semimajor_axis_PPE(Mprim, Msec, P_min=10.|units.day, P_max=100.|units.yr):
+# See Eggleton 1999 Equation 1.6.3 (in private copy of his book).
+
+def random_semimajor_axis_PPE(Mprim, Msec, P_min=10.|units.day,
+                              P_max=100.|units.yr):
 
     Pmax = P_max.value_in(units.day)
     Pmin = P_min.value_in(units.day)
@@ -65,7 +72,8 @@ def make_secondaries(center_of_masses, Nbin):
     mmin = center_of_masses.mass.min()
     for bi in binaries:
         mp = bi.mass
-        ms = numpy.random.uniform(mmin.value_in(units.MSun), mp.value_in(units.MSun)) | units.MSun
+        ms = numpy.random.uniform(mmin.value_in(units.MSun),
+                                  mp.value_in(units.MSun)) | units.MSun
         a = random_semimajor_axis_PPE(mp, ms)
         e = numpy.sqrt(numpy.random.random())
 
@@ -88,8 +96,6 @@ def make_secondaries(center_of_masses, Nbin):
 
 def calculate_orbital_elementss(bi, converter):
     kep = new_kepler(converter)
-#    kep.set_longitudinal_unit_vector(1.0,0.0, 0.0)
-#    kep.set_transverse_unit_vector(0.0, 1.0, 0)
     comp1 = bi.child1
     comp2 = bi.child2
     mass = (comp1.mass + comp2.mass)
@@ -101,8 +107,8 @@ def calculate_orbital_elementss(bi, converter):
     kep.stop()
     return a, e
 
+###BOOKLISTSTART###
 def resolve_changed_binaries(stopping_condition, stellar, converter):
-
     new_binaries = stopping_condition.particles(0)
     for bi in new_binaries:
         print "add new binary:", bi
@@ -126,18 +132,20 @@ def resolve_changed_binaries(stopping_condition, stellar, converter):
         bs.eccentricity = e
         print "Modified binary parameters", a, e
         print bs
+###BOOKLISTSTOP###
 
 def update_dynamical_binaries_from_stellar(stellar, multiples_code, converter):
     kep = new_kepler(converter)
-#    kep.set_longitudinal_unit_vector(1.0,0.0, 0.0)
-#    kep.set_transverse_unit_vector(0.0, 1.0, 0)
-    # THIS NEEDS TO BE CHECKED
+
+    # THIS NEEDS TO BE CHECKED!
     print "++++++++++++ THIS NEEDS TO BE CHECKED ++++++++++++++++++++"
+
     print "Number of binaries=", len(stellar.binaries)
     for bi in stellar.binaries:
         bs = bi.as_particle_in_set(multiples_code.binaries)
         total_mass = bi.child1.mass+bi.child2.mass 
-        kep.initialize_from_elements(total_mass, bi.semi_major_axis, bi.eccentricity)
+        kep.initialize_from_elements(total_mass, bi.semi_major_axis,
+                                     bi.eccentricity)
         rel_position = as_vector_quantity(kep.get_separation_vector())
         rel_velocity = as_vector_quantity(kep.get_velocity_vector())
         mu = bi.child1.mass / total_mass 
@@ -145,12 +153,12 @@ def update_dynamical_binaries_from_stellar(stellar, multiples_code, converter):
         bs.child2.position = -(1-mu) * rel_position 
         bs.child1.velocity = mu * rel_velocity
         bs.child2.velocity = -(1-mu) * rel_velocity
-        print "semi_major_axis=", bi.semi_major_axis, total_mass, bi.child1.mass, bi.child2.mass, bi.eccentricity
+        print "semi_major_axis=", bi.semi_major_axis, total_mass, \
+              bi.child1.mass, bi.child2.mass, bi.eccentricity
     kep.stop()
         
 def kira(tend, N, R, Nbin):
     logging.basicConfig(level=logging.ERROR)
-    #encounters.LOG_ENERGY.setLevel(logging.DEBUG)
 
     mass = new_salpeter_mass_distribution(N, mass_min=10|units.MSun)
     converter = nbody_system.nbody_to_si(mass.sum(), R)
@@ -159,7 +167,8 @@ def kira(tend, N, R, Nbin):
     stars.mass = mass
     stars.radius = 0.01/len(stars) | R.unit
 
-    single_stars, binary_stars, singles_in_binaries = make_secondaries(stars, Nbin)
+    single_stars, binary_stars, singles_in_binaries \
+        = make_secondaries(stars, Nbin)
     print binary_stars
 
     stellar = SeBa()
@@ -172,38 +181,45 @@ def kira(tend, N, R, Nbin):
         kepler_code =  new_kepler(converter),
         resolve_collision_code = new_smalln(converter),
         interaction_over_code = None,
-        G=constants.G
-        )
+        G = constants.G
+    )
     multiples_code = encounters.Multiples(
         gravity_code = code,
         handle_encounter_code = encounter_code,
-        G=constants.G
-        )
+        G = constants.G
+    )
     multiples_code.particles.add_particles((stars-binary_stars).copy())
     multiples_code.singles_in_binaries.add_particles(singles_in_binaries)
     multiples_code.binaries.add_particles(binary_stars)
     multiples_code.commit_particles()
-    channel_from_stars_to_particles = stellar.particles.new_channel_to(multiples_code.particles)
+    channel_from_stars_to_particles \
+        = stellar.particles.new_channel_to(multiples_code.particles)
 
-    stopping_condition = multiples_code.stopping_conditions.binaries_change_detection
+    stopping_condition \
+        = multiples_code.stopping_conditions.binaries_change_detection
     stopping_condition.enable()
 
     t = quantities.linspace(0*tend, tend, 11)
     for ti in t:
-        print "t, Energy=", ti, multiples_code.particles.mass.sum(), multiples_code.get_total_energy()
+        print "t, Energy=", ti, multiples_code.particles.mass.sum(), \
+              multiples_code.get_total_energy()
         multiples_code.evolve_model(ti)
-        print "at t=", multiples_code.model_time, "N-multiples:", len(multiples_code.multiples)
+        print "at t=", multiples_code.model_time, \
+              "Nmultiples:", len(multiples_code.multiples)
 
         if stopping_condition.is_set():
             resolve_changed_binaries(stopping_condition, stellar, converter)
 
         stellar.evolve_model(ti)
         channel_from_stars_to_particles.copy_attributes(["mass", "radius"])
-        update_dynamical_binaries_from_stellar(stellar, multiples_code, converter)
+        update_dynamical_binaries_from_stellar(stellar, multiples_code,
+                                               converter)
 
-        print "Lagrangian radii:", multiples_code.all_singles.LagrangianRadii(converter)
+        print "Lagrangian radii:", \
+              multiples_code.all_singles.LagrangianRadii(converter)
         print "MC.particles", multiples_code.particles
-        print "Lagrangian radii:", multiples_code.particles.LagrangianRadii(converter)
+        print "Lagrangian radii:", \
+              multiples_code.particles.LagrangianRadii(converter)
         print "t, Energy=", ti, multiples_code.get_total_energy()
 
     multiples_code.stop()
@@ -213,15 +229,15 @@ def new_option_parser():
     from amuse.units.optparse import OptionParser
     result = OptionParser()
     result.add_option("-t", unit=units.Myr,
-                      dest="tend",type="float",default=1.|units.Myr)
+                      dest="tend",type="float", default=1.|units.Myr)
     result.add_option("-R", unit=units.parsec,
-                      dest="R",type="float",default=1|units.parsec)
+                      dest="R",type="float", default=1|units.parsec)
     result.add_option("-N", 
-                      dest="N",type="float",default=100)
+                      dest="N",type="float", default=100)
     result.add_option("--Nbin", 
-                      dest="Nbin",type="int",default=50)
+                      dest="Nbin",type="int", default=50)
     result.add_option("--seed", 
-                      dest="seed",type="int",default=-1)
+                      dest="seed",type="int", default=-1)
     return result
 
 if __name__ == "__main__":
@@ -231,7 +247,7 @@ if __name__ == "__main__":
                       separator = " [", suffix = "]")
 
     options, arguments  = new_option_parser().parse_args()
-    if options.seed>=0:
+    if options.seed >= 0:
         numpy.random.seed(options.seed)
         # This is only for random.sample, which apparently does not use numpy
         import random
