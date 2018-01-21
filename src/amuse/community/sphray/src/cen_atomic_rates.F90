edaf6381a21f5458a0ff4579db9af5f58c8cc31a
ipelupessy <pelupes@strw.leidenuniv.nl>
2018-01-21 13:34:41 +0100

    sphray fix for verner cross section for freq < Ry
    this may allow freq below ionization threshold (and thus more realistic
    heating when including dust photo electric heating, still not included though)

--- src/amuse/community/sphray/src/cen_atomic_rates.F90
+++ src/amuse/community/sphray/src/cen_atomic_rates.F90
@@ -295,6 +295,11 @@ contains
     real(r8b) :: x
     real(r8b) :: y
 
+    if (freq < 1.d0) then
+      sigma = 0.de0
+      return
+    end if
+    
     eV = Ry * 13.6d0
     x = eV / E0
     y = x
