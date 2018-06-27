df8ab148b84a1bad88cdc0a676f0fc9c7f7aa1c7
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-06-27 14:42:50 +0200

    Updated the install scripts for Python2 and Python3. Furthermore, the script for Python2 can also install Python3 now, so in the future a single script could be used.

--- doc/install/install-python.sh
+++ doc/install/install-python.sh
@@ -6,131 +6,122 @@
 # date  : 2009 - 05 -18
 #
 
-#APPVER=2.5.4
-APPVER=2.7.14
-#APPVER=2.6.5
-#APPVER=2.7.1
-APPFILE=Python-${APPVER}.tar.bz2
-APPFILE=Python-${APPVER}.tgz
-APP_DIR=Python-${APPVER}
-URL=https://www.python.org/ftp/python/${APPVER}/${APPFILE}
-
-
-OPENSSLVERSION="1.0.2m"
-OPENSSLFILE=openssl-${OPENSSLVERSION}.tar.gz 
-OPENSSLURL=https://www.openssl.org/source/old/1.0.2/${OPENSSLFILE}
-OPENSSLDIR=openssl-${OPENSSLVERSION}
-
+# Prerequisites directory check
 if [ -z ${PREFIX} ]; then
 	echo The PREFIX variable is not set, please set it to an user directory
 	exit 1
 fi
-
 if [ ! -d ${PREFIX} ]; then
 	echo ${PREFIX} directory does not exists, please create it first!
 	exit 1
- 
 fi
 
- 
+# Python version selection (default to Python 2)
+if [ "${1}" = "python3" ]
+then
+	APPVER=3.6.5
+else
+	APPVER=2.7.15
+fi
 
-INSTALL_DIR=$PREFIX/install
-mkdir $INSTALL_DIR
-cd $INSTALL_DIR
+# Python
+APPFILE=Python-${APPVER}.tgz
+APP_DIR=Python-${APPVER}
+URL=https://www.python.org/ftp/python/${APPVER}/${APPFILE}
 
-DOWNLOAD_DIR=$INSTALL_DIR/_downloaded
-BUILD_DIR=$INSTALL_DIR/_build
-SOURCE_DIR=$INSTALL_DIR/_source
+# OpenSSL
+OPENSSLVERSION="1.1.0g"
+OPENSSLFILE=openssl-${OPENSSLVERSION}.tar.gz 
+OPENSSLURL=https://www.openssl.org/source/old/1.1.0/${OPENSSLFILE}
+OPENSSLDIR=openssl-${OPENSSLVERSION}
 
+# Setting up the directory structure
+INSTALL_DIR=${PREFIX}/install
+mkdir ${INSTALL_DIR}
+cd ${INSTALL_DIR}
+DOWNLOAD_DIR=${INSTALL_DIR}/_downloaded
 mkdir ${DOWNLOAD_DIR}
-mkdir ${SOURCE_DIR}
+BUILD_DIR=${INSTALL_DIR}/_build
 mkdir ${BUILD_DIR}
+SOURCE_DIR=${INSTALL_DIR}/_source
+mkdir ${SOURCE_DIR}
 
-
-echo "Downloading archive file..."
+# Download
+echo "Downloading"
 cd  ${DOWNLOAD_DIR}
 if [ -e ${APPFILE} ] ; then
-	echo "...File already downloaded";
+	echo "Python already downloaded";
 else
+	echo "Downloading Python"
 	if which curl >/dev/null; then
 		curl -L -O ${URL} ;
 	else
 		wget ${URL};		
 	fi
 fi
-
 if [ -e ${OPENSSLFILE} ] ; then
-	echo "...openssl file already downloaded";
+	echo "OpenSSL already downloaded";
 else
+	echo "Downloading OpenSSL"
 	if which curl >/dev/null; then
 		curl -L -O ${OPENSSLURL} ;
 	else
 		wget ${OPENSSLURL};
 	fi
 fi
-
 cd ..
 echo "Done"
 
-
+# Unpack
+echo "Unpacking"
 cd ${SOURCE_DIR}
 rm -Rf ${APP_DIR}
-echo "Unpacking source files.."
+echo "Unpacking Python..."
 tar -xf ${DOWNLOAD_DIR}/${APPFILE}
-echo "..Done"
-
 cd ${SOURCE_DIR}
 rm -Rf ${OPENSSLDIR}
-echo "Unpacking openssl source files.."
+echo "Unpacking OpenSSL..."
 tar -xf ${DOWNLOAD_DIR}/${OPENSSLFILE} || exit $?
-echo "..Done"
 cd ..
+echo "Done"
 
-echo "Building files.."
-
-cd ${SOURCE_DIR}
-cd ${OPENSSLDIR}
-
-
+# Build
+echo "Building"
 MACHINE=`(uname -m) 2>/dev/null`
 PLATFORM=`uname`
-
+echo "Building OpenSSL"
+cd ${SOURCE_DIR}/${OPENSSLDIR}
 if [ ${PLATFORM} == 'Darwin' ]; then
 	if [ ${MACHINE} == 'x86_64' ]; then
-        	./Configure darwin64-x86_64-cc \
-    			--prefix=${PREFIX}  \
-    			--openssldir=${PREFIX}/openssl \
-    			--shared
+		./Configure darwin64-x86_64-cc \
+			--prefix=${PREFIX}  \
+			--openssldir=${PREFIX}/openssl \
+			--shared
 	else
-        	./Configure darwin64-i386-cc \
-    			--prefix=${PREFIX}  \
-    			--openssldir=${PREFIX}/openssl \
-    			--shared
+		./Configure darwin64-i386-cc \
+			--prefix=${PREFIX}  \
+			--openssldir=${PREFIX}/openssl \
+			--shared
 	fi
 else
-./config \
-    --prefix=${PREFIX}  \
-    --openssldir=${PREFIX}/openssl \
-    --shared
+	./config \
+		--prefix=${PREFIX}  \
+		--openssldir=${PREFIX}/openssl \
+		--shared
 fi
 make
-
 make install
-
+echo "Build Python"
 cd ${BUILD_DIR}
 rm -Rf ${APP_DIR}
 mkdir ${APP_DIR}
 cd ${APP_DIR}
-
-UNAME=`uname`
-if [ $UNAME == 'Darwin' ] ; then
+if [ $PLATFORM == 'Darwin' ] ; then
 	${SOURCE_DIR}/${APP_DIR}/configure \
 		--with-dyld \
 		--prefix=${PREFIX} \
 	    --enable-unicode=ucs4\
 		--program-suffix=.exe ;
-#--enable-framework=${PREFIX}/Framework \
-#--enable-universal-sdk \
 else
 	${SOURCE_DIR}/${APP_DIR}/configure \
 		--prefix=${PREFIX} \
@@ -139,9 +130,8 @@ else
 	    --enable-unicode=ucs4\
 		--program-suffix=.exe ;
 fi
+make 
+make install
+echo "Done"
 
-make 
-make install
-make 
-make install
-echo "..Done"
+echo "Install complete"
--- doc/install/install-python3.sh
+++ doc/install/install-python3.sh
@@ -6,123 +6,125 @@
 # date  : 2009 - 05 -18
 #
 
-APPVER=3.6.1
-APPFILE=Python-${APPVER}.tgz
-APP_DIR=Python-${APPVER}
-URL=https://www.python.org/ftp/python/${APPVER}/${APPFILE}
-
-
-OPENSSLVERSION="1.0.2m"
-OPENSSLFILE=openssl-${OPENSSLVERSION}.tar.gz 
-OPENSSLURL=http://mirrors.ibiblio.org/openssl/source/old/1.0.2/${OPENSSLFILE}
-OPENSSLDIR=openssl-${OPENSSLVERSION}
-
+# Prerequisites directory check
 if [ -z ${PREFIX} ]; then
 	echo The PREFIX variable is not set, please set it to an user directory
 	exit 1
 fi
-
 if [ ! -d ${PREFIX} ]; then
 	echo ${PREFIX} directory does not exists, please create it first!
 	exit 1
- 
 fi
 
- 
+# Python
+APPVER=3.6.5
+APPFILE=Python-${APPVER}.tgz
+APP_DIR=Python-${APPVER}
+URL=https://www.python.org/ftp/python/${APPVER}/${APPFILE}
 
-INSTALL_DIR=$PREFIX/install
-mkdir $INSTALL_DIR
-cd $INSTALL_DIR
+# OpenSSL
+OPENSSLVERSION="1.1.0g"
+OPENSSLFILE=openssl-${OPENSSLVERSION}.tar.gz 
+OPENSSLURL=https://www.openssl.org/source/old/1.1.0/${OPENSSLFILE}
+OPENSSLDIR=openssl-${OPENSSLVERSION}
 
-DOWNLOAD_DIR=$INSTALL_DIR/_downloaded
-BUILD_DIR=$INSTALL_DIR/_build
-SOURCE_DIR=$INSTALL_DIR/_source
-
+# Setting up the directory structure
+INSTALL_DIR=${PREFIX}/install
+mkdir ${INSTALL_DIR}
+cd ${INSTALL_DIR}
+DOWNLOAD_DIR=${INSTALL_DIR}/_downloaded
 mkdir ${DOWNLOAD_DIR}
-mkdir ${SOURCE_DIR}
+BUILD_DIR=${INSTALL_DIR}/_build
 mkdir ${BUILD_DIR}
+SOURCE_DIR=${INSTALL_DIR}/_source
+mkdir ${SOURCE_DIR}
 
-
-echo "Downloading archive file..."
+# Download
+echo "Downloading"
 cd  ${DOWNLOAD_DIR}
 if [ -e ${APPFILE} ] ; then
-	echo "...File already downloaded";
+	echo "Python already downloaded";
 else
+	echo "Downloading Python"
 	if which curl >/dev/null; then
-		curl -O ${URL} ;
+		curl -L -O ${URL} ;
 	else
 		wget ${URL};		
 	fi
 fi
-
 if [ -e ${OPENSSLFILE} ] ; then
-	echo "...openssl file already downloaded";
+	echo "OpenSSL already downloaded";
 else
+	echo "Downloading OpenSSL"
 	if which curl >/dev/null; then
-		curl -O ${OPENSSLURL} ;
+		curl -L -O ${OPENSSLURL} ;
 	else
 		wget ${OPENSSLURL};
 	fi
 fi
-
 cd ..
 echo "Done"
 
-
+# Unpack
+echo "Unpacking"
 cd ${SOURCE_DIR}
 rm -Rf ${APP_DIR}
-echo "Unpacking source files.."
+echo "Unpacking Python..."
 tar -xf ${DOWNLOAD_DIR}/${APPFILE}
-echo "..Done"
-
 cd ${SOURCE_DIR}
 rm -Rf ${OPENSSLDIR}
-echo "Unpacking openssl source files.."
-tar -xf ${DOWNLOAD_DIR}/${OPENSSLFILE}
-echo "..Done"
+echo "Unpacking OpenSSL..."
+tar -xf ${DOWNLOAD_DIR}/${OPENSSLFILE} || exit $?
 cd ..
+echo "Done"
 
-echo "Building files.."
-
-cd ${SOURCE_DIR}
-cd ${OPENSSLDIR}
-
-
+# Build
+echo "Building"
 MACHINE=`(uname -m) 2>/dev/null`
-
-./config \
-    --prefix=${PREFIX}  \
-    --openssldir=${PREFIX}/openssl \
-    --shared
-
+PLATFORM=`uname`
+echo "Building OpenSSL"
+cd ${SOURCE_DIR}/${OPENSSLDIR}
+if [ ${PLATFORM} == 'Darwin' ]; then
+	if [ ${MACHINE} == 'x86_64' ]; then
+		./Configure darwin64-x86_64-cc \
+			--prefix=${PREFIX}  \
+			--openssldir=${PREFIX}/openssl \
+			--shared
+	else
+		./Configure darwin64-i386-cc \
+			--prefix=${PREFIX}  \
+			--openssldir=${PREFIX}/openssl \
+			--shared
+	fi
+else
+	./config \
+		--prefix=${PREFIX}  \
+		--openssldir=${PREFIX}/openssl \
+		--shared
+fi
 make
-
 make install
-
+echo "Build Python"
 cd ${BUILD_DIR}
 rm -Rf ${APP_DIR}
 mkdir ${APP_DIR}
 cd ${APP_DIR}
-
-UNAME=`uname`
-if [ $UNAME == 'Darwin' ] ; then
+if [ $PLATFORM == 'Darwin' ] ; then
 	${SOURCE_DIR}/${APP_DIR}/configure \
 		--with-dyld \
 		--prefix=${PREFIX} \
 	    --enable-unicode=ucs4\
 		--program-suffix=.exe ;
-#--enable-framework=${PREFIX}/Framework \
-#--enable-universal-sdk \
 else
 	${SOURCE_DIR}/${APP_DIR}/configure \
 		--prefix=${PREFIX} \
+		--libdir=${PREFIX}/lib \
 		--enable-shared \
 	    --enable-unicode=ucs4\
 		--program-suffix=.exe ;
 fi
+make 
+make install
+echo "Done"
 
-make 
-make install
-make 
-make install
-echo "..Done"
+echo "Install complete"
