1d0b63272e955f4b0d0e15765c508453e24d1138
Steven Rieder <steven@rieder.nl>
2018-10-09 17:22:36 +0200

    Make example scripts more compliant with the AMUSE code style guide (#312)
    * Autopep8 syntax corrections on examples/simple scripts
    (with some manual fixes where autopep8 made strange choices)
    
    This makes the examples (mostly) complient with the Amuse style guide
    
    - from `amuse.lab import *` is still used for the 'tutorial' examples, but not in the other examples (textbook examples remain untouched)

--- examples/simple/pythagorean.py
+++ examples/simple/pythagorean.py
@@ -3,75 +3,76 @@ Calculates the Pythagorean 3-body problem using different values for
 the smoothing length in the n-body code.
 """
 
-import numpy
-import time
+# import numpy
+# import time
 
-from amuse.community.phiGRAPE.interface import PhiGRAPE
+# from amuse.community.phiGRAPE.interface import PhiGRAPE
 from amuse.community.hermite0.interface import Hermite
-from amuse.community.huayno.interface import Huayno
-from amuse.community.bhtree.interface import BHTree
+# from amuse.community.huayno.interface import Huayno
+# from amuse.community.bhtree.interface import BHTree
 from amuse.units import nbody_system
 from amuse.units.quantities import AdaptingVectorQuantity
 from matplotlib import pyplot
 from amuse.datamodel import Particles
 
+
 def new_particles():
     particles = Particles(3)
-    
-    particles.mass=[3.,4.,5.] | nbody_system.mass
+
+    particles.mass = [3., 4., 5.] | nbody_system.mass
     particles.position = [
         [1, 3, 0],
         [-2, -1, 0],
         [1, -1, 0],
     ] | nbody_system.length
-        
-    particles.velocity = [0.,0.,0.] | nbody_system.speed
+
+    particles.velocity = [0., 0., 0.] | nbody_system.speed
     particles.radius = 0 | nbody_system.length
-    
+
     return particles
 
-def run_pyth(interface,tend=100,dt=0.125,parameters=[]):
-  
+
+def run_pyth(interface, tend=100, dt=0.125, parameters=[]):
+
     code = interface()
-    
-    for name,value in parameters:
+
+    for name, value in parameters:
         setattr(code.parameters, name, value)
-        
+
     code.particles.add_particles(new_particles())
     code.commit_particles()
 
     x = AdaptingVectorQuantity()
     y = AdaptingVectorQuantity()
-    t=0. | nbody_system.time
+    t = 0. | nbody_system.time
     while(t < tend-dt/2):
-        t=t+dt
+        t = t+dt
         code.evolve_model(t)
         x.append(code.particles.x)
         y.append(code.particles.y)
     code.stop()
 
-    return x,y
+    return x, y
+
 
 if __name__ == "__main__":
-  codes_to_run=[ ('Hermite0, $\eta=0.03$', Hermite,  [("dt_param",0.03)] ),
-                 ('Hermite0, $\eta=0.01$', Hermite,  [("dt_param",0.01)] ),
-                 ('Hermite0, $\eta=0.003$', Hermite,  [("dt_param",0.003)] ),
-                 ('Hermite0, $\eta=0.001$', Hermite,  [("dt_param",0.001)] ) ]
-  N=(len(codes_to_run)-1)/2+1
-  f=pyplot.figure(figsize=(8,4*N))
-  
-  for i,(label,interface,parameters) in enumerate(codes_to_run):
-    x,y=run_pyth(interface,tend=100 | nbody_system.time ,dt=0.0625 | nbody_system.time,parameters=parameters)
-    x = x.value_in(nbody_system.length)
-    y = y.value_in(nbody_system.length)
-    subplot=f.add_subplot(N,2,i+1)
-    subplot.plot(x[:,0],y[:,0],'r')
-    subplot.plot(x[:,1],y[:,1],'b')
-    subplot.plot(x[:,2],y[:,2],'g')
-    subplot.set_title(label)
-    subplot.set_xlim(-8,8)
-    subplot.set_ylim(-6,6)
-  pyplot.show()
+    codes_to_run = [('Hermite0, $\eta=0.03$', Hermite, [("dt_param", 0.03)]),
+                    ('Hermite0, $\eta=0.01$', Hermite, [("dt_param", 0.01)]),
+                    ('Hermite0, $\eta=0.003$', Hermite, [("dt_param", 0.003)]),
+                    ('Hermite0, $\eta=0.001$', Hermite, [("dt_param", 0.001)])]
+    N = (len(codes_to_run)-1)/2+1
+    f = pyplot.figure(figsize=(8, 4*N))
 
-
-  
+    for i, (label, interface, parameters) in enumerate(codes_to_run):
+        x, y = run_pyth(interface, tend=100 | nbody_system.time,
+                        dt=0.0625 | nbody_system.time, parameters=parameters)
+        x = x.value_in(nbody_system.length)
+        y = y.value_in(nbody_system.length)
+        subplot = f.add_subplot(N, 2, i+1)
+        subplot.plot(x[:, 0], y[:, 0], 'r')
+        subplot.plot(x[:, 1], y[:, 1], 'b')
+        subplot.plot(x[:, 2], y[:, 2], 'g')
+        subplot.set_title(label)
+        subplot.set_xlim(-8, 8)
+        subplot.set_ylim(-6, 6)
+    pyplot.show()
--- examples/tutorial/plot_cluster.py
+++ examples/tutorial/plot_cluster.py
@@ -4,17 +4,18 @@
 """
 from matplotlib import pyplot
 from amuse.plot import scatter, xlabel, ylabel
-from amuse.lab import *
 from amuse.io import store
 from optparse import OptionParser
 
-def main(filename = "nbody.hdf5", lim=3):
+
+def main(filename="nbody.hdf5", lim=3):
     pyplot.ion()
-    storage = store.StoreHDF(filename,"r")
+    storage = store.StoreHDF(filename, "r")
     stars = storage.load()
 #    lim = 4*stars.center_of_mass().length().value_in(stars.x.unit)
-    lim = 2*max(max(stars.x).value_in(stars.x.unit),  stars.center_of_mass().length().value_in(stars.x.unit))
-    m =  1 + 3.0*stars.mass/min(stars.mass)
+    lim = 2*max(max(stars.x).value_in(stars.x.unit),
+                stars.center_of_mass().length().value_in(stars.x.unit))
+    m = 1 + 3.0*stars.mass/min(stars.mass)
     for si in stars.history:
         time = si.get_timestamp()
         pyplot.title("Cluster at t="+str(time))
@@ -28,14 +29,14 @@ def main(filename = "nbody.hdf5", lim=3):
         pyplot.cla()
     pyplot.show()
 
+
 def new_option_parser():
     result = OptionParser()
-    result.add_option("-f", dest="filename", default = "nbody.hdf5",
+    result.add_option("-f", dest="filename", default="nbody.hdf5",
                       help="output filename [nbody.hdf5]")
     return result
 
+
 if __name__ in ('__main__', '__plot__'):
-    o, arguments  = new_option_parser().parse_args()
+    o, arguments = new_option_parser().parse_args()
     main(**o.__dict__)
-
-
