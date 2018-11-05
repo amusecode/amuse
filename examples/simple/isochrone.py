1417df50289389a68af9a05fa464df7a12161238
Steven Rieder <steven@rieder.nl>
2018-11-05 11:52:03 +0100

    Update isochrone.py

--- examples/simple/isochrone.py
+++ examples/simple/isochrone.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: ascii -*-
 """
 Generates an isochrone of a cluster of stars in a Hertzsprung-Russell diagram
 """
@@ -15,8 +15,8 @@ def simulate_stellar_evolution(particles, endtime):
     stellar_evolution = SeBa()
     stellar_evolution.particles.add_particles(particles)
     from_code_to_model = stellar_evolution.particles.new_channel_to(particles)
-    print(("Evolving {0} stars using {1} up to {2}.".format(
-        len(particles), stellar_evolution.__class__.__name__, endtime)))
+    print("Evolving {0} stars using {1} up to {2}.".format(
+        len(particles), stellar_evolution.__class__.__name__, endtime))
     stellar_evolution.evolve_model(endtime)
     from_code_to_model.copy_all_attributes()
     stellar_evolution.stop()
