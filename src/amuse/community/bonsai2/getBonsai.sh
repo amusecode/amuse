a1e3df3417fd53d630381640313cb791a5e6abf3
Jeroen Bédorf <jeroen@bedorf.net>
2019-01-18 17:29:07 +0100

    Cleanup

--- src/amuse/community/bonsai2/getBonsai.sh
+++ src/amuse/community/bonsai2/getBonsai.sh
@@ -7,18 +7,12 @@
 sha=d1c23d33263090fcf681aa727f300b31efef0f03
 
 rm Bonsai2016.zip
+rm -r src/
 
 wget --no-check-certificate  https://github.com/treecode/Bonsai/archive/${sha}.zip -O Bonsai2016.zip
 
-
 unzip Bonsai2016.zip
 mv Bonsai-${sha} src
-
-# apply patch until changes in the main Bonsai repository are applied
-# patch -p0  < fix_for_new_gcc.patch
-
-#mv Bonsai-Bonsai2016/* .
-#rm -r Bonsai-Bonsai2016/
 
 cd src/runtime
 
