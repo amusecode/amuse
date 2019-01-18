d04437032fbd96a5933f934d3437f71592a50a51
Jeroen Bédorf <jeroen@bedorf.net>
2019-01-18 17:25:41 +0100

    This brings Bonsai2 up to date with the latest version (Jan 18, 2019)

--- src/amuse/community/bonsai2/getBonsai.sh
+++ src/amuse/community/bonsai2/getBonsai.sh
@@ -4,7 +4,7 @@
 #LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgmp.so.10 curl https://codeload.github.com/treecode/Bonsai/zip/Bonsai2016 -o Bonsai2016.zip
 #LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgmp.so.10 curl https://github.com/treecode/Bonsai/archive/4be0b4194eba1825cd1a91bb5104e1d979e44c79.zip -o Bonsai2016.zip
 
-sha=54953e89903ee824c82ede7f6b86b19f3d53d254
+sha=d1c23d33263090fcf681aa727f300b31efef0f03
 
 rm Bonsai2016.zip
 
@@ -15,14 +15,14 @@ unzip Bonsai2016.zip
 mv Bonsai-${sha} src
 
 # apply patch until changes in the main Bonsai repository are applied
-patch -p0  < fix_for_new_gcc.patch
+# patch -p0  < fix_for_new_gcc.patch
 
 #mv Bonsai-Bonsai2016/* .
 #rm -r Bonsai-Bonsai2016/
 
 cd src/runtime
 
-cmake -DUSE_MPI=0 -DUSE_MPIMT=0 -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake -DUSE_MPI=0 -DUSE_MPIMT=0 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_THRUST=1 -DUSE_CUB=0
 
 make -j
-make 
+make
