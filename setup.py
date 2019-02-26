e085d9c33478734bf901c285856943a7be0c11b8
Steven Rieder <steven@rieder.nl>
2019-02-26 13:11:34 +0000

    Whitespace fixes

--- setup.py
+++ setup.py
@@ -36,21 +36,21 @@ use_setuptools()
 extensions = []
 
 class Clean(clean):
-    
+
     def run(self):
         for cmd_name in self.get_sub_commands():
             self.run_command(cmd_name)
-            
+
 class Install(install):
-    
+
     def run(self):
         for cmd_name in self.get_sub_commands():
             self.run_command(cmd_name)
-            
+
         install.run(self)
 
 mapping_from_command_name_to_command_class = {
-    'build_latex':build_latex, 
+    'build_latex':build_latex,
     'build_codes':BuildCodes,
     'build_libraries':BuildLibraries,
     'build_code':BuildOneCode,
@@ -59,7 +59,7 @@ mapping_from_command_name_to_command_class = {
     'dist_clean':DistCleanCodes,
     'clean_python':clean,
     'clean': Clean,
-    'tests':run_tests, 
+    'tests':run_tests,
     'generate_main': generate_main,
     'generate_install_ini': GenerateInstallIni,
     'install':install,
@@ -68,14 +68,14 @@ mapping_from_command_name_to_command_class = {
 
 if sys.hexversion > 0x03000000:
     mapping_from_command_name_to_command_class['build_py'] = build_py_2to3
-    
-build.sub_commands.insert(0,('configure_codes', None))
-build.sub_commands.append(('build_codes', None))
-Clean.sub_commands.append(('clean_codes',None))
-Clean.sub_commands.append(('clean_python',None))
 
-Install.sub_commands.insert(0, ('generate_install_ini',None) )
-Install.sub_commands.append(('install_libraries',None))
+build.sub_commands.insert(0, ('configure_codes', None))
+build.sub_commands.append(('build_codes', None))
+Clean.sub_commands.append(('clean_codes', None))
+Clean.sub_commands.append(('clean_python', None))
+
+Install.sub_commands.insert(0, ('generate_install_ini', None))
+Install.sub_commands.append(('install_libraries', None))
 
 def find_packages(where='.', exclude=()):
     """Return a list all Python packages found within directory 'where'
@@ -87,18 +87,20 @@ def find_packages(where='.', exclude=()):
     'foo' itself).
     """
     out = []
-    stack=[(convert_path(where), '')]
+    stack = [(convert_path(where), '')]
     while stack:
-        where,prefix = stack.pop(0)
+        where, prefix = stack.pop(0)
         for name in os.listdir(where):
-            fn = os.path.join(where,name)
-            if ('.' not in name and os.path.isdir(fn) and
-                os.path.isfile(os.path.join(fn,'__init__.py'))
+            fn = os.path.join(where, name)
+            if (
+                    '.' not in name and os.path.isdir(fn) and
+                    os.path.isfile(os.path.join(fn, '__init__.py'))
             ):
-                out.append(prefix+name); stack.append((fn,prefix+name+'.'))
+                out.append(prefix+name)
+                stack.append((fn, prefix+name+'.'))
     for pat in list(exclude)+['ez_setup', 'distribute_setup']:
         from fnmatch import fnmatchcase
-        out = [item for item in out if not fnmatchcase(item,pat)]
+        out = [item for item in out if not fnmatchcase(item, pat)]
     return out
 
 def find_data_files(srcdir, destdir, *wildcards, **kw):
@@ -120,12 +122,12 @@ def find_data_files(srcdir, destdir, *wildcards, **kw):
                     names.append(filename)
         if names:
             destdirname = dirnameconverter.sub(destdir, dirname)
-            lst.append( (destdirname, names ) )
+            lst.append((destdirname, names))
 
     file_list = []
     recursive = kw.get('recursive', True)
     converter = re.compile('^({0})'.format(srcdir))
-    
+
     if recursive:
         walk(srcdir, walk_helper, (file_list, wildcards, converter, destdir))
     else:
@@ -134,11 +136,11 @@ def find_data_files(srcdir, destdir, *wildcards, **kw):
                     [os.path.basename(f) for f in glob.glob(os.path.join(srcdir, '*'))])
     return file_list
 
-all_data_files = find_data_files('data', 'share/amuse/data', '*', recursive = True)
-all_data_files.extend(find_data_files('support', 'share/amuse/support', '*', recursive = False))
-#all_data_files.extend(find_data_files('support3', 'share/amuse/support3', '*', recursive = False))
-#~ all_data_files.extend(find_data_files('lib', 'share/amuse/lib', '*.h', '*.a', '*.mod', '*.inc', '*.so', '*.dylib', recursive = True))
-all_data_files.append(('share/amuse',['./config.mk','./build.py']))
+all_data_files = find_data_files('data', 'share/amuse/data', '*', recursive=True)
+all_data_files.extend(find_data_files('support', 'share/amuse/support', '*', recursive=False))
+# all_data_files.extend(find_data_files('support3', 'share/amuse/support3', '*', recursive = False))
+# all_data_files.extend(find_data_files('lib', 'share/amuse/lib', '*.h', '*.a', '*.mod', '*.inc', '*.so', '*.dylib', recursive = True))
+all_data_files.append(('share/amuse', ['./config.mk', './build.py']))
 
 packages = find_packages('src')
 packages.extend(['amuse.test.suite.' + x for x in find_packages('test')])
@@ -147,7 +149,7 @@ packages.extend(['amuse.examples.' + x for x in find_packages('examples')])
 package_data = {
     'amuse.rfi.tools': ['*.template'],
     'amuse.test.suite.core_tests': [
-        '*.txt', '*.dyn', '*.ini', 
+        '*.txt', '*.dyn', '*.ini',
         '*.nemo',
         '*.dat', 'gadget_snapshot'
     ],
@@ -166,15 +168,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name = 'amuse',
-    version = "11",
-    cmdclass = mapping_from_command_name_to_command_class,
-    ext_modules = extensions,
-    package_dir = {'': 'src', 'amuse.test.suite' :'test', 'amuse.examples' : 'examples'},
-    packages =  packages,
-    package_data = package_data,
-    data_files = all_data_files,
-    classifiers = [
+    name='amuse',
+    version="11",
+    cmdclass=mapping_from_command_name_to_command_class,
+    ext_modules=extensions,
+    package_dir={'': 'src', 'amuse.test.suite' :'test', 'amuse.examples' : 'examples'},
+    packages=packages,
+    package_data=package_data,
+    data_files=all_data_files,
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
@@ -191,17 +193,17 @@ setup(
         'Programming Language :: Fortran',
         'Topic :: Scientific/Engineering :: Astronomy',
     ],
-    url = 'http://www.amusecode.org/',
-    author_email = 'info@amusecode.org',
-    author = 'The Amuse Team',
+    url='http://www.amusecode.org/',
+    author_email='info@amusecode.org',
+    author='The Amuse Team',
     description='The Astrophysical Multipurpose Software Environment',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires = [
+    install_requires=[
         'docutils>=0.6',
         'numpy>=1.2.2',
         'nose>=0.11.1',
         'mpi4py>=1.1.0',
-        'h5py>=1.1.0',        
+        'h5py>=1.1.0',
     ]
 )
