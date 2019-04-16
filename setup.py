9c99786e320e647f9e01e6af14d686b6570e7436
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-04-16 11:23:03 +0200

    move helper find_data_files to support

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import sys
 import os
-import fnmatch
-import re
-import glob
 
 from distutils.command.build import build
 from distutils.command.clean import clean
@@ -19,16 +16,10 @@ from support.setup_codes import (
     ConfigureCodes, GenerateInstallIni, InstallLibraries,
 )
 from support.run_tests import run_tests
+from support.misc import find_data_files
 
 if sys.hexversion > 0x03000000:
     from distutils.command.build_py import build_py_2to3
-    from os import walk as py_walk
-    def walk(top, callback, args):
-        for root, dirs, files in py_walk(top):
-            callback(args, root, files)
-else:
-    from os.path import walk
-
 
 use_setuptools()
 #include_dirs.append(sysconfig.get_python_inc())
@@ -76,39 +67,6 @@ Clean.sub_commands.append(('clean_python', None))
 
 Install.sub_commands.insert(0, ('generate_install_ini', None))
 Install.sub_commands.append(('install_libraries', None))
-
-def find_data_files(srcdir, destdir, *wildcards, **kw):
-    """
-    get a list of all files under the srcdir matching wildcards,
-    returned in a format to be used for install_data
-    """
-    def walk_helper(arg, dirname, files):
-        if '.svn' in dirname:
-            return
-        names = []
-        lst, wildcards, dirnameconverter, destdir = arg
-        for wc in wildcards:
-            wc_name = os.path.normpath(os.path.join(dirname, wc))
-            for f in files:
-                filename = os.path.normpath(os.path.join(dirname, f))
-
-                if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(filename):
-                    names.append(filename)
-        if names:
-            destdirname = dirnameconverter.sub(destdir, dirname)
-            lst.append((destdirname, names))
-
-    file_list = []
-    recursive = kw.get('recursive', True)
-    converter = re.compile('^({0})'.format(srcdir))
-
-    if recursive:
-        walk(srcdir, walk_helper, (file_list, wildcards, converter, destdir))
-    else:
-        walk_helper((file_list, wildcards, converter, destdir),
-                    srcdir,
-                    [os.path.basename(f) for f in glob.glob(os.path.join(srcdir, '*'))])
-    return file_list
 
 all_data_files = find_data_files('data', 'share/amuse/data', '*', recursive=True)
 # all_data_files.extend(find_data_files('support', 'share/amuse/support', '*', recursive=False))
