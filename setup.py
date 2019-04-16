48f5cf037775d208336c91d6304b8063b7d34688
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-04-16 13:17:35 +0200

    cleanup setup.py matching setup in packages

--- setup.py
+++ setup.py
@@ -18,6 +18,42 @@ from support.misc import find_data_files
 if sys.hexversion > 0x03000000:
     from distutils.command.build_py import build_py_2to3
 
+name = 'amuse'
+version = "12.0"
+author = 'The AMUSE team'
+author_email = 'info@amusecode.org'
+license = "Apache License 2.0"
+url = 'http://www.amusecode.org/'
+install_requires = [
+    'wheel>=0.32',
+    'docutils>=0.6',
+    'numpy>=1.2.2',
+    'nose>=0.11.1',
+    'mpi4py>=1.1.0',
+    'h5py>=1.1.0',
+]
+description = 'The Astrophysical Multipurpose Software Environment'
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+long_description_content_type = "text/markdown"
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: POSIX',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: C',
+    'Programming Language :: C++',
+    'Programming Language :: Fortran',
+    'Topic :: Scientific/Engineering :: Astronomy',
+]
+
 extensions = []
 
 class Clean(clean):
@@ -35,20 +71,20 @@ class Install(install):
         install.run(self)
 
 mapping_from_command_name_to_command_class = {
-    'build_latex':build_latex,
-    'build_codes':BuildCodes,
-    'build_libraries':BuildLibraries,
-    'build_code':BuildOneCode,
-    'configure_codes':ConfigureCodes,
-    'clean_codes':CleanCodes,
-    'dist_clean':DistCleanCodes,
-    'clean_python':clean,
+    'build_latex': build_latex,
+    'build_codes': BuildCodes,
+    'build_libraries': BuildLibraries,
+    'build_code': BuildOneCode,
+    'configure_codes': ConfigureCodes,
+    'clean_codes': CleanCodes,
+    'dist_clean': DistCleanCodes,
+    'clean_python': clean,
     'clean': Clean,
-    'tests':run_tests,
+    'tests': run_tests,
     'generate_main': generate_main,
     'generate_install_ini': GenerateInstallIni,
-    'install':install,
-    'install_libraries':InstallLibraries
+    'install': install,
+    'install_libraries': InstallLibraries
 }
 
 if sys.hexversion > 0x03000000:
@@ -63,9 +99,6 @@ Install.sub_commands.insert(0, ('generate_install_ini', None))
 Install.sub_commands.append(('install_libraries', None))
 
 all_data_files = find_data_files('data', 'share/amuse/data', '*', recursive=True)
-# all_data_files.extend(find_data_files('support', 'share/amuse/support', '*', recursive=False))
-# all_data_files.extend(find_data_files('support3', 'share/amuse/support3', '*', recursive = False))
-# all_data_files.extend(find_data_files('lib', 'share/amuse/lib', '*.h', '*.a', '*.mod', '*.inc', '*.so', '*.dylib', recursive = True))
 all_data_files.append(('share/amuse', ['./config.mk', './build.py']))
 
 packages = find_packages('src')
@@ -90,12 +123,17 @@ package_data = {
     ]
 }
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
-    name='amuse',
-    version="11",
+    name=name,
+    version=version,
+    classifiers=classifiers,
+    url=url,
+    author_email=author_email,
+    author=author,
+    description=description,
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    install_requires=install_requires,
     cmdclass=mapping_from_command_name_to_command_class,
     ext_modules=extensions,
     package_dir={'': 'src', 'amuse.test.suite' :'test', 'amuse.examples' : 'examples'},
@@ -103,34 +141,4 @@ setup(
     package_data=package_data,
     data_files=all_data_files,
     scripts=[ "amusifier" ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: C',
-        'Programming Language :: C++',
-        'Programming Language :: Fortran',
-        'Topic :: Scientific/Engineering :: Astronomy',
-    ],
-    url='http://www.amusecode.org/',
-    author_email='info@amusecode.org',
-    author='The Amuse Team',
-    description='The Astrophysical Multipurpose Software Environment',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=[
-        'docutils>=0.6',
-        'numpy>=1.2.2',
-        'nose>=0.11.1',
-        'mpi4py>=1.1.0',
-        'h5py>=1.1.0',
-    ]
 )
