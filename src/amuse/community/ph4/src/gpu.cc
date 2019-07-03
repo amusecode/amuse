2dfde2a2270867ab1e0405c8cd22a9db571acb28
ipelupessy <i.pelupessy@esciencecenter.nl>
2019-07-03 20:49:34 +0200

    continue fixes

--- src/amuse/community/ph4/src/gpu.cc
+++ src/amuse/community/ph4/src/gpu.cc
@@ -37,7 +37,7 @@ void jdata::initialize_gpu(bool reinitialize)	// default = false
 {
 #ifdef GPU
 #ifndef NOMPI
-    clusterid = MPI_COMM_WORLD.Get_rank();
+    MPI_Comm_rank(MPI_COMM_WORLD, &clusterid);
 #endif
     const char *in_function = "jdata::initialize_gpu";
     if (DEBUG > 2 && mpi_rank == 0) PRL(in_function);
@@ -807,8 +807,8 @@ void jdata::get_densities_on_gpu()	// under development
 #ifndef NOMPI
 	    cout << mpi_rank << " sending neighbors to" << mpi_rank-offset
 		 << endl << flush;
-	    MPI_Send(mpi_comm,pneighbors, nj*knn, MPI_INT,
-			  mpi_rank-offset, 42);
+	    MPI_Send(pneighbors, nj*knn, MPI_INT,
+			  mpi_rank-offset, 42, mpi_comm);
 	    cout << mpi_rank << " send done" << endl << flush;
 #endif
 
@@ -826,8 +826,8 @@ void jdata::get_densities_on_gpu()	// under development
         cout << mpi_rank << " receiving neighbors from"
 		     << mpi_rank+offset
 		     << endl << flush;
-		MPI_Recv(mpi_comm, pneighbors+nj*knn, nj*knn, MPI_INT,
-			      mpi_rank+offset, 42);
+		MPI_Recv(pneighbors+nj*knn, nj*knn, MPI_INT,
+			      mpi_rank+offset, 42, mpi_comm);
 		cout << mpi_rank << " receive done" << endl << flush;
 #endif
 
--- src/amuse/community/ph4/src/test_hosts.cc
+++ src/amuse/community/ph4/src/test_hosts.cc
@@ -10,8 +10,10 @@ void get_local_rank(MPI_Intracomm comm,
 {
     MPI_Barrier(comm);
 
-    int rank = MPI_Get_rank(comm);
-    int size = MPI_Get_size(comm);
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+    int size;
+    MPI_Comm_size(comm, &size);
 
     // Get my hostname.
 
@@ -29,13 +31,13 @@ void get_local_rank(MPI_Intracomm comm,
 	// Send my hostname to process 0.
 
 	int nh = strlen(hostname);
-	MPI_Send(comm, &nh, 1, MPI_INT, 0, 990);
-	MPI_Send(comm, hostname, strlen(hostname), MPI_CHAR, 0, 991);
+	MPI_Send( &nh, 1, MPI_INT, 0, 990, comm);
+	MPI_Send( hostname, strlen(hostname), MPI_CHAR, 0, 991, comm);
 
 	// Get my local size and rank from process 0.
 
-	MPI_Recv(comm, &mylocalrank, 1, MPI_INT, 0, 992);
-	MPI_Recv(comm, &mylocalsize, 1, MPI_INT, 0, 993);
+	MPI_Recv(&mylocalrank, 1, MPI_INT, 0, 992, comm);
+	MPI_Recv(&mylocalsize, 1, MPI_INT, 0, 993, comm);
 
     } else {
 
@@ -48,8 +50,8 @@ void get_local_rank(MPI_Intracomm comm,
 	hostnames[0][nh] = '\0';
 
 	for (int ip = 1; ip < size; ip++) {
-	    MPI_Recv(comm, &nh, 1, MPI_INT, ip, 990);
-	    MPI_Recv(comm, hostnames[ip], nh, MPI_CHAR, ip, 991);
+	    MPI_Recv( &nh, 1, MPI_INT, ip, 990, comm);
+	    MPI_Recv( hostnames[ip], nh, MPI_CHAR, ip, 991, comm);
 	    hostnames[ip][nh] = '\0';	    
 	}
 
@@ -91,8 +93,8 @@ void get_local_rank(MPI_Intracomm comm,
 	// Distribute localsize and localrank to all processes.
 
 	for (int ip = 1; ip < size; ip++) {
-	    MPI_Send(comm, localrank+ip, 1, MPI_INT, ip, 992);
-	    MPI_Send(comm, localsize+ip, 1, MPI_INT, ip, 993);
+	    MPI_Send( localrank+ip, 1, MPI_INT, ip, 992, comm);
+	    MPI_Send( localsize+ip, 1, MPI_INT, ip, 993, comm);
 	}
     }
 
@@ -104,12 +106,14 @@ void run_test(bool verbose = false)
     MPI_Intracomm comm = MPI_COMM_WORLD;
     int mylocalrank, mylocalsize;
     get_local_rank(comm, mylocalrank, mylocalsize, verbose);
-    PRC(MPI_COMM_WORLD.Get_rank()); PRL(mylocalrank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, & rank)
+    PRC(rank); PRL(mylocalrank);
 }
 
 int main(int argc, char *argv[])
 {
-    MPI_Init(argc, argv);
+    MPI_Init(&argc, &argv);
     run_test();
     MPI_Finalize();
 }
