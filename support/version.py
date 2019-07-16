9aaa994ead3c64bd1e9cc018084efb8affcb6a62
Steven Rieder <steven@rieder.nl>
2019-07-16 14:52:08 +0200

    Prepare for amuse-framework 12.0.2

--- support/version.py
+++ support/version.py
@@ -1,6 +1,6 @@
 major_version = 12
 minor_version = 0
-micro_version = 1
+micro_version = 2
 patch_version = ""
 post_version = ""
 version = "%i.%i.%i" % (major_version, minor_version, micro_version)
