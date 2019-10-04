94c4fb4c6852735eeffafc080cb403ac37dd3ae7
Steven Rieder <steven@rieder.nl>
2019-10-04 13:26:05 +0100

    Change default store version to 2.0
    This is a long overdue change in my opinion...

--- src/amuse/io/store.py
+++ src/amuse/io/store.py
@@ -154,7 +154,7 @@ class HDF5FileFormatProcessor(base.FileFormatProcessor):
     def version(self):
         """AMUSE storage version to use, needs to be >= '2.0' if you want
         to store links between particles and grids (default: '1.0')"""
-        return '1.0'
+        return '2.0'
 
     @base.format_option
     def return_working_copy(self):
