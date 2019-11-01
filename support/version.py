4b681c59a8f0b45f9f46f2e1ff3bd78d5e07c655
Steven Rieder <steven@rieder.nl>
2019-11-01 12:24:01 +0100

    require the correct versions

--- support/version.py
+++ support/version.py
@@ -1,13 +1,14 @@
 major_version = 13
 minor_version = 0
 micro_version = 0
-patch_version = "b1"
+patch_version = "b2"
 post_version = ""
 version = "%i.%i.%i" % (major_version, minor_version, micro_version)
 main_version = "%i.%i.0" % (major_version, minor_version)
                           
 if patch_version != "":
     version += "%s" % patch_version
+    main_version = version
 if post_version != "":
     version += "%s" % post_version
 
