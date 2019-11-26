b6cf39c91d345e41938eef29e951c69b4aea3ab6
Steven Rieder <steven@rieder.nl>
2019-11-26 10:53:19 +0000

    Remove compiled documentation

--- docs/examples/syllabus/add_function_to_particleset.py
+++
@@ -1,9 +0,0 @@
-from amuse.lab import *
-
-def function(body):
-    return 0.2*body[0].mass
-
-bodies = Particles(1)
-bodies.mass = 1|units.MSun
-bodies.add_global_function_attribute("new_function", function)
-print bodies.new_function().in_(units.MSun)
