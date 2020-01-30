4e57d2019ceb7863f3f1e614cbc9f42af1e82d2e
silviatoonen <s.toonen@bham.ac.uk>
2020-01-30 20:42:44 +0000

    update metallicity dependence de Jager wind on MS

--- src/amuse/community/seba/src/sstar/starclass/main_sequence.C
+++ src/amuse/community/seba/src/sstar/starclass/main_sequence.C
@@ -125,7 +125,7 @@ void main_sequence::update_wind_constant() {
     if (luminosity > 4000.) {
         real x = min(1.0, (luminosity -4000.0)/500.0);
         dm_dj = x * 9.6310E-15 * pow(radius, 0.81) * pow(luminosity, 1.24) * 
-        pow(get_total_mass(), 0.16)*pow(metalicity/cnsts.parameters(solar_metalicity), 0.5);
+        pow(get_total_mass(), 0.16)*pow(metalicity/cnsts.parameters(solar_metalicity), 0.85);
     }
     
     // Vink 2000, 2001
