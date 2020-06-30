da82f069e2ad5f7b3b39a4747a44741002529608
Steven Rieder <steven@rieder.nl>
2020-06-30 15:54:15 +0100

    update package requirements

--- packages/amuse-tutorial/setup.py
+++ packages/amuse-tutorial/setup.py
@@ -1,5 +1,3 @@
-import sys
-import os
 from support.version import version, main_version
 from support.classifiers import classifiers
 from support.misc import find_data_files
@@ -15,7 +13,9 @@ install_requires = [
     'matplotlib>=2.2',
     'amuse-framework>=%s' % main_version,
     'amuse-bhtree>=%s' % main_version,
-    'amuse-sse>=%s' % main_version,
+    'amuse-hermite>=%s' % main_version,
+    'amuse-seba>=%s' % main_version,
+    'amuse-sphray>=%s' % main_version,
     'notebook',
 ]
 description = 'The Astrophysical Multipurpose Software Environment: tutorial'
@@ -23,7 +23,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 long_description_content_type = "text/markdown"
 
-all_data_files = find_data_files('tutorial', 'share/amuse/tutorial', '*', recursive=True)
+all_data_files = find_data_files(
+    'tutorial', 'share/amuse/tutorial', '*', recursive=True
+)
 
 setup(
     name=name,
@@ -38,7 +40,7 @@ setup(
     long_description_content_type=long_description_content_type,
     install_requires=install_requires,
     python_requires=">=3.5",
-    #~ cmdclass=mapping_from_command_name_to_command_class,
+    # cmdclass=mapping_from_command_name_to_command_class,
     data_files=all_data_files,
-    scripts=[ "bin/amuse-tutorial" ],
+    scripts=["bin/amuse-tutorial"],
 )
