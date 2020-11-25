56d3b725da68069a2546be260ee1369d19bacbbd
Steven Rieder <steven@rieder.nl>
2020-11-25 10:50:54 +0000

    add return value for Brown Dwarf stellar type

--- src/amuse/community/seba/interface.cc
+++ src/amuse/community/seba/interface.cc
@@ -139,6 +139,7 @@ local int translate_stellar_type_to_int(stellar_type stp, const real mass) {
 
   switch (stp) {
     case Brown_Dwarf:
+      return 19;
     case Main_Sequence: 
       if (mass<0.1) {
             return 0;
