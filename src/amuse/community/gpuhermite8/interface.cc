d7fd7ced91c8aa19ee7899d5cea218425c4fe7d9
GFTwrt <GFTwrt@github.com>
2021-03-16 18:29:16 +0100

    added LICENSE and log

--- src/amuse/community/gpuhermite8/interface.cc
+++ src/amuse/community/gpuhermite8/interface.cc
@@ -1,3 +1,31 @@
+/*MIT License
+
+Copyright (c) 2021 Keigo Nitadori
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
+/*
+-ported Keigo Nitadori "eight-16th" to AMUSE by Thomas Schano.
+-added support for QD in addition to DD by Thomas Schano.
+        QD is available at http://crd-legacy.lbl.gov/~dhbailey/mpdist/ (https://github.com/GFTwrt/QD)
+        eight-16th is available at https://github.com/nitadori/HighOrder
+*/
 
 #include <vector>
 #include <limits>
