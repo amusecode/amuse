97561f9ba129d8622b8c7fccceda2d71d77aa29b
Steven Rieder <steven+github@rieder.nl>
2021-06-04 12:58:50 +0100

    Update codes.py

--- src/amuse/codes.py
+++ src/amuse/codes.py
@@ -2,7 +2,7 @@
 import sys
 
 _CODES = [
-'athena', 'capreole', 'cachedse',     'gadget2',      'mesa',      'octgrav',      'twobody',
+'athena', 'capreole', 'cachedse',     'gadget2',      'mesa',         'octgrav',      'twobody',
 'capreole',     'hermite0',     'mocassin',     'phiGRAPE',
 'athena',       'evtwin',       'hop',          'seba',
 'bhtree',       'evtwin2sse',   'interface',    'smallN',
