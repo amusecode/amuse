76ac4129e180a9d814c94c697a0bde87ebaaf3c2
Steven Rieder <steven@rieder.nl>
2021-11-22 15:47:04 +0100

    move imports to top

--- src/amuse/community/mesa/interface.py
+++ src/amuse/community/mesa/interface.py
@@ -1,14 +1,18 @@
 """
 MESA chooser
 """
+from amuse.community.mesa_r2208.interface import \
+    MESAInterface as MESAInterface_2208
+from amuse.community.mesa_r15140.interface import \
+    MESAInterface as MESAInterface_15140
+from amuse.community.mesa_r2208.interface import MESA as MESA_2208
+from amuse.community.mesa_r15140.interface import MESA as MESA_15140
 
 
 def MESAInterface(version="15140", **options):
     if str(version) == "2208":
-        from amuse.community.mesa_r2208.interface import MESAInterface as MESAInterface_2208
         return MESAInterface_2208(**options)
     if str(version) == "15140":
-        from amuse.community.mesa_r15140.interface import MESAInterface as MESAInterface_15140
         return MESAInterface_15140(**options)
     raise AttributeError(
         "This version of MESA is not (yet) supported by AMUSE"
@@ -17,10 +21,8 @@ def MESAInterface(version="15140", **options):
 
 def MESA(version="15140", **options):
     if str(version) == "2208":
-        from amuse.community.mesa_r2208.interface import MESA as MESA_2208
         return MESA_2208(**options)
     if str(version) == "15140":
-        from amuse.community.mesa_r15140.interface import MESA as MESA_15140
         return MESA_15140(**options)
     raise AttributeError(
         "This version of MESA is not (yet) supported by AMUSE"
