e8a31134927ecb0225583195694b3655607d073f
Felicity Guest <F.L.Guest@exeter.ac.uk>
2022-03-28 14:29:11 +0100

    minor reformatting

--- src/amuse/community/arepo/interface.py
+++ src/amuse/community/arepo/interface.py
@@ -6,29 +6,30 @@ from amuse.community import LiteratureReferencesMixIn
 from amuse.community.interface.gd import GravitationalDynamicsInterface
 from amuse.community.interface.gd import GravitationalDynamics
 
+
 class ArepoInterface(
     CodeInterface,
     GravitationalDynamicsInterface,
-    LiteratureReferencesMixIn):
-    
-    include_headers = ['worker_code.h']
-    
+    LiteratureReferencesMixIn
+):
+
+    include_headers = ["worker_code.h"]
+
     def __init__(self, **keyword_arguments):
         CodeInterface.__init__(self, name_of_the_worker="arepo_worker", **keyword_arguments)
         LiteratureReferencesMixIn.__init__(self)
-    
+
     @legacy_function
     def echo_int():
-        function = LegacyFunctionSpecification()  
-        function.addParameter('int_in', dtype='int32', direction=function.IN)
-        function.addParameter('int_out', dtype='int32', direction=function.OUT)
-        function.result_type = 'int32'
+        function = LegacyFunctionSpecification()
+        function.addParameter("int_in", dtype="int32", direction=function.IN)
+        function.addParameter("int_out", dtype="int32", direction=function.OUT)
+        function.result_type = "int32"
         function.can_handle_array = True
         return function
-        
-    
-class Arepo(GravitationalDynamics):
 
-    def __init__(self, **options):
-        GravitationalDynamics.__init__(self,  ArepoInterface(**options), **options)
+
+class Arepo(GravitationalDynamics):
     
+    def __init__(self, **options):
+        GravitationalDynamics.__init__(self, ArepoInterface(**options), **options)
