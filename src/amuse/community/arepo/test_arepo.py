2c1f16add06379e9d8d62c67f9026eed1678f775
Felicity Guest <F.L.Guest@exeter.ac.uk>
2022-04-01 15:50:26 +0100

    add TODO to test_arepo.py

--- src/amuse/community/arepo/test_arepo.py
+++ src/amuse/community/arepo/test_arepo.py
@@ -7,7 +7,7 @@ class ArepoInterfaceTests(TestWithMPI):
     
     def test1(self):
         instance = ArepoInterface()
-        result,error = instance.echo_int(12)
+        result,error = instance.echo_int(12)  # TODO: Update test and add more...
         self.assertEquals(error, 0)
         self.assertEquals(result, 12)
         instance.stop()
