d553de2d14a5245a4db9aff5675a90389c28d4f8
Stephen P. Cook <s.cook4@exeter.ac.uk>
2022-10-13 15:28:51 +0000

    Placeholder functions for evolution code

--- src/amuse/community/arepo/test_simple.py
+++ src/amuse/community/arepo/test_simple.py
@@ -1,6 +1,13 @@
 from amuse.community.arepo import Arepo
+# import unit seconds as s
 
 # Check code runs without errors
 x = Arepo(redirection="none")
 x.initialize_code()
-x.run_sim()
+#x.run_sim()
+#END_TIME = 1.0 | s
+#x.evolve_model(END_TIME)
+
+# x.(evolve for a single timestep)
+
+# amuse tests to check for diverging behaviour
