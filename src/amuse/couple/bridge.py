616fec2165fe32e144259fda70a85ed348539556
Steven Rieder <steven@rieder.nl>
2022-11-19 10:46:09 +0100

    replace if..else with try..except in bridge.py (#903)
    * replace if..else with try..except
    
    * add AttributeError exception
    
    * Update bridge.py

--- src/amuse/couple/bridge.py
+++ src/amuse/couple/bridge.py
@@ -89,7 +89,7 @@ import threading
 from amuse.units import quantities
 from amuse.units import units, constants, generic_unit_system, nbody_system
 from amuse import datamodel
-from amuse.support.exceptions import AmuseException
+from amuse.support.exceptions import AmuseException, CoreException
 
 
 
@@ -320,12 +320,13 @@ class GravityCodeInField(object):
             required_attributes.append('h_smooth')
         self.required_attributes = lambda p, x : x in required_attributes
 
-        if not hasattr(self.code,"parameters"):
-            self.zero_smoothing=True
-        elif not hasattr(self.code.parameters,"epsilon_squared"):
-            self.zero_smoothing=True
-        else:
-            self.zero_smoothing=zero_smoothing
+        try:
+            hasattr(self.code.parameters, "epsilon_squared")
+            self.zero_smoothing = zero_smoothing
+        except AttributeError:
+            self.zero_smoothing = True
+        except CoreException:  # hasattr will fail with an exception
+            self.zero_smoothing = True
 
 
     def evolve_model(self,tend,timestep=None):
