ff07114a1cbe788c6b90eae1d57e7d4fc35a35ff
Steven Rieder <steven+github@rieder.nl>
2023-03-06 23:05:02 +0100

    quantities now convert strings to floats

--- src/amuse/units/quantities.py
+++ src/amuse/units/quantities.py
@@ -283,6 +283,8 @@ class ScalarQuantity(Quantity):
         # Quantity.__init__(self, unit)
         # commented out super call, this speeds thing up
         self.unit = unit
+        if isinstance(number, str):
+            number = float(number)
         if unit.dtype is None:
             self.number = number
         else:
