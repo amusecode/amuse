45bf0378200a4eff3fd6d782a5a10ae380b72d9e
Steven Rieder <steven@rieder.nl>
2024-04-24 11:05:31 -0400

    cleanup units, split off stellar_type unit

--- src/amuse/units/units.py
+++ src/amuse/units/units.py
@@ -3,26 +3,19 @@ Units supported in AMUSE
 """
 
 import numpy
-from amuse.units.si import (
-    named,
-    s,
-    m,
-    kg,
-    none,
-    core,
-)
-from amuse.units.derivedsi import (
-    km,
-    V,
-    W,
-    J,
-    Pa,
-    rad,
-)
-from amuse.units.si import *
-from amuse.units.derivedsi import *
-from amuse.units import constants
+from . import constants
 from . import quantities
+
+# The two imports below are to explicitly expose everything directly used in
+# this module.
+from .si import (named, s, m, kg, none, core, no_unit,)
+from .derivedsi import (km, V, W, J, Pa, rad,)
+
+# importing * from si and derivedsi since we do want all of the units in these
+# modules, and we don't want to repeat everything here.
+from .si import *
+from .derivedsi import *
+from .stellar_types import stellar_type
 
 # misc every day
 minute = named("minute", "min", 60.0 * s)
@@ -57,7 +50,6 @@ kpc = named("kilo parsec", "kpc", 10**3 * parsec)
 Mpc = named("mega parsec", "Mpc", 10**6 * parsec)
 Gpc = named("giga parsec", "Gpc", 10**9 * parsec)
 lightyear = named("light year", "ly", 9460730472580.8 * km)
-# lightyear = named('light year', 'ly', c*julianyr)
 LSun = named("solar luminosity", "LSun", 3.839e26 * W)
 MSun = named("solar mass", "MSun", 1.98892e30 * kg)
 RSun = named("solar radius", "RSun", 6.955e8 * m)
@@ -99,39 +91,10 @@ metallicity = core.none_unit("metallicity", "metallicity")
 
 string = core.string_unit("string", "string")
 
-stellar_type = core.enumeration_unit(
-    "stellar_type",
-    "stellar_type",
-    None,
-    [
-        "deeply or fully convective low mass MS star",  # 0
-        "Main Sequence star",  # 1
-        "Hertzsprung Gap",  # 2
-        "First Giant Branch",  # 3
-        "Core Helium Burning",  # 4
-        "First Asymptotic Giant Branch",  # 5
-        "Second Asymptotic Giant Branch",  # 6
-        "Main Sequence Naked Helium star",  # 7
-        "Hertzsprung Gap Naked Helium star",  # 8
-        "Giant Branch Naked Helium star",  # 9
-        "Helium White Dwarf",  # 10
-        "Carbon/Oxygen White Dwarf",  # 11
-        "Oxygen/Neon White Dwarf",  # 12
-        "Neutron Star",  # 13
-        "Black Hole",  # 14
-        "Massless Supernova",  # 15
-        "Unknown stellar type",  # 16
-        "Pre-main-sequence Star",  # 17
-        "Planet",  # 18
-        "Brown Dwarf",  # 19
-    ],
-)
-
 # special unit for keys of particles
 object_key = core.key_unit("object_key", "key")
 
 # angles
-# rad=named('radian','rad',m/m) (defined in derivedsi.py)
 pi = numpy.pi | rad
 rev = named("revolutions", "rev", (2 * numpy.pi) * rad)
 deg = named("degree", "deg", (numpy.pi / 180) * rad)
