56d4921ed4f95f3d3f2d5896bf76e8fee87fa1bd
Lourens Veen <l.veen@esciencecenter.nl>
2024-06-04 14:41:33 +0200

    Make library installation more robust and portable

--- support/shared/lib-targets.mk
+++ support/shared/lib-targets.mk
@@ -9,6 +9,15 @@ all: $(DYNAMIC_LIB) $(STATIC_LIB) $(DYNAMIC_LIB_MPI) $(STATIC_LIB_MPI)
 # ends up inside of them, and it may not be defined when make all is called.
 .PHONY: install
 install: all $(PKG_CONFIG_FILE) $(PKG_CONFIG_FILE_MPI)
+ifneq (,$(HEADERS)$(HEADERS_MPI))
+	mkdir -p $(PREFIX)/include
+endif
+ifneq (,$(STATIC_LIB)$(DYNAMIC_LIB)$(STATIC_LIB_MPI)$(DYNAMIC_LIB_MPI))
+	mkdir -p $(PREFIX)/lib
+endif
+ifneq (,$(PKG_CONFIG_FILE)$(PKG_CONFIG_FILE_MPI))
+	mkdir -p $(PREFIX)/lib/pkgconfig
+endif
 ifdef HEADERS
 	$(INSTALL) -m 644 $(HEADERS) $(PREFIX)/include/
 endif
@@ -19,7 +28,7 @@ ifdef DYNAMIC_LIB
 	$(INSTALL) -m 644 $(DYNAMIC_LIB) $(PREFIX)/lib/$(DYNAMIC_LIB)
 endif
 ifdef PKG_CONFIG_FILE
-	$(INSTALL) -D -m 644 $(PKG_CONFIG_FILE) $(PREFIX)/lib/pkgconfig/$(PKG_CONFIG_FILE)
+	$(INSTALL) -m 644 $(PKG_CONFIG_FILE) $(PREFIX)/lib/pkgconfig/$(PKG_CONFIG_FILE)
 endif
 ifdef HEADERS_MPI
 	$(INSTALL) -m 644 $(HEADERS_MPI) $(PREFIX)/include/
@@ -31,7 +40,7 @@ ifdef DYNAMIC_LIB_MPI
 	$(INSTALL) -m 644 $(DYNAMIC_LIB_MPI) $(PREFIX)/lib/$(DYNAMIC_LIB_MPI)
 endif
 ifdef PKG_CONFIG_FILE_MPI
-	$(INSTALL) -D -m 644 $(PKG_CONFIG_FILE_MPI) $(PREFIX)/lib/pkgconfig/$(PKG_CONFIG_FILE_MPI)
+	$(INSTALL) -m 644 $(PKG_CONFIG_FILE_MPI) $(PREFIX)/lib/pkgconfig/$(PKG_CONFIG_FILE_MPI)
 endif
 
 
