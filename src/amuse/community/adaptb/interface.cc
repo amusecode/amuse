5ec3843e1b82b6efedd356c13bbed96b0edca3ca
Lourens Veen <l.veen@esciencecenter.nl>
2024-08-28 10:56:09 +0200

    Convert adaptb to the new build system

--- src/amuse/community/adaptb/interface.cc
+++ src/amuse/community/adaptb/interface.cc
@@ -6,7 +6,7 @@
 #include "Bs_integrator.h"
 #include "Clock.h"
 #include <string.h>
-#include "worker_code.h"
+#include "adaptb_worker.h"
 
 #define MAX_STRING_LEN 1024
 using namespace mpfr;
--- src/amuse/community/adaptb/interface.py
+++ src/amuse/community/adaptb/interface.py
@@ -8,7 +8,7 @@ class AdaptbInterface(CodeInterface, GravitationalDynamicsInterface, LiteratureR
     """
     Adaptb (Accurate Dynamics with Arbitrary Precision by Tjarda Boekholt)
     """
-    include_headers = ['worker_code.h', 'stopcond.h']
+    include_headers = ['adaptb_worker.h', 'stopcond.h']
 
     def __init__(self, **options):
         CodeInterface.__init__(self, name_of_the_worker="adaptb_worker", **options)
