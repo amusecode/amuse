d891e5da76f6c2a0dc978cd082868f5d234c34dc
Steven Rieder <steven@rieder.nl>
2024-11-26 13:40:10 +0100

    run Black on molecular_cloud.py

--- src/amuse/ic/molecular_cloud.py
+++ src/amuse/ic/molecular_cloud.py
@@ -4,12 +4,24 @@ Generate a molecular cloud of SPH particles
 
 from amuse.ext.molecular_cloud import molecular_cloud
 
+
 def new_molecular_cloud(
-    nf=32, power=-3., targetN=10000, ethep_ratio=0.01,
-    convert_nbody=None, ekep_ratio=1., seed=None, base_grid=None,
+    nf=32,
+    power=-3.0,
+    targetN=10000,
+    ethep_ratio=0.01,
+    convert_nbody=None,
+    ekep_ratio=1.0,
+    seed=None,
+    base_grid=None,
 ):
     return molecular_cloud(
-        nf=nf, power=power, targetN=targetN, ethep_ratio=ethep_ratio,
-        convert_nbody=convert_nbody, ekep_ratio=ekep_ratio, seed=seed,
+        nf=nf,
+        power=power,
+        targetN=targetN,
+        ethep_ratio=ethep_ratio,
+        convert_nbody=convert_nbody,
+        ekep_ratio=ekep_ratio,
+        seed=seed,
         base_grid=base_grid,
     ).result
