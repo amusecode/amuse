22d098728d55ea14d15bd8ab4bbfd3c42c1663ff
Lourens Veen <l.veen@esciencecenter.nl>
2025-01-25 11:42:43 +0100

    Fix hermite_grx build and add trivial test

---
+++ src/amuse/community/hermite_grx/tests/test_hermite_grx.py
@@ -0,0 +1,14 @@
+from amuse.community.hermite_grx.interface import HermiteGRX, HermiteGRXInterface
+
+
+class TestTests:
+    """Dummy test class to avoid failure.
+
+    Pytest fails the tests if it doesn't find any. Hermite GRX doesn't have any tests
+    yet, so here's a dummy test to avoid the CI failing.
+
+    Of course ideally there would be some actual tests, so please add some if you can.
+    """
+
+    def test_dummy(self):
+        pass
