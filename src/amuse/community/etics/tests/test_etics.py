68ab05e07080bd70ce82cbeb5eb4509e834eadd2
Lourens Veen <l.veen@esciencecenter.nl>
2025-01-26 18:33:06 +0100

    Fix etics package

---
+++ src/amuse/community/etics/tests/test_etics.py
@@ -0,0 +1,14 @@
+from amuse.community.etics.interface import Etics, EticsInterface
+
+
+class TestTests:
+    """Dummy test class to avoid failure.
+
+    Pytest fails the tests if it doesn't find any. Etics doesn't have any tests yet, so
+    here's a dummy test to avoid the CI failing.
+
+    Of course ideally there would be some actual tests, so please add some if you can.
+    """
+
+    def test_dummy(self):
+        pass
