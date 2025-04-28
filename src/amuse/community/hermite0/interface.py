41f0a86288ae0a279ddf549cb379de8e4b8be7e5
Lourens Veen <l.veen@esciencecenter.nl>
2025-04-28 21:11:16 +0200

    Fix broken syntax in forwards to empty codes

--- src/amuse/community/hermite0/interface.py
+++ src/amuse/community/hermite0/interface.py
@@ -1,7 +1,1 @@
 from amuse.support.import_helper import load_code
-
-
- = load_code("hermite0", "")
- = load_code("hermite0", "")
-
- = 
--- src/amuse/community/simplex2_5/interface.py
+++ src/amuse/community/simplex2_5/interface.py
@@ -1,7 +1,1 @@
 from amuse.support.import_helper import load_code
-
-
- = load_code("simplex2_5", "")
- = load_code("simplex2_5", "")
-
- = 
