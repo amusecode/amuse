7c3e7b211e423e78f166e4aa068ee66b510a9bb1
Steven Rieder <s.rieder@uva.nl>
2025-05-11 19:42:56 +0200

    still a missing import

--- src/amuse/community/mocassin/interface.py
+++ src/amuse/community/mocassin/interface.py
@@ -1,7 +1,7 @@
 from amuse.support.import_helper import load_code
-
+from amuse.units.units import eV
 
 MocassinInterface = load_code("mocassin", "MocassinInterface")
 Mocassin = load_code("mocassin", "Mocassin")
 
-mocassin_rydberg_unit = 13.6 * units.eV
+mocassin_rydberg_unit = 13.6 * eV
