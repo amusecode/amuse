95725450ee942dc7b3612b99dee2447fc27c5676
Steven Rieder <s.rieder@uva.nl>
2025-05-11 19:29:20 +0200

    Add missing unit back to Mocassin interface
    Fixes regression due to importing mocassin_rydberg_unit failing

--- src/amuse/community/mocassin/interface.py
+++ src/amuse/community/mocassin/interface.py
@@ -4,4 +4,4 @@ from amuse.support.import_helper import load_code
 MocassinInterface = load_code("mocassin", "MocassinInterface")
 Mocassin = load_code("mocassin", "Mocassin")
 
-Mocassin = Mocassin
+mocassin_rydberg_unit = 13.6 * units.eV
